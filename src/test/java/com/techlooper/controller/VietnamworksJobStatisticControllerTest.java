a1de9358501f38031c1c20781bb13a7238fa0307
Phuong H <phuonghqh@gmail.com>
2014-09-18 09:10:13 +0700

    Add unit test for Controller.countTechnicalTerm(..)

--- src/test/java/com/techlooper/controller/VietnamworksJobStatisticControllerTest.java
+++ src/test/java/com/techlooper/controller/VietnamworksJobStatisticControllerTest.java
@@ -2,48 +2,37 @@ package com.techlooper.controller;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@Configuration
-@ContextConfiguration(classes = { VietnamworksJobStatisticControllerTest.class })
+import com.techlooper.model.JobStatisticResquest;
+import com.techlooper.model.TechnicalTermEnum;
+import com.techlooper.service.JobStatisticService;
+
 public class VietnamworksJobStatisticControllerTest {
 
-   // @Value("classpath:json/VietnamworksJobStatisticControllerTest.json")
-   // private Resource jsonResource;
-   //
-   // @Mock
-   // private JobStatisticService vietnamWorksJobStatisticService;
-   //
-   // @InjectMocks
-   // private VietnamworksJobStatisticController controller;
-   //
-   // private MockMvc mockMvc;
+   @Mock
+   private JobStatisticService vietnamWorksJobStatisticService;
+
+   @InjectMocks
+   private VietnamworksJobStatisticController controller;
+
+   @Mock
+   private SimpMessagingTemplate messagingTemplate;
 
    @Before
    public void setup() {
-      // MockitoAnnotations.initMocks(this);
-      // mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+      MockitoAnnotations.initMocks(this);
    }
 
    @Test
    public void testCountTechnicalJobs() throws Exception {
-      // for (TechnicalTermEnum term : TechnicalTermEnum.values()) {
-      // Mockito.when(vietnamWorksJobStatisticService.count(term)).thenReturn(2L);
-      // }
-      //
-      // String expectedJson =
-      // Files.readAllLines(Paths.get(jsonResource.getURI())).stream()
-      // .collect(Collectors.joining(""));
-      // mockMvc.perform(get("/technical-job")).andExpect(status().isOk())
-      // .andExpect(content().contentType("application/json;charset=UTF-8")).andExpect(content().json(expectedJson));
-      //
-      // for (TechnicalTermEnum term : TechnicalTermEnum.values()) {
-      // Mockito.verify(vietnamWorksJobStatisticService,
-      // Mockito.times(1)).count(term);
-      // }
+      JobStatisticResquest requestTerm = new JobStatisticResquest.Builder().withTerm("JAVA").build();
+      Mockito.when(vietnamWorksJobStatisticService.count(TechnicalTermEnum.JAVA)).thenReturn(2L);
+      controller.countTechnicalJobs(requestTerm);
+      Mockito.verify(vietnamWorksJobStatisticService, Mockito.times(1)).count(TechnicalTermEnum.JAVA);
    }
 }
