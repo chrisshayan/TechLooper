7289940df861f992252a8972f6e4c47844c91247
Phuong H <phuonghqh@gmail.com>
2014-09-19 16:07:58 +0700

    Support request mapping to get technical job by term

--- src/main/java/com/techlooper/controller/VietnamworksJobStatisticController.java
+++ src/main/java/com/techlooper/controller/VietnamworksJobStatisticController.java
@@ -2,11 +2,15 @@ package com.techlooper.controller;
 
 import javax.annotation.Resource;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 import com.techlooper.model.JobStatisticResponse;
+import com.techlooper.model.JobStatisticResquest;
+import com.techlooper.model.TechnicalTermEnum;
 import com.techlooper.service.JobStatisticService;
 
 @Controller
@@ -18,62 +22,29 @@ public class VietnamworksJobStatisticController {
    @Resource
    private SimpMessagingTemplate messagingTemplate;
 
-   @Scheduled(cron = "${scheduled.cron}")
-   public void countJavaJobs() {
-      messagingTemplate.convertAndSend("/topic/technical-job/java",
-            new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countJavaJobs()).build());
-   }
-
-   @Scheduled(cron = "${scheduled.cron}")
-   public void countRubyJobs() {
-      messagingTemplate.convertAndSend("/topic/technical-job/ruby",
-            new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countRubyJobs()).build());
-   }
-
-   @Scheduled(cron = "${scheduled.cron}")
-   public void countQAJobs() {
-      messagingTemplate.convertAndSend("/topic/technical-job/qa",
-            new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countQAJobs()).build());
-   }
-
-   @Scheduled(cron = "${scheduled.cron}")
-   public void countPythonJobs() {
-      messagingTemplate.convertAndSend("/topic/technical-job/python",
-            new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countPythonJobs()).build());
-   }
-
-   @Scheduled(cron = "${scheduled.cron}")
-   public void countProjectManagerJobs() {
-      messagingTemplate.convertAndSend("/topic/technical-job/pm", new JobStatisticResponse.Builder()
-            .withCount(vietnamWorksJobStatisticService.countProjectManagerJobs()).build());
-   }
-
-   @Scheduled(cron = "${scheduled.cron}")
-   public void countPhpJobs() {
-      messagingTemplate.convertAndSend("/topic/technical-job/php",
-            new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countPhpJobs()).build());
-   }
-
-   @Scheduled(cron = "${scheduled.cron}")
-   public void countDBAJobs() {
-      messagingTemplate.convertAndSend("/topic/technical-job/dba",
-            new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countDBAJobs()).build());
-   }
-
-   @Scheduled(cron = "${scheduled.cron}")
-   public void countBAJobs() {
-      messagingTemplate.convertAndSend("/topic/technical-job/ba",
-            new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countBAJobs()).build());
-   }
-
-   @Scheduled(cron = "${scheduled.cron}")
-   public void countDotNetJobs() {
-      messagingTemplate.convertAndSend("/topic/technical-job/dotnet",
-            new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countDotNetJobs()).build());
+   public static void main(String[] a) {
+      System.out.println(TechnicalTermEnum.PROJECT_MANAGER.name().toLowerCase());
    }
 
    @Scheduled(cron = "${scheduled.cron}")
    public void countTechnicalJobs() {
+      for (TechnicalTermEnum term : TechnicalTermEnum.values()) {
+         messagingTemplate.convertAndSend("/topic/technical-job/" + term.name().toLowerCase(),
+               new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.count(term)).build());
+      }
+   }
+
+   @MessageMapping("/technical-job")
+   public void countTechnicalJobs(JobStatisticResquest request) {
+      messagingTemplate.convertAndSend(
+            "/topic/technical-job/" + request.getTerm().toLowerCase(),
+            new JobStatisticResponse.Builder().withCount(
+                  vietnamWorksJobStatisticService.count(TechnicalTermEnum.valueOf(request.getTerm().toUpperCase())))
+                  .build());
+   }
+
+   @Scheduled(cron = "${scheduled.cron}")
+   public void countAllTechnicalJobs() {
       messagingTemplate.convertAndSend("/topic/technical-job/all",
             new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countTechnicalJobs()).build());
    }
