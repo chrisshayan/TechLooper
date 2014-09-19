dc69682cdea2ee79225b4fd1b6cd911938a9d895
Phuong H <phuonghqh@gmail.com>
2014-09-19 14:08:47 +0700

    Remove unused code

--- src/main/java/com/techlooper/controller/VietnamworksJobStatisticController.java
+++ src/main/java/com/techlooper/controller/VietnamworksJobStatisticController.java
@@ -23,58 +23,54 @@ public class VietnamworksJobStatisticController {
       messagingTemplate.convertAndSend("/topic/technical-job/java",
             new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countJavaJobs()).build());
    }
-   
+
    @Scheduled(cron = "${scheduled.cron}")
    public void countRubyJobs() {
       messagingTemplate.convertAndSend("/topic/technical-job/ruby",
             new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countRubyJobs()).build());
    }
-   
-   
+
    @Scheduled(cron = "${scheduled.cron}")
    public void countQAJobs() {
       messagingTemplate.convertAndSend("/topic/technical-job/qa",
             new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countQAJobs()).build());
    }
-   
+
    @Scheduled(cron = "${scheduled.cron}")
    public void countPythonJobs() {
       messagingTemplate.convertAndSend("/topic/technical-job/python",
             new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countPythonJobs()).build());
    }
-   
+
    @Scheduled(cron = "${scheduled.cron}")
    public void countProjectManagerJobs() {
-      messagingTemplate.convertAndSend("/topic/technical-job/project_manager",
-            new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countProjectManagerJobs()).build());
+      messagingTemplate.convertAndSend("/topic/technical-job/project_manager", new JobStatisticResponse.Builder()
+            .withCount(vietnamWorksJobStatisticService.countProjectManagerJobs()).build());
    }
-   
+
    @Scheduled(cron = "${scheduled.cron}")
    public void countPhpJobs() {
       messagingTemplate.convertAndSend("/topic/technical-job/php",
             new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countPhpJobs()).build());
    }
-   
-   
+
    @Scheduled(cron = "${scheduled.cron}")
    public void countDBAJobs() {
       messagingTemplate.convertAndSend("/topic/technical-job/dba",
             new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countDBAJobs()).build());
    }
-   
-   
+
    @Scheduled(cron = "${scheduled.cron}")
    public void countBAJobs() {
       messagingTemplate.convertAndSend("/topic/technical-job/ba",
             new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countBAJobs()).build());
    }
-   
+
    @Scheduled(cron = "${scheduled.cron}")
    public void countDotNetJobs() {
       messagingTemplate.convertAndSend("/topic/technical-job/dotnet",
             new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.countDotNetJobs()).build());
    }
-
 
    @Scheduled(cron = "${scheduled.cron}")
    public void countTechnicalJobs() {
