1c9dc84073de6f9a3030f7360593585337e8a00a
Phuong H <phuonghqh@gmail.com>
2014-09-22 15:45:05 +0700

    Return terms

--- src/main/java/com/techlooper/controller/VietnamworksJobStatisticController.java
+++ src/main/java/com/techlooper/controller/VietnamworksJobStatisticController.java
@@ -3,6 +3,7 @@ package com.techlooper.controller;
 import javax.annotation.Resource;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
@@ -24,9 +25,15 @@ public class VietnamworksJobStatisticController {
    @Scheduled(cron = "${scheduled.cron}")
    public void countTechnicalJobs() {
       for (TechnicalTermEnum term : TechnicalTermEnum.values()) {
-         messagingTemplate.convertAndSend("/topic/technical-job/" + term.name().toLowerCase(),
-               new JobStatisticResponse.Builder().withCount(vietnamWorksJobStatisticService.count(term)).build());
+         messagingTemplate.convertAndSend("/topic/technical-job/" + term.name(), new JobStatisticResponse.Builder()
+               .withCount(vietnamWorksJobStatisticService.count(term)).build());
       }
+   }
+
+   @SendTo("/topic/technical-job/terms")
+   @MessageMapping("/technical-job/terms")
+   public TechnicalTermEnum[] countTechnicalTerms() {
+      return TechnicalTermEnum.values();
    }
 
    @MessageMapping("/technical-job")
