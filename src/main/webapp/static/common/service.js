cfc8960b6266b84f02ba68e4845e9d2625164ef5
Phuong H <phuonghqh@gmail.com>
2014-09-27 10:45:29 +0700

    Header section

--- src/main/webapp/static/common/service.js
+++
@@ -1,9 +0,0 @@
-angular.module('Common', []).factory('connectionService', function() {
-   
-   return {
-      fetchTerms : function() {
-         
-         return "Yeah, it is running...";
-      }
-   }
-});
---
+++ src/main/webapp/static/modules/header/header.ctrl.js
@@ -0,0 +1,37 @@
+angular.module("Header").controller("headerController", function($scope, $http, jsonFactory) {
+
+   $scope.keyList = jsonFactory.createShortcut();
+   
+   
+   // TODO: refactor later
+   var key = $(".keyboard");
+   key.on("click", function() {
+       if ($(".keyboard-shortcuts-items:first").is(":hidden")) {
+           $(".keyboard-shortcuts-items").slideDown("slow");
+       } else {
+           $(".keyboard-shortcuts-items").hide();
+       }
+   });
+
+   $scope.settingStyle = function() {
+      var set = $(".setting-content");
+      set.on("click", function() {
+         set.find("ul.setting-items").css("display", "block");
+         set.stop().animate({
+            width : "125"
+         });
+      }).mouseleave(function() {
+         set.find("ul.setting-items").css("display", "none");
+         set.stop().animate({
+            width : "28px"
+         });
+         $(".keyboard-shortcuts-items").hide();
+      });
+   }
+
+   // $http.get("data/keyboardshortcuts.json").success(function(data, status, headers, config) {
+   // $scope.keyList = data;
+   // }).error(function(data, status, headers, config) {
+   // // log error
+   // });
+}).controller();
