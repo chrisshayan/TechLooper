9b208d8e6a0ae74858d754452986e5c8bfa49077
Phuong H <phuonghqh@gmail.com>
2014-09-27 15:19:31 +0700

    Find jobs button

--- src/main/webapp/static/modules/common/common.ftr.js
+++ src/main/webapp/static/modules/common/common.ftr.js
@@ -20,8 +20,33 @@ angular.module("Common").factory("jsonFactory", function() {
             "keyShort" : "Ctrl + Alt + 2"
          } ];
       },
-      companies : function() {
 
+      companies : function() {
+         return [ {
+            "id" : 1,
+            "img" : "images/cp-logo-atlassian.png",
+            "url" : "https://www.atlassian.com/"
+         }, {
+            "id" : 2,
+            "img" : "images/cp-logo-google.png",
+            "url" : "https://www.google.com/"
+         }, {
+            "id" : 3,
+            "img" : "images/cp-logo-techcombank.png",
+            "url" : "https://www.techcombank.com.vn/"
+         }, {
+            "id" : 4,
+            "img" : "images/cp-logo-fpt.png",
+            "url" : "https://www.fpt.vn/"
+         }, {
+            "id" : 5,
+            "img" : "images/cp-logo-harveynash.png",
+            "url" : "https://www.harveynash.vn/"
+         }, {
+            "id" : 6,
+            "img" : "images/cp-logo-vtv.png",
+            "url" : "https://www.vtv.vn/"
+         } ];
       }
    }
 });
