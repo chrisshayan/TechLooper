7874f1e85daa01e112d97b6909726d2ea969ad65
Phuong H <phuonghqh@gmail.com>
2014-09-29 22:25:20 +0700

    * Config Angular to support transaltion multiple language * Fix error 404 "...company.img...not found"

--- src/main/webapp/assets/modules/app.js
+++ src/main/webapp/assets/modules/app.js
@@ -5,30 +5,40 @@ angular.module("Header", []);
 angular.module("Footer", []);
 angular.module("Chart", []);
 
-var techlooper = angular.module("Techlooper",
-      [ "ngResource", "ngRoute", "Home", "Header", "Footer", "Common", "Chart" ]);
+var techlooper = angular.module("Techlooper", [ "pascalprecht.translate", "ngResource", "ngCookies", "ngRoute", "Home", "Header", "Footer", "Common", "Chart" ]);
 
-techlooper.config(function($routeProvider) {
-   // $urlRouterProvider.otherwise('/');
-   $routeProvider.when('/', {
+techlooper.config(function($routeProvider, $translateProvider, $locationProvider) {
+   $translateProvider.useStaticFilesLoader({
+      prefix : "modules/translation/messages_",
+      suffix : ".json"
+   });
+
+   $translateProvider.registerAvailableLanguageKeys(['en-US', 'vi']);
+   $translateProvider.fallbackLanguage("en-US");
+   $translateProvider.preferredLanguage("en-US");
+   $translateProvider.useLocalStorage();
+   $translateProvider.useCookieStorage();
+   $translateProvider.use(window.navigator.userLanguage || window.navigator.language);
+
+   $routeProvider.when("/", {
       templateUrl : "modules/home/home.tpl.html",
       controller : "homeController"
    }).otherwise({
-      redirectTo : '/'
+      redirectTo : "/"
    });
-   // $locationProvider.html5Mode(true);
+   $locationProvider.html5Mode(true);
 });
 
-techlooper.directive('header', function() {
+techlooper.directive("header", function() {
    return {
-      restrict : 'A', // This mens that it will be used as an attribute and NOT as an element.
+      restrict : "A", // This mens that it will be used as an attribute and NOT as an element.
       replace : true,
       templateUrl : "modules/header/header.tpl.html",
       controller : "headerController"
    }
-}).directive('footer', function() {
+}).directive("footer", function() {
    return {
-      restrict : 'A', // This mens that it will be used as an attribute and NOT as an element.
+      restrict : "A", // This mens that it will be used as an attribute and NOT as an element.
       replace : true,
       templateUrl : "modules/footer/footer.tpl.html",
       controller : "footerController"
