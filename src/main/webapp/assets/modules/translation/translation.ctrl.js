0fbed5e7a3662481b4221872950549679b1b64ee
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-09-30 16:17:42 +0700

    remove footer, keyboard shortcuts and add language icon on the setting bar

--- src/main/webapp/assets/modules/translation/translation.ctrl.js
+++ src/main/webapp/assets/modules/translation/translation.ctrl.js
@@ -1,5 +1,6 @@
 angular.module('Common').controller('translationController', function($scope, $translate) {
-   $scope.setLang = function(langKey) {
-      $translate.use(langKey);
-   };
+   $(".langKey").on('click', function() {
+   	    var lang = $translate.use();
+   		$translate.use(lang == "vi" ? "en-US":"vi");
+   });
 });
