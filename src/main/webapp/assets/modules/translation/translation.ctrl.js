e17dbb45d14f6001c966ef3ccf44940455992288
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-09-30 16:33:58 +0700

    add language to setting bar on the header

--- src/main/webapp/assets/modules/translation/translation.ctrl.js
+++ src/main/webapp/assets/modules/translation/translation.ctrl.js
@@ -1,6 +1,6 @@
 angular.module('Common').controller('translationController', function($scope, $translate) {
-   $(".langKey").on('click', function() {
+   $scope.setLang =  function() {
    	    var lang = $translate.use();
    		$translate.use(lang == "vi" ? "en-US":"vi");
-   });
+   };
 });
