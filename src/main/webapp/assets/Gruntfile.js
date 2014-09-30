e92cdb386134221a8082fb10afc251d3a1919fcd
hamedshayan@gmail.com <Hamed9243>
2014-09-30 18:07:17 +0700

    refactored and organized it.

--- src/main/webapp/assets/Gruntfile.js
+++ src/main/webapp/assets/Gruntfile.js
@@ -3,6 +3,12 @@ module.exports = function(grunt) {
    grunt.initConfig({
       pkg : grunt.file.readJSON("package.json"),
 
+      bower : {
+          install : {
+
+          }
+      },
+
       wiredep : {
          target : {
             src : [ "index.html" ]
@@ -56,8 +62,9 @@ module.exports = function(grunt) {
    grunt.loadNpmTasks("grunt-contrib-watch");
    grunt.loadNpmTasks("grunt-contrib-connect");
    grunt.loadNpmTasks("grunt-include-source");
-   grunt.loadNpmTasks("grunt-wiredep")
+   grunt.loadNpmTasks("grunt-wiredep");
+   grunt.loadNpmTasks("grunt-bower-task");
 
-   grunt.registerTask("build", [ "includeSource:target", "wiredep:target" ]);
+   grunt.registerTask("build", [ "bower:install", "includeSource:target", "wiredep:target" ]);
    grunt.registerTask("run", [ "connect", "watch" ]);
 };
