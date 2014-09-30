c90aa3ed3befd5db2aa9ad8983c1064c9774a052
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-09-30 16:33:58 +0700

    add language to setting bar on the header

--- src/main/webapp/assets/modules/home/home.mod.js
+++ src/main/webapp/assets/modules/home/home.mod.js
@@ -2,8 +2,8 @@ angular.module("Home").directive("chart", function() {
    return {
       restrict : "A", // This mens that it will be used as an attribute and NOT as an element.
       replace : true,
-      templateUrl : "modules/collection/chart.tpl.html"
-      //controller : "chartController"
+      templateUrl : "modules/collection/chart.tpl.html",
+      controller : "chartController"
    }
 }).directive("findjobs", function() {
    return {
