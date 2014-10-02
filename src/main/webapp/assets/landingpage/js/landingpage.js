6bba9074158a3bb2e3d38ba3d613418c7ef8de73
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-10-02 18:14:55 +0700

    Change index

--- src/main/webapp/assets/landingpage/js/landingpage.js
+++ src/main/webapp/assets/landingpage/js/landingpage.js
@@ -1,4 +1,22 @@
 $( document ).ready(function() {
-	$('#bubble').parallax();
+	var h = $(window).height();
+	$('.bg-landingpage').css({
+		height:h - 30
+	});
+
+	$('#bubble').parallax({
+	  calibrateX: false,
+	  calibrateY: true,
+	  invertX: false,
+	  invertY: true,
+	  limitX: false,
+	  limitY: 10,
+	  scalarX: 2,
+	  scalarY: 8,
+	  frictionX: 0.2,
+	  frictionY: 0.8,
+	  originX: 0.0,
+	  originY: 1.0
+	});
 	$('#main-page').parallax();
 });
