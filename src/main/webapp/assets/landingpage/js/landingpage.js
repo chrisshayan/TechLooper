dfa9af97668083d9fa6a1aa27b68d824a8fd1bd0
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-10-02 19:00:51 +0700

    fixed bug for button on mobile version

--- src/main/webapp/assets/landingpage/js/landingpage.js
+++ src/main/webapp/assets/landingpage/js/landingpage.js
@@ -4,20 +4,7 @@ $( document ).ready(function() {
 		height:h - 30
 	});
 
-	$('#bubble').parallax({
-	  calibrateX: false,
-	  calibrateY: true,
-	  invertX: false,
-	  invertY: true,
-	  limitX: false,
-	  limitY: 10,
-	  scalarX: 2,
-	  scalarY: 8,
-	  frictionX: 0.2,
-	  frictionY: 0.8,
-	  originX: 0.0,
-	  originY: 1.0
-	});
+	$('#bubble').parallax();
 	$('#main-page').parallax();
 	$("iframe").contents().find("#u_0_3").css("display", "none");
 });
