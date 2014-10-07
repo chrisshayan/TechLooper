1f1142ceaf149d11350d83fe56c0306665c8c9fd
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-10-07 11:23:51 +0700

    update source code

--- src/main/webapp/assets/modules/home/home.mod.js
+++ src/main/webapp/assets/modules/home/home.mod.js
@@ -9,7 +9,7 @@ angular.module("Home").directive("chart", function() {
    return {
       restrict : "A", // This mens that it will be used as an attribute and NOT as an element.
       replace : true,
-      templateUrl : "modules/job/findJobs.button.tpl.html",
+      templateUrl : "modules/job/findJobs.tpl.html",
       controller: 'findJobsController'
    }
 });
