c81250f361683bf6076ced5543ae910c268a04c4
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-10-07 17:16:29 +0700

    add pie chart, fix bug for setting bar, add text intro

--- src/main/webapp/assets/modules/header/header.srv.js
+++ src/main/webapp/assets/modules/header/header.srv.js
@@ -1,13 +1,35 @@
-angular.module("Header").factory("headerService",['$location', function($location) {
+angular.module("Header").factory("headerService",['$location','$rootScope', function($location, $rootScope) {
 	var hash;
     return {
         changeView: function(event) {
+            var item = $(event.target).parent().parent().find('i');
+            item.removeClass('active');
         	if($(event.target).hasClass('fa-pie-chart')){
         		hash = 'pieChart';
+                $(event.target).addClass('active');
         	}else{
         		hash = 'bubbleChart';
+                $(event.target).addClass('active');
         	}
         	$location.path(hash);
+            $rootScope.$apply();
+        },
+        showSetting: function(){
+            var set = $(".setting-content");
+            if(set.hasClass('hideContent')){
+                set.removeClass('hideContent').addClass('showContent');
+                set.find("ul.setting-items").css("display", "block");
+                set.stop().animate({
+                    width : "125" // 125
+                });
+                
+            }else{
+                set.removeClass('showContent').addClass('hideContent');
+                set.find("ul.setting-items").css("display", "none");
+                set.stop().animate({
+                    width : "28" // 125
+                });               
+            }           
         }
     }
 }]);
