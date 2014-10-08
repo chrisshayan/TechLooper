efbbd15c56dd99a54f82a9555b435df57015729c
Phuong H <phuonghqh@gmail.com>
2014-10-08 16:02:47 +0700

    Fix dupplicate terms

--- src/main/webapp/assets/modules/collection/chart.ctrl.js
+++ src/main/webapp/assets/modules/collection/chart.ctrl.js
@@ -6,54 +6,51 @@ angular.module('Chart', [ "Bubble", "Pie", "Common", "Header" ]).controller(
             $rootScope.$doChart();
          });
 
-         var doChart = function() {
-            var events = jsonValue.events;
-            var rColor = 0;
-            var terms = [];
+         var events = jsonValue.events;
+         var rColor = 0;
+         var terms = [];
+         var chartFactory;
+         
+         connectionFactory.initialize($scope);
+         $rootScope.$doChart = function() {
             var chart = headerService.getChart();
-            var chartFactory = (chart === jsonValue.charts.pie) ? pieFactory : bubbleFactory;
             $('.chart-contrainer').empty().append((chart === jsonValue.charts.pie) ? jsonValue.ui.pie : jsonValue.ui.bubble);
+            chartFactory = (chart === jsonValue.charts.pie) ? pieFactory : bubbleFactory;
+            connectionFactory.receiveTechnicalTerms();
+         }
+         
+         $scope.$on(events.terms, function(event, data) {
+            terms = data;
+            chartFactory.setTerms(terms);
+            $.each(terms, function(index, term) {
+               rColor = rColor + 1;
+               if (rColor == 9) {
+                  rColor = 0;
+               }
+               chartFactory.draw({
+                  'colorID' : rColor,
+                  'count' : term.count,
+                  'termName' : term.name,
+                  'termID' : term.term
+               }, true);
 
-            connectionFactory.initialize($scope);
-
-            $scope.$on(events.terms, function(event, data) {
-               terms = data;
-               chartFactory.setTerms(terms);
-               $.each(terms, function(index, term) {
+               $scope.$on(events.term + term.term, function(event, data) {
                   rColor = rColor + 1;
                   if (rColor == 9) {
                      rColor = 0;
                   }
                   chartFactory.draw({
                      'colorID' : rColor,
-                     'count' : term.count,
-                     'termName' : term.name,
-                     'termID' : term.term
-                  }, true);
-
-                  $scope.$on(events.term + term.term, function(event, data) {
-                     rColor = rColor + 1;
-                     if (rColor == 9) {
-                        rColor = 0;
-                     }
-                     chartFactory.draw({
-                        'colorID' : rColor,
-                        'count' : data.count,
-                        'termName' : data.termName,
-                        'termID' : data.term
-                     });
+                     'count' : data.count,
+                     'termName' : data.termName,
+                     'termID' : data.term
                   });
-
                });
 
-               chartFactory.initializeAnimation();
             });
 
-            connectionFactory.receiveTechnicalTerms();
-         }
-         $rootScope.$doChart = doChart;
+            chartFactory.initializeAnimation();
+         });
          
-         
-         console.log("chart controller doChart");
-         doChart();
+         $rootScope.$doChart();
       } ]);
