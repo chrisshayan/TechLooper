176a20cd645baa9f87d47ef0e6eaad89f51f1b01
Phuong H <phuonghqh@gmail.com>
2014-10-08 16:33:19 +0700

    Fix reload chart when duplicate click on same button

--- src/main/webapp/assets/modules/header/header.srv.js
+++ src/main/webapp/assets/modules/header/header.srv.js
@@ -1,41 +1,54 @@
-angular.module("Header").factory("headerService",['jsonValue', '$rootScope', function(jsonValue, $rootScope) {
-	var chartStyle;
-    return {
-        changeChart: function(event) {
-            var item = $(event.target).parent().parent().find('i');
-            item.removeClass('active');
-        	if($(event.target).hasClass('fa-pie-chart')){
-        		chartStyle = jsonValue.charts.pie;
-                $(event.target).addClass('active');
-        	}else{
-        		chartStyle = jsonValue.charts.bubble;
-                $(event.target).addClass('active');
-        	}
+angular.module("Header").factory("headerService", [ 'jsonValue', '$rootScope', function(jsonValue, $rootScope) {
+   var chartStyle;
+   var instance = {
+      changeChart : function(event) {
+         var chart = instance.getClickedChartStyle($(event.target));
+         if (chart === chartStyle) {
+            return;
+         }
 
-            $rootScope.$emit(jsonValue.events.changeChart, chartStyle);
-        },
-        showSetting: function(){
-            var set = $(".setting-content");
-            if(set.hasClass('hideContent')){
-                set.removeClass('hideContent').addClass('showContent');
-                set.find("ul.setting-items").css("display", "block");
-                set.stop().animate({
-                    width : "125"
-                });
-                
-            }else{
-                set.removeClass('showContent').addClass('hideContent');
-                set.find("ul.setting-items").css("display", "none");
-                set.stop().animate({
-                    width : "28"
-                });               
-            }           
-        },
-        getChart: function(){
-            return chartStyle;
-        },
-        setChart: function($chart) {
-            chartStyle = $chart;
-        }
-    }
-}]);
+         var item = $(event.target).parent().parent().find('i');
+         item.removeClass('active');
+         $(event.target).addClass('active');
+         chartStyle = chart;
+         $rootScope.$emit(jsonValue.events.changeChart, chartStyle);
+      },
+
+      getClickedChartStyle : function($target) {
+         if ($target.hasClass('fa-pie-chart')) {
+            return jsonValue.charts.pie;
+         }
+         else if ($target.hasClass('fa-bubble-chart')) {
+            return jsonValue.charts.bubble;
+         }
+         return undefined;
+      },
+
+      showSetting : function() {
+         var set = $(".setting-content");
+         if (set.hasClass('hideContent')) {
+            set.removeClass('hideContent').addClass('showContent');
+            set.find("ul.setting-items").css("display", "block");
+            set.stop().animate({
+               width : "125"
+            });
+
+         }
+         else {
+            set.removeClass('showContent').addClass('hideContent');
+            set.find("ul.setting-items").css("display", "none");
+            set.stop().animate({
+               width : "28"
+            });
+         }
+      },
+      getChart : function() {
+         return chartStyle;
+      },
+      setChart : function($chart) {
+         chartStyle = $chart;
+      }
+   }
+
+   return instance;
+} ]);
