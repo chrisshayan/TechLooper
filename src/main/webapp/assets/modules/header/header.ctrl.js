05e3637ec111aec89c7a8bb0f0d9cbd4b4d8cdf4
Phuong H <phuonghqh@gmail.com>
2014-10-09 18:53:35 +0700

    Use cache to keep track chart render

--- src/main/webapp/assets/modules/header/header.ctrl.js
+++ src/main/webapp/assets/modules/header/header.ctrl.js
@@ -1,23 +1,8 @@
-angular.module("Header").controller("headerController",
-   [ "$scope", "jsonValue", "headerService", "$location",  function($scope, jsonValue, headerService, $location) {
+angular.module("Header").controller("headerController", [ "$scope", "jsonValue", "headerService", "$location", function($scope, jsonValue, headerService, $location) {
 
-   var hashtag = $location.path();
+   var chart = headerService.getChart();
    $('.btn-setting').click(headerService.showSetting);
    $("i[techlooper='chartsMenu']").click(headerService.changeChart);
-   var chartMenu;
-   switch (hashtag) {
-   case jsonValue.charts.pie:
-      chartMenu = $("i[techlooper='chartsMenu'].fa-pie-chart")[0];
-      break;
-   case jsonValue.charts.bubble:
-      chartMenu = $("i[techlooper='chartsMenu'].fa-bubble-chart")[0];
-      break;
-   default:
-   }
-   headerService.changeChart({
-      target : chartMenu,
-      notEmit : true
-   });
-
+   headerService.reflectChart();
    $scope.langKeys = jsonValue.availableLanguageKeys;
 } ]);
