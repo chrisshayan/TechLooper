33f8a1272f96d795ce98d121150a9265c90c6b6b
Phuong H <phuonghqh@gmail.com>
2014-10-22 17:26:59 +0700

    Commit for scrolling page

--- src/main/webapp/assets/modules/job/searchResult.ctrl.js
+++ src/main/webapp/assets/modules/job/searchResult.ctrl.js
@@ -1,3 +1,27 @@
-angular.module('Jobs').controller('searchResultController', ["$scope", "$routeParams", function($scope, $routeParams) {
-  console.log($routeParams.text);
-}]);
+angular.module('Jobs', ['infinite-scroll']).controller('searchResultController',
+  ["$scope", "$routeParams", "connectionFactory", "jsonValue",
+    function ($scope, $routeParams, connectionFactory, jsonValue) {
+      //console.log($routeParams.text);
+      connectionFactory.initialize($scope);
+      $scope.search = {
+        jobs: [],
+        busy: false,
+        nextPage: function () {
+          if (this.busy) return;
+          this.busy = true;
+          console.log("scrolling me");
+          //for (var i = 0; i < 20; i++) {
+          //  $scope.search.jobs.push({title: "sample job " + i});
+          //}
+          connectionFactory.findJobs({"terms": "java", "pageNumber": "1"});
+        }
+      };
+
+
+      $scope.$on(jsonValue.events.foundJobs, function (event, data) {
+        $scope.search.jobs = $scope.search.jobs.concat(data.jobs);
+        $scope.search.busy = false;
+        $scope.$apply();
+      });
+    }]);
+
