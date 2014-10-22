8fc3e3267ffa54f38bcadd694fe01c8d99ec7b87
Phuong H <phuonghqh@gmail.com>
2014-10-22 15:06:49 +0700

    Refactor "jobs/search"

--- src/main/webapp/assets/modules/job/searchForm.ctrl.js
+++ src/main/webapp/assets/modules/job/searchForm.ctrl.js
@@ -1,5 +1,5 @@
 angular.module('Jobs').controller('searchFormController', ["$scope", "$location", function($scope, $location) {
   $scope.abcClick = function() {
-    $location.path("/jobs/" + "acd");
+    $location.path("/jobs/search/" + "acd");
   }
 }]);
