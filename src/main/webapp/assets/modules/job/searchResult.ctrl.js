8400d9686f0e7804f880debc0f144bce2a0b8acc
khoa-nd <ndkhoa.is@gmail.com>
2014-10-23 11:57:00 +0700

    Merge commit

--- src/main/webapp/assets/modules/job/searchResult.ctrl.js
+++ src/main/webapp/assets/modules/job/searchResult.ctrl.js
@@ -1,49 +1,118 @@
-angular.module('Jobs', ['infinite-scroll']).controller('searchResultController',
-  ["$scope", "$routeParams", "connectionFactory", "jsonValue", "searchBoxService",
+angular.module('Jobs').controller('searchResultController',
+  ["$scope", "$routeParams", "connectionFactory", "jsonValue", "searchBoxService", 
     function ($scope, $routeParams, connectionFactory, jsonValue, searchBoxService) {
-    searchBoxService.initializeIntelligent($scope);
+    	searchBoxService.initializeIntelligent($scope);
 
-    function transferKeyWords(){
-        var skills = $routeParams.text.split(" ");
-        console.log(skills);
+ 	function transferKeyWords(){
+    	var skills = $routeParams.text.split(" ");;
+    	console.log(skills)
     }
     transferKeyWords();
+
+// //console.log($routeParams.text);
       connectionFactory.initialize($scope);
       $scope.search = {
         jobs: [],
-        totalPages : 0,
-        pageNumber : 1,
         busy: false,
         nextPage: function () {
           if (this.busy) return;
           this.busy = true;
-          if ($scope.search.pageNumber == 1 || $scope.search.pageNumber <= $scope.search.totalPages) {
-              connectionFactory.findJobs({"terms": $routeParams.text, "pageNumber": $scope.search.pageNumber});
-              console.log("scrolling me : " + $routeParams.text + $scope.search.pageNumber);
-          } else {
-              $scope.search.busy = false;
-              $scope.$apply();
-          }
+          console.log("scrolling me");
+          connectionFactory.findJobs({"terms": $routeParams.text, "pageNumber": "1"});
         }
       };
 
 
       $scope.$on(jsonValue.events.foundJobs, function (event, data) {
-          // count total pages at the first time
-          if ($scope.search.pageNumber == 1) {
-              console.log("Total : " + data.total);
-              console.log("Item/Page : " + data.jobs.length);
-              //TODO : move this total page calculation to util
-              if (data.total % data.jobs.length == 0) {
-                  $scope.search.totalPages = Math.floor(data.total / data.jobs.length);
-              } else {
-                  $scope.search.totalPages = Math.floor(data.total / data.jobs.length) + 1;
-              }
-          }
         $scope.search.jobs = $scope.search.jobs.concat(data.jobs);
         $scope.search.busy = false;
-        $scope.search.pageNumber++;
         $scope.$apply();
       });
-    }]);
+
+  // var pageNumber = 0,
+  //       termKeys = {},
+  //       oldKeys ="",
+  //       newKeys = "",
+  //       totalPage = 0,
+  //       countItems = 0,
+  //       jobItems = '';
+
+  // $scope.$on(jsonValue.events.foundJobs, function(event, data){
+  //       $('.jobs-total').show().find('strong').text(data.total);
+  //       addDataResult(data);
+  //       totalPage = Math.ceil(data.total/countItems);
+  //       $('.loading-paging').fadeOut();
+  //   });
+  //   searchFormController.getKeyWords();
+  //   function showSearchResult(){
+  //       if(!$('.technical-Skill-List').hasClass('hide')){
+  //           var sForm = $('.search-form-container');
+  //           sForm.parent().animate({
+  //               'padding': '20px 40px'
+  //           },{
+  //               duration: '10000',
+  //               easing: 'easeOutQuad'
+  //           });
+
+  //           sForm.animate({
+  //               'width': '100%'
+  //           },{
+  //               duration: '10000',
+  //               easing: 'easeOutQuad'
+  //           });
+  //           $('.technical-Skill-List').addClass('hide');
+  //           $('body').css("background-color","#201d1e");
+  //           $('.search-block').css({
+  //               "position":"inherit",
+  //               "height": "auto"
+  //           });
+  //           $('.search-form-container').css("max-width","100%");
+  //       }
+  //       connectionFactory.findJobs(termKeys);
+  //   }
+  //   function addDataResult(json){
+  //       countItems = 0;
+  //       jobItems ="";
+  //       var video = "";
+  //       $.each(json.jobs, function(index, job){
+  //           if(job.logoUrl ==''){
+  //               companyInfo = job.company;
+  //           }else{
+  //               companyInfo = '<img src="'+ job.logoUrl +'" title="'+ job.company +'">';
+  //           }
+  //           if(job.videoUrl == null){
+  //               video = '';
+  //           }else{
+  //               video = '<p><a class="play-video" href="'+ job.videoUrl +'" data-toggle="modal" data-target="#myModal"><i class="fa fa-play-circle"></i></a></p>';
+  //           }
+  //           jobItems = jobItems + '<div class="job-item"><div class="job-infor"><div class="job-title">';
+  //           jobItems = jobItems + '<a href="'+ job.url +'" target="_blank">'+ job.title +'</a></div>';
+  //           jobItems = jobItems + '<ul><li><i class="fa fa-money"></i>Negotiable</li><li><i class="fa fa-map-marker"></i>'+ job.location +'</li><li><i class="fa fa-male"></i>'+ job.level +'';
+  //           jobItems = jobItems + '</li><li><i class="fa fa-calendar"></i>Posted: '+ job.postedOn +'</li></ul></div>';
+  //           jobItems = jobItems + '<div class="company-logo">'+ companyInfo + video +'</div>';
+  //           jobItems = jobItems + '<div class="view-more"><a href="'+ job.url +'" target="_blank">View More</a></div></div></div>';
+  //           countItems = ++countItems;
+  //       });
+  //       $('.search-result-container-block').append(jobItems);
+  //       $('.play-video').on("click",function(){
+  //           var url = '//www.youtube.com/embed/' + $(this).attr('href').substr($(this).attr('href').indexOf("=") + 1) + '?autoplay=1';
+            
+  //           $('.modal-body').find('iframe').attr('src',url);
+  //       });
+  //   }
+  //    $(window).scroll(function() {
+  //      if($(window).scrollTop() + $(window).height() == $(document).height()) {
+  //          addMoreData();
+  //      }
+  //   });
+  //   function addMoreData(){
+  //       if (pageNumber >= totalPage) {
+  //           $('.loading-paging').addClass('hide');
+  //           return;
+  //       }
+  //       $('.loading-paging').fadeIn();
+  //       termKeys["pageNumber"] = '' + (++pageNumber);
+  //       connectionFactory.findJobs(termKeys);
+  //   }
+}]);
 
