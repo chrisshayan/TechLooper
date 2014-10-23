a8cb4930663417ca6467a827404d7dd93d3a3e4b
khoa-nd <ndkhoa.is@gmail.com>
2014-10-23 10:15:24 +0700

    An arrow in below to show there are more rows

--- src/main/webapp/assets/modules/job/searchResult.ctrl.js
+++ src/main/webapp/assets/modules/job/searchResult.ctrl.js
@@ -1,26 +1,41 @@
 angular.module('Jobs', ['infinite-scroll']).controller('searchResultController',
   ["$scope", "$routeParams", "connectionFactory", "jsonValue",
     function ($scope, $routeParams, connectionFactory, jsonValue) {
-      //console.log($routeParams.text);
       connectionFactory.initialize($scope);
       $scope.search = {
         jobs: [],
+        totalPages : 0,
+        pageNumber : 1,
         busy: false,
         nextPage: function () {
           if (this.busy) return;
           this.busy = true;
-          console.log("scrolling me");
-          //for (var i = 0; i < 20; i++) {
-          //  $scope.search.jobs.push({title: "sample job " + i});
-          //}
-          connectionFactory.findJobs({"terms": "java", "pageNumber": "1"});
+          if ($scope.search.pageNumber == 1 || $scope.search.pageNumber <= $scope.search.totalPages) {
+              connectionFactory.findJobs({"terms": $routeParams.text, "pageNumber": $scope.search.pageNumber});
+              console.log("scrolling me : " + $routeParams.text + $scope.search.pageNumber);
+          } else {
+              $scope.search.busy = false;
+              $scope.$apply();
+          }
         }
       };
 
 
       $scope.$on(jsonValue.events.foundJobs, function (event, data) {
+          // count total pages at the first time
+          if ($scope.search.pageNumber == 1) {
+              console.log("Total : " + data.total);
+              console.log("Item/Page : " + data.jobs.length);
+              //TODO : move this total page calculation to util
+              if (data.total % data.jobs.length == 0) {
+                  $scope.search.totalPages = Math.floor(data.total / data.jobs.length);
+              } else {
+                  $scope.search.totalPages = Math.floor(data.total / data.jobs.length) + 1;
+              }
+          }
         $scope.search.jobs = $scope.search.jobs.concat(data.jobs);
         $scope.search.busy = false;
+        $scope.search.pageNumber++;
         $scope.$apply();
       });
     }]);
