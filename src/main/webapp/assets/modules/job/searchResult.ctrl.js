ffaa9e7de09cb9d189e758c61dd6386cf37ffcb5
Phuong H <phuonghqh@gmail.com>
2014-10-23 15:04:31 +0700

    Commit for selector

--- src/main/webapp/assets/modules/job/searchResult.ctrl.js
+++ src/main/webapp/assets/modules/job/searchResult.ctrl.js
@@ -2,29 +2,27 @@ angular.module('Jobs').controller('searchResultController',
   function ($scope, $routeParams, connectionFactory, jsonValue, searchBoxService) {
     searchBoxService.initializeIntelligent($scope);
 
-    function transferKeyWords() {
-      var skills = $routeParams.text.split(" ");
-      $('.selectator_chosen_items').css('display', 'block');
-      $.each(skills, function (index, skill) {
-        var items = '<div class="selectator_chosen_item"><div class="selectator_chosen_item_left">';
-        items = items + '<img src="images/lg-' + skill + '.png"></div>';
-        items = items + '<div class="selectator_chosen_item_title">' + skill + '</div>';
-        items = items + '<div class="selectator_chosen_item_remove">X</div><div style="clear: both;"></div></div></div>';
-        $('.selectator_chosen_items').append(items);
-        removeItem(skill);
-      });
+    var skills = $routeParams.text.split(" ");
+    $('.selectator_chosen_items').css('display', 'block');
+    $.each(skills, function (index, skill) {
+      //var items = '<div class="selectator_chosen_item"><div class="selectator_chosen_item_left">';
+      //items = items + '<img src="images/lg-' + skill + '.png"></div>';
+      //items = items + '<div class="selectator_chosen_item_title">' + skill + '</div>';
+      //items = items + '<div class="selectator_chosen_item_remove">X</div><div style="clear: both;"></div></div></div>';
+      //$('.selectator_chosen_items').append(items);
+      //removeItem(skill);
+      $("select.termsList option:contains('Java')").attr("selected", "selected");
+      searchBoxService.refresh();
+    });
+    //function removeItem(skill) {
+    //  var list = $('.selectator_options li').find('.selectator_option_title');
+    //  $.each(list, function (index, text) {
+    //    if (angular.lowercase($(this).text()) == skill) {
+    //      $(this).parent().remove();
+    //    }
+    //  });
+    //}
 
-    }
-
-    function removeItem(skill) {
-      var list = $('.selectator_options li').find('.selectator_option_title');
-      $.each(list, function (index, text) {
-        if (angular.lowercase($(this).text()) == skill) {
-          $(this).parent().remove();
-        }
-      });
-    }
-
-    transferKeyWords();
+    //transferKeyWords();
 
 });
