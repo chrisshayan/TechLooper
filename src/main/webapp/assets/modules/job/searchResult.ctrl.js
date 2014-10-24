a2d068363248c1bbf7ebcec328bd90fc02a13706
Phuong H <phuonghqh@gmail.com>
2014-10-24 14:37:33 +0700

    Commit to get new code

--- src/main/webapp/assets/modules/job/searchResult.ctrl.js
+++ src/main/webapp/assets/modules/job/searchResult.ctrl.js
@@ -5,96 +5,96 @@ angular.module('Jobs').controller('searchResultController',
     var skills = $routeParams.text.split(" ");
     $('.selectator_chosen_items').css('display', 'block');
     $.each(skills, function (index, skill) {
-      $("select.termsList option:contains('"+skill+"')").attr("selected", "selected");
+      $("select.termsList option:contains('" + skill + "')").attr("selected", "selected");
       searchBoxService.refresh();
     });
 
-	$('.search-result').click(function () {
-		if (!$('.selectator_chosen_items').is(':empty') || $('input.selectator_input').val() != '') {
-		  keyWords = "";
-		  getKeyWords();
-		  if (keyWords != '') {
-		    $location.path("/jobs/search/" + keyWords);
-		    $scope.$apply();
-		  }
-		}
-	});
-	function getKeyWords() {
-        var $this = $('.selectator_chosen_items').find('.selectator_chosen_item_title');
-        $this.each(function () {
-          keyWords = keyWords + ' ' + $(this).text();
-        });
-        var val = $('input.selectator_input').val();
-        if(val != ''){
-          keyWords = keyWords + ' ' + $('input.selectator_input').val();
+    $('.search-result').click(function () {
+      if (!$('.selectator_chosen_items').is(':empty') || $('input.selectator_input').val() != '') {
+        keyWords = "";
+        getKeyWords();
+        if (keyWords != '') {
+          $location.path("/jobs/search/" + keyWords);
+          $scope.$apply();
         }
-        keyWords = keyWords.substring(1);
-        return keyWords;
       }
+    });
+    function getKeyWords() {
+      var $this = $('.selectator_chosen_items').find('.selectator_chosen_item_title');
+      $this.each(function () {
+        keyWords = keyWords + ' ' + $(this).text();
+      });
+      var val = $('input.selectator_input').val();
+      if (val != '') {
+        keyWords = keyWords + ' ' + $('input.selectator_input').val();
+      }
+      keyWords = keyWords.substring(1);
+      return keyWords;
+    }
 
     connectionFactory.initialize($scope);
+
+    var count = 0;
     $scope.search = {
       jobs: [],
-      totalPages : 0,
-      pageNumber : 1,
+      pageNumber: 0,
       busy: false,
+      total: 1,
       nextPage: function () {
-        if (this.busy) return;
+        if (this.busy) {
+          return;
+        }
         this.busy = true;
-        if ($scope.search.pageNumber == 1 || $scope.search.pageNumber <= $scope.search.totalPages) {
-          connectionFactory.findJobs({"terms": $routeParams.text, "pageNumber": $scope.search.pageNumber});
-        } else {
-          $scope.search.busy = false;
+
+        if (this.jobs.length < this.total) {
+          connectionFactory.findJobs({"terms": $routeParams.text, "pageNumber": ++this.pageNumber});
         }
       }
     };
 
 
     $scope.$on(jsonValue.events.foundJobs, function (event, data) {
-      if ($scope.search.pageNumber == 1) {
-        if (data.total % data.jobs.length == 0) {
-          $scope.search.totalPages = Math.floor(data.total / data.jobs.length);
-        } else {
-          $scope.search.totalPages = Math.floor(data.total / data.jobs.length) + 1;
-        }
-      }
+      var search = $scope.search;
+      search.total = data.total;
       $scope.search.jobs = $scope.search.jobs.concat(data.jobs);
-      $scope.search.busy = false;
-      $scope.search.pageNumber++;
-      
       $scope.$apply();
-      alignLogo();
       resetHeight();
+      alignLogo();
     });
-	$scope.playVideo = function(event) {
-		var url = $(event.currentTarget).attr('ng-url');
-		var myUrl = '//www.youtube.com/embed/'+ getURL(url) + '?autoplay=1';
-		$('.modal-body').find('iframe').attr('src',myUrl);
-	}
-	function getURL(url) {
-	    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-	    var match = url.match(regExp);
-	    if (match && match[2].length == 11) {
-	        return match[2];
-	    } else {
-	        return 'error';
-	    }
-	}
-	$scope.openDetail =function(event){
-		var url = $(event.currentTarget).attr('data-url');
-		window.open(url);
-	}
-  function alignLogo(){
-    var list = $('.job-item'),
-    h = list.height();
-    $('.company-logo').css('height',h);
-    $('.company-video').css('height',h);
-    $('.job-infor').css('height',h);
-  }
-  function resetHeight(){
-    $('.search-block').css({
-       'height':'auto',
-       position: 'inherit'
-    });
-  }
-});
+
+    $scope.playVideo = function (event) {
+      var url = $(event.currentTarget).attr('ng-url');
+      var myUrl = '//www.youtube.com/embed/' + getURL(url) + '?autoplay=1';
+      $('.modal-body').find('iframe').attr('src', myUrl);
+    }
+    function getURL(url) {
+      var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+      var match = url.match(regExp);
+      if (match && match[2].length == 11) {
+        return match[2];
+      }
+      else {
+        return 'error';
+      }
+    }
+
+
+    $scope.openDetail = function (event) {
+      var url = $(event.currentTarget).attr('data-url');
+      window.open(url);
+    }
+    function alignLogo() {
+      var list = $('.job-item'),
+        h = list.height();
+      $('.company-logo').css('height', h);
+      $('.company-video').css('height', h);
+      $('.job-infor').css('height', h);
+    }
+
+    function resetHeight() {
+      $('.search-block').css({
+        'height': 'auto',
+        position: 'inherit'
+      });
+    }
+  });
