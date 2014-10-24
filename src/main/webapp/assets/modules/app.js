25945d0f1356eaf2430b79fe034cfc7cbd7caf8d
Phuong H <phuonghqh@gmail.com>
2014-10-24 21:33:22 +0700

    Commit for new ui-select

--- src/main/webapp/assets/modules/app.js
+++ src/main/webapp/assets/modules/app.js
@@ -3,54 +3,91 @@ angular.module("Bubble", []);
 angular.module("Home", []);
 angular.module("Header", []);
 angular.module("Footer", []);
-angular.module("Chart", [ "Common", "Bubble", "Pie", "Common", "Header"]);
+angular.module("Chart", ["Common", "Bubble", "Pie", "Common", "Header"]);
 angular.module("Jobs", ['infinite-scroll']);
 angular.module("Pie", []);
 angular.module("SearchForm", []);
 
-var techlooper = angular.module("Techlooper", [ "pascalprecht.translate", "ngResource", "ngCookies", "ngRoute", "Bubble", "Pie", "Home", "Header", "Footer", "Common", "Chart", "Jobs", "SearchForm"]);
+var techlooper = angular.module("Techlooper", [
+  "pascalprecht.translate", "ngResource", "ngCookies", "ngRoute", 'ngSanitize', 'ui.select',
+  "Bubble", "Pie", "Home", "Header", "Footer", "Common", "Chart", "Jobs", "SearchForm"
+]);
 
-techlooper.config([ "$routeProvider", "$translateProvider", "$locationProvider", function($routeProvider, $translateProvider, $locationProvider) {
-   $translateProvider.useStaticFilesLoader({
-      prefix : "modules/translation/messages_",
-      suffix : ".json"
-   });
+techlooper.config(["$routeProvider", "$translateProvider", "$locationProvider",
+  function ($routeProvider, $translateProvider, $locationProvider) {
+    //uiSelectConfig.theme = 'select2';
 
-   $translateProvider.registerAvailableLanguageKeys([ 'en', 'vi' ]);
-   $translateProvider.fallbackLanguage("en");
-   $translateProvider.preferredLanguage("en");
-   $translateProvider.useLocalStorage();
-   $translateProvider.use((window.navigator.userLanguage || window.navigator.language).substring(0, 2));
+    $translateProvider.useStaticFilesLoader({
+      prefix: "modules/translation/messages_",
+      suffix: ".json"
+    });
 
-   $routeProvider.when("/bubble-chart", {
-      templateUrl : "modules/bubble-chart/bubble-chart.tpl.html",
-      controller : "chartController"
-   }).when("/pie-chart", {
-      templateUrl : "modules/pie-chart/pie-chart.tpl.html",
-      controller : "chartController"
-   }).when("/jobs/search", {
-      templateUrl : "modules/job/searchForm.tpl.html",
-      controller : "searchFormController"
-   }).when("/jobs/search/:text", {
-      templateUrl : "modules/job/searchResult.tpl.html",
-      controller : "searchResultController"
-   })
-   .otherwise({
-      redirectTo : "/bubble-chart"
-   });
-} ]);
+    $translateProvider.registerAvailableLanguageKeys(['en', 'vi']);
+    $translateProvider.fallbackLanguage("en");
+    $translateProvider.preferredLanguage("en");
+    $translateProvider.useLocalStorage();
+    $translateProvider.use((window.navigator.userLanguage || window.navigator.language).substring(0, 2));
 
-techlooper.directive("header", function() {
-   return {
-      restrict : "A", // This mens that it will be used as an attribute and NOT as an element.
-      replace : true,
-      templateUrl : "modules/header/header.tpl.html",
-      controller : "headerController"
-   }
-}).directive("findjobs", function() {
-   return {
-      restrict : "A", // This mens that it will be used as an attribute and NOT as an element.
-      replace : true,
-      templateUrl : "modules/job/findJobs.tpl.html"
-   }
+    $routeProvider.when("/bubble-chart", {
+      templateUrl: "modules/bubble-chart/bubble-chart.tpl.html",
+      controller: "chartController"
+    }).when("/pie-chart", {
+      templateUrl: "modules/pie-chart/pie-chart.tpl.html",
+      controller: "chartController"
+    }).when("/jobs/search", {
+      templateUrl: "modules/job/searchForm.tpl.html",
+      controller: "searchFormController"
+    }).when("/jobs/search/:text", {
+      templateUrl: "modules/job/searchResult.tpl.html",
+      controller: "searchResultController"
+    })
+      .otherwise({
+        redirectTo: "/bubble-chart"
+      });
+  }]);
+
+techlooper.directive("header", function () {
+  return {
+    restrict: "A", // This mens that it will be used as an attribute and NOT as an element.
+    replace: true,
+    templateUrl: "modules/header/header.tpl.html",
+    controller: "headerController"
+  }
+}).directive("findjobs", function () {
+  return {
+    restrict: "A", // This mens that it will be used as an attribute and NOT as an element.
+    replace: true,
+    templateUrl: "modules/job/findJobs.tpl.html"
+  }
+});
+
+techlooper.filter('propsFilter', function () {
+  return function (items, props) {
+    var out = [];
+    if (angular.isArray(items)) {
+      items.forEach(function (item) {
+        var itemMatches = false;
+
+        var keys = Object.keys(props);
+        for (var i = 0; i < keys.length; i++) {
+          var prop = keys[i];
+          var text = props[prop].toLowerCase();
+          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+            itemMatches = true;
+            break;
+          }
+        }
+
+        if (itemMatches) {
+          out.push(item);
+        }
+      });
+    }
+    else {
+      // Let the output be the input untouched
+      out = items;
+    }
+
+    return out;
+  };
 });
--- src/main/webapp/assets/modules/job/searchResult.ctrl.js
+++ src/main/webapp/assets/modules/job/searchResult.ctrl.js
@@ -42,7 +42,7 @@ angular.module('Jobs').controller('searchResultController',
       busy: false,
       total: 1,
       nextPage: function () {
-        if (this.jobs.length === this.total || this.busy) {
+        if (this.busy || this.jobs.length === this.total) {
           return;
         }
         this.busy = true;
@@ -61,7 +61,7 @@ angular.module('Jobs').controller('searchResultController',
       search.busy = false;
       $scope.$apply();
       alignLogo();
-      $('.search-block').css('min-height',$(window).height());
+      $('.search-block').css('min-height', $(window).height());
       $('.jobs-total strong').text(search.total);
       $('.jobs-total').show();
     });
@@ -92,9 +92,33 @@ angular.module('Jobs').controller('searchResultController',
         h = list.height();
       $('.company-logo').css({
         'height': h,
-        'line-height': h+'px'
+        'line-height': h + 'px'
       });
       $('.company-video').css('height', h);
       $('.job-infor').css('height', h);
     }
+
+
+
+
+    $scope.people = [
+      { name: 'Adam',      email: 'adam@email.com',      age: 12, country: 'United States' },
+      { name: 'Amalie',    email: 'amalie@email.com',    age: 12, country: 'Argentina' },
+      { name: 'Estefanía', email: 'estefania@email.com', age: 21, country: 'Argentina' },
+      { name: 'Adrian',    email: 'adrian@email.com',    age: 21, country: 'Ecuador' },
+      { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30, country: 'Ecuador' },
+      { name: 'Samantha',  email: 'samantha@email.com',  age: 30, country: 'United States' },
+      { name: 'Nicole',    email: 'nicole@email.com',    age: 43, country: 'Colombia' },
+      { name: 'Natasha',   email: 'natasha@email.com',   age: 54, country: 'Ecuador' },
+      { name: 'Michael',   email: 'michael@email.com',   age: 15, country: 'Colombia' },
+      { name: 'Nicolás',   email: 'nicolas@email.com',    age: 43, country: 'Colombia' }
+    ];
+
+    $scope.availableColors = ['Red','Green','Blue','Yellow','Magenta','Maroon','Umbra','Turquoise'];
+
+    $scope.multipleDemo = {};
+    $scope.multipleDemo.colors = ['Blue','Red'];
+    $scope.multipleDemo.selectedPeople = [$scope.people[5], $scope.people[4]];
+    $scope.multipleDemo.selectedPeopleWithGroupBy = [$scope.people[8], $scope.people[6]];
+    $scope.multipleDemo.selectedPeopleSimple = ['samantha@email.com','wladimir@email.com'];
   });
