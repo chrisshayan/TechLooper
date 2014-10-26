4cdc5e8fff7437f7e2d8c667f74f8de1bd241ddb
Phuong H <phuonghqh@gmail.com>
2014-10-26 20:35:12 +0700

    Remove some comments

--- src/main/webapp/assets/modules/app.js
+++ src/main/webapp/assets/modules/app.js
@@ -40,10 +40,9 @@ techlooper.config(["$routeProvider", "$translateProvider", "$locationProvider",
     }).when("/jobs/search/:text", {
       templateUrl: "modules/job/searchResult.tpl.html",
       controller: "searchResultController"
-    })
-      .otherwise({
-        redirectTo: "/bubble-chart"
-      });
+    }).otherwise({
+      redirectTo: "/bubble-chart"
+    });
   }]);
 
 techlooper.directive("header", function () {
@@ -60,34 +59,3 @@ techlooper.directive("header", function () {
     templateUrl: "modules/job/findJobs.tpl.html"
   }
 });
-
-//techlooper.filter('propsFilter', function () {
-//  return function (items, props) {
-//    var out = [];
-//    if (angular.isArray(items)) {
-//      items.forEach(function (item) {
-//        var itemMatches = false;
-//
-//        var keys = Object.keys(props);
-//        for (var i = 0; i < keys.length; i++) {
-//          var prop = keys[i];
-//          var text = props[prop].toLowerCase();
-//          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-//            itemMatches = true;
-//            break;
-//          }
-//        }
-//
-//        if (itemMatches) {
-//          out.push(item);
-//        }
-//      });
-//    }
-//    else {
-//      // Let the output be the input untouched
-//      out = items;
-//    }
-//
-//    return out;
-//  };
-//});
