72d9a7a1e8f1fe402847088b156ec25eea91761d
khoa-nd <ndkhoa.is@gmail.com>
2014-10-30 17:28:24 +0700

    Refactor JobController, convertToJobSearchResponse method using Stream map()

--- src/main/java/com/techlooper/controller/JobsController.java
+++ src/main/java/com/techlooper/controller/JobsController.java
@@ -11,90 +11,82 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 public class JobsController {
 
-  @Resource
-  private JobStatisticService vietnamWorksJobStatisticService;
+    @Resource
+    private JobStatisticService vietnamWorksJobStatisticService;
 
-  @Resource
-  private JobSearchService vietnamWorksJobSearchService;
+    @Resource
+    private JobSearchService vietnamWorksJobSearchService;
 
-  @Resource
-  private SimpMessagingTemplate messagingTemplate;
+    @Resource
+    private SimpMessagingTemplate messagingTemplate;
 
-  @Value("${vnw.api.configuration.category.it.software.en}")
-  private String category;
-
-
-  @SendTo("/topic/jobs/search")
-  @MessageMapping("/jobs/search")
-  public JobSearchResponse searchJobs(JobSearchRequest searchJobsRequest) {
-    final VNWJobSearchRequest vnwJobSearchRequest = convertToVNWJobSearchRequest(searchJobsRequest);
-    final VNWJobSearchResponse vnwJobSearchResponse = vietnamWorksJobSearchService.searchJob(vnwJobSearchRequest);
-    return convertToJobSearchResponse(vnwJobSearchResponse);
-  }
+    @Value("${vnw.api.configuration.category.it.software.en}")
+    private String category;
 
 
-  @Scheduled(cron = "${scheduled.cron}")
-  public void countTechnicalJobs() {
-    Arrays.stream(TechnicalTermEnum.values()).forEach(term -> {
-      messagingTemplate.convertAndSend("/topic/jobs/term/" + term.name(), new JobStatisticResponse.Builder()
-        .withCount(vietnamWorksJobStatisticService.count(term)).build());
-    });
-  }
+    @SendTo("/topic/jobs/search")
+    @MessageMapping("/jobs/search")
+    public JobSearchResponse searchJobs(JobSearchRequest searchJobsRequest) {
+        final VNWJobSearchRequest vnwJobSearchRequest = convertToVNWJobSearchRequest(searchJobsRequest);
+        final VNWJobSearchResponse vnwJobSearchResponse = vietnamWorksJobSearchService.searchJob(vnwJobSearchRequest);
+        return convertToJobSearchResponse(vnwJobSearchResponse);
+    }
 
-  /**
-   * need to think a way how fetch TechnicalTerms dynamic, we might rework on
-   * business model
-   */
-  @SendTo("/topic/jobs/terms")
-  @MessageMapping("/jobs/terms")
-  public List<TechnicalTermResponse> countTechnicalTerms() {
-    List<TechnicalTermResponse> terms = new LinkedList<TechnicalTermResponse>();
-    Arrays.stream(TechnicalTermEnum.values()).forEach(term -> {
-      terms.add(new TechnicalTermResponse.Builder().withTerm(term)
-        .withCount(vietnamWorksJobStatisticService.count(term)).build());
-    });
-    return terms;
-  }
 
-  @MessageMapping("/jobs")
-  public void countTechnicalJobs(JobStatisticRequest request) {
-    messagingTemplate.convertAndSend(
-      "/topic/jobs/" + request.getTerm().toLowerCase(),
-      new JobStatisticResponse.Builder().withCount(
-        vietnamWorksJobStatisticService.count(TechnicalTermEnum.valueOf(request.getTerm().toUpperCase())))
-        .build());
-  }
+    @Scheduled(cron = "${scheduled.cron}")
+    public void countTechnicalJobs() {
+        Arrays.stream(TechnicalTermEnum.values()).forEach(term -> {
+            messagingTemplate.convertAndSend("/topic/jobs/term/" + term.name(), new JobStatisticResponse.Builder()
+                    .withCount(vietnamWorksJobStatisticService.count(term)).build());
+        });
+    }
 
-  private VNWJobSearchRequest convertToVNWJobSearchRequest(JobSearchRequest jobSearchRequest) {
-    return new VNWJobSearchRequest(
-      jobSearchRequest.getTerms(),
-      category,
-      jobSearchRequest.getPageNumber()
-    );
-  }
+    /**
+     * need to think a way how fetch TechnicalTerms dynamic, we might rework on
+     * business model
+     */
+    @SendTo("/topic/jobs/terms")
+    @MessageMapping("/jobs/terms")
+    public List<TechnicalTermResponse> countTechnicalTerms() {
+        List<TechnicalTermResponse> terms = new LinkedList<TechnicalTermResponse>();
+        Arrays.stream(TechnicalTermEnum.values()).forEach(term -> {
+            terms.add(new TechnicalTermResponse.Builder().withTerm(term)
+                    .withCount(vietnamWorksJobStatisticService.count(term)).build());
+        });
+        return terms;
+    }
 
-  private JobSearchResponse convertToJobSearchResponse(VNWJobSearchResponse vnwJobSearchResponse) {
-    final JobSearchResponse jobSearchResponse = new JobSearchResponse();
-    jobSearchResponse.setTotal(vnwJobSearchResponse.getData().getTotal());
-    final Set<JobResponse> jobs = new HashSet<>();
-    vnwJobSearchResponse.getData().getJobs().stream().forEach(item -> {
-      final JobResponse.Builder builder = new JobResponse.Builder();
-      jobs.add(builder.withCompany(item.getCompany())
-        .withLevel(item.getLevel())
-        .withLocation(item.getLocation())
-        .withLogoUrl(item.getLogoUrl())
-        .withPostedOn(item.getPostedOn())
-        .withTitle(item.getTitle())
-        .withUrl(item.getUrl())
-        .withVideoUrl(item.getVideoUrl())
-        .build());
-    });
-    jobSearchResponse.setJobs(jobs);
-    return jobSearchResponse;
-  }
+    @MessageMapping("/jobs")
+    public void countTechnicalJobs(JobStatisticRequest request) {
+        messagingTemplate.convertAndSend(
+                "/topic/jobs/" + request.getTerm().toLowerCase(),
+                new JobStatisticResponse.Builder().withCount(
+                        vietnamWorksJobStatisticService.count(TechnicalTermEnum.valueOf(request.getTerm().toUpperCase())))
+                        .build());
+    }
+
+    private VNWJobSearchRequest convertToVNWJobSearchRequest(JobSearchRequest jobSearchRequest) {
+        return new VNWJobSearchRequest(
+                jobSearchRequest.getTerms(),
+                category,
+                jobSearchRequest.getPageNumber()
+        );
+    }
+
+    private JobSearchResponse convertToJobSearchResponse(VNWJobSearchResponse vnwJobSearchResponse) {
+        final JobSearchResponse jobSearchResponse = new JobSearchResponse();
+        jobSearchResponse.setTotal(vnwJobSearchResponse.getData().getTotal());
+        final Stream<VNWJobSearchResponseDataItem> responseDataItemStream = vnwJobSearchResponse.getData().getJobs().stream();
+        jobSearchResponse.setJobs(responseDataItemStream.map(item -> item.toJobResponse()).collect(Collectors.toSet()));
+        return jobSearchResponse;
+    }
 }
