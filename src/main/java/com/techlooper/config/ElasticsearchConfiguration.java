cea90fd1d4a00ba3e5b0dced8206e83a3981984e
hamedshayan@gmail.com <Hamed9243>
2014-11-10 10:10:34 +0700

    Fixed code based on Java 8

--- src/main/java/com/techlooper/config/ElasticsearchConfiguration.java
+++ src/main/java/com/techlooper/config/ElasticsearchConfiguration.java
@@ -48,14 +48,12 @@ public class ElasticsearchConfiguration {
     public TechnicalSkillEnumMap technicalSkillEnumMap() {
         TechnicalSkillEnumMap technicalSkillEnumMap = new TechnicalSkillEnumMap();
         Stream.of(TechnicalTermEnum.values()).forEach(term -> {
-//            if (TechnicalTermEnum.EMPTY != term) {
-            String termKey = environment.getProperty(term.value().replaceAll(" ", "_"));
+            final String termKey = environment.getProperty(term.value().replaceAll(" ", "_"));
             Optional<String> skillOptional = Optional.ofNullable(termKey);
             if (skillOptional.isPresent()) {
                 String[] skills = StringUtils.split(skillOptional.get(), ',');
                 technicalSkillEnumMap.put(term, Arrays.asList(skills));
             }
-//            }
         });
         return technicalSkillEnumMap;
     }
