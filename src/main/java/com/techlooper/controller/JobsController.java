7dd3f8c82e93f659b99b6e44c14a00e186feb495
Phuong H <phuonghqh@gmail.com>
2014-11-11 16:29:55 +0700

    Replace PeriodEnum by HistogramEnum

--- src/main/java/com/techlooper/controller/JobsController.java
+++ src/main/java/com/techlooper/controller/JobsController.java
@@ -18,104 +18,103 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static com.techlooper.model.PeriodEnum.lookUp;
-
 @Controller
 public class JobsController {
 
-    @Resource
-    private JobStatisticService vietnamWorksJobStatisticService;
+  @Resource
+  private JobStatisticService vietnamWorksJobStatisticService;
 
-    @Resource
-    private JobSearchService vietnamWorksJobSearchService;
+  @Resource
+  private JobSearchService vietnamWorksJobSearchService;
 
-    @Resource
-    private SimpMessagingTemplate messagingTemplate;
+  @Resource
+  private SimpMessagingTemplate messagingTemplate;
 
-    @Resource
-    private TechnicalSkillEnumMap technicalSkillEnumMap;
+  @Resource
+  private TechnicalSkillEnumMap technicalSkillEnumMap;
 
-    @Value("${vnw.api.configuration.category.it.software.en}")
-    private String category;
-
-
-    @SendTo("/topic/jobs/search")
-    @MessageMapping("/jobs/search")
-    public JobSearchResponse searchJobs(JobSearchRequest searchJobsRequest) {
-        final VNWJobSearchRequest vnwJobSearchRequest = convertToVNWJobSearchRequest(searchJobsRequest);
-        final VNWJobSearchResponse vnwJobSearchResponse = vietnamWorksJobSearchService.searchJob(vnwJobSearchRequest);
-        return convertToJobSearchResponse(vnwJobSearchResponse);
-    }
+  @Value("${vnw.api.configuration.category.it.software.en}")
+  private String category;
 
 
-    @Scheduled(cron = "${scheduled.cron}")
-    public void countTechnicalJobs() {
-        Arrays.stream(TechnicalTermEnum.values()).forEach(term -> {
-            messagingTemplate.convertAndSend("/topic/jobs/term/" + term.name(), new JobStatisticResponse.Builder()
-                    .withCount(vietnamWorksJobStatisticService.count(term)).build());
-        });
-    }
+  @SendTo("/topic/jobs/search")
+  @MessageMapping("/jobs/search")
+  public JobSearchResponse searchJobs(JobSearchRequest searchJobsRequest) {
+    final VNWJobSearchRequest vnwJobSearchRequest = convertToVNWJobSearchRequest(searchJobsRequest);
+    final VNWJobSearchResponse vnwJobSearchResponse = vietnamWorksJobSearchService.searchJob(vnwJobSearchRequest);
+    return convertToJobSearchResponse(vnwJobSearchResponse);
+  }
 
-    /**
-     * need to think a way how fetch TechnicalTerms dynamic, we might rework on
-     * business model
-     */
-    @SendTo("/topic/jobs/terms")
-    @MessageMapping("/jobs/terms")
-    public List<TechnicalTermResponse> countTechnicalTerms() {
-        List<TechnicalTermResponse> terms = new LinkedList<>();
-        Arrays.stream(TechnicalTermEnum.values()).forEach(term -> {
+
+  @Scheduled(cron = "${scheduled.cron}")
+  public void countTechnicalJobs() {
+    Arrays.stream(TechnicalTermEnum.values()).forEach(term -> {
+      messagingTemplate.convertAndSend("/topic/jobs/term/" + term.name(), new JobStatisticResponse.Builder()
+        .withCount(vietnamWorksJobStatisticService.count(term)).build());
+    });
+  }
+
+  /**
+   * need to think a way how fetch TechnicalTerms dynamic, we might rework on
+   * business model
+   */
+  @SendTo("/topic/jobs/terms")
+  @MessageMapping("/jobs/terms")
+  public List<TechnicalTermResponse> countTechnicalTerms() {
+    List<TechnicalTermResponse> terms = new LinkedList<>();
+    Arrays.stream(TechnicalTermEnum.values()).forEach(term -> {
 //            if (TechnicalTermEnum.EMPTY != term) {
-            terms.add(new TechnicalTermResponse.Builder().withTerm(term)
-                    .withCount(vietnamWorksJobStatisticService.count(term)).build());
+      terms.add(new TechnicalTermResponse.Builder().withTerm(term)
+        .withCount(vietnamWorksJobStatisticService.count(term)).build());
 //            }
-        });
-        return terms;
+    });
+    return terms;
+  }
+
+  @MessageMapping("/jobs")
+  public void countTechnicalJobs(JobStatisticRequest request) {
+    messagingTemplate.convertAndSend(
+      "/topic/jobs/" + request.getTerm().toLowerCase(),
+      new JobStatisticResponse.Builder().withCount(
+        vietnamWorksJobStatisticService.count(TechnicalTermEnum.valueOf(request.getTerm().toUpperCase())))
+        .build());
+  }
+
+  /**
+   * need to think a way how fetch TechnicalTerms dynamic, we might rework on
+   * business model
+   */
+  @SendTo("/topic/analytics/skill")
+  @MessageMapping("/analytics/skill")
+  public SkillStatisticResponse countTechnicalSkillByTerm(SkillStatisticRequest skillStatisticRequest) {
+    Optional<TechnicalTermEnum> termOptional = Optional.ofNullable(
+      TechnicalTermEnum.lookUp(skillStatisticRequest.getTerm().toUpperCase()));
+    TechnicalTermEnum term = termOptional.get();
+
+    if (termOptional.isPresent() && technicalSkillEnumMap.containsKey(term)) {
+//            PeriodEnum period = lookUp(skillStatisticRequest.getPeriod().toUpperCase());
+      // TODO might support period later
+      return vietnamWorksJobStatisticService.countJobsBySkill(term, HistogramEnum.TWO_WEEK, HistogramEnum.THIRTY_DAY);
     }
 
-    @MessageMapping("/jobs")
-    public void countTechnicalJobs(JobStatisticRequest request) {
-        messagingTemplate.convertAndSend(
-                "/topic/jobs/" + request.getTerm().toLowerCase(),
-                new JobStatisticResponse.Builder().withCount(
-                        vietnamWorksJobStatisticService.count(TechnicalTermEnum.valueOf(request.getTerm().toUpperCase())))
-                        .build());
-    }
+    SkillStatisticResponse defaultObject = SkillStatisticResponse.getDefaultObject();
+    defaultObject.setJobTerm(skillStatisticRequest.getTerm());
+    return SkillStatisticResponse.getDefaultObject();
+  }
 
-    /**
-     * need to think a way how fetch TechnicalTerms dynamic, we might rework on
-     * business model
-     */
-    @SendTo("/topic/analytics/skill")
-    @MessageMapping("/analytics/skill")
-    public SkillStatisticResponse countTechnicalSkillByTerm(SkillStatisticRequest skillStatisticRequest) {
-        Optional<TechnicalTermEnum> termOptional = Optional.ofNullable(
-                TechnicalTermEnum.lookUp(skillStatisticRequest.getTerm().toUpperCase()));
-        TechnicalTermEnum term = termOptional.get();
+  private VNWJobSearchRequest convertToVNWJobSearchRequest(JobSearchRequest jobSearchRequest) {
+    return new VNWJobSearchRequest(
+      jobSearchRequest.getTerms(),
+      category,
+      jobSearchRequest.getPageNumber()
+    );
+  }
 
-        if (termOptional.isPresent() && technicalSkillEnumMap.containsKey(term)) {
-            PeriodEnum period = lookUp(skillStatisticRequest.getPeriod().toUpperCase());
-            return vietnamWorksJobStatisticService.countJobsBySkill(term, period);
-        }
-
-        SkillStatisticResponse defaultObject = SkillStatisticResponse.getDefaultObject();
-        defaultObject.setJobTerm(skillStatisticRequest.getTerm());
-        return SkillStatisticResponse.getDefaultObject();
-    }
-
-    private VNWJobSearchRequest convertToVNWJobSearchRequest(JobSearchRequest jobSearchRequest) {
-        return new VNWJobSearchRequest(
-                jobSearchRequest.getTerms(),
-                category,
-                jobSearchRequest.getPageNumber()
-        );
-    }
-
-    private JobSearchResponse convertToJobSearchResponse(VNWJobSearchResponse vnwJobSearchResponse) {
-        final JobSearchResponse jobSearchResponse = new JobSearchResponse();
-        jobSearchResponse.setTotal(vnwJobSearchResponse.getData().getTotal());
-        final Stream<VNWJobSearchResponseDataItem> responseDataItemStream = vnwJobSearchResponse.getData().getJobs().stream();
-        jobSearchResponse.setJobs(responseDataItemStream.map(VNWJobSearchResponseDataItem::toJobResponse).collect(Collectors.toSet()));
-        return jobSearchResponse;
-    }
+  private JobSearchResponse convertToJobSearchResponse(VNWJobSearchResponse vnwJobSearchResponse) {
+    final JobSearchResponse jobSearchResponse = new JobSearchResponse();
+    jobSearchResponse.setTotal(vnwJobSearchResponse.getData().getTotal());
+    final Stream<VNWJobSearchResponseDataItem> responseDataItemStream = vnwJobSearchResponse.getData().getJobs().stream();
+    jobSearchResponse.setJobs(responseDataItemStream.map(VNWJobSearchResponseDataItem::toJobResponse).collect(Collectors.toSet()));
+    return jobSearchResponse;
+  }
 }
