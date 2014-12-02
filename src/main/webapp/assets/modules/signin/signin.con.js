f8cb41caffa8542e65c6b8dcef09d39851b8f554
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-12-02 13:51:36 +0700

    add signin popup

--- src/main/webapp/assets/modules/signin/signin.con.js
+++ src/main/webapp/assets/modules/signin/signin.con.js
@@ -1,9 +1,18 @@
-angular.module('SignIn').controller('signInController', function (jsonValue, utils, $scope, shortcutFactory) {
+angular.module('SignIn').controller('signInController', function ($location, jsonValue, utils, $scope, shortcutFactory) {
 	
 	$scope.accounts = jsonValue.accountSignin;
 	
 	$('.signin-accounts').parallax();
 	
-    $('.btn-close').click(function(){shortcutFactory.trigger('esc');});
-    $('.btn-logo').click(function(){shortcutFactory.trigger('esc');});
+  $('.btn-close').click(function(){shortcutFactory.trigger('esc');});
+  $('.btn-logo').click(function(){shortcutFactory.trigger('esc');});
+
+  $(".signin-popup-close").on('click', function() {
+    $('#signin-form').modal('hide');
+  });
+
+  $('.sign-successful').on('click', function(){
+  	$location.path('/register');
+  	$scope.$apply();
+  });
 });
