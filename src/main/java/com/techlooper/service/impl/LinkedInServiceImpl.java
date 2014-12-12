146e2aeb4ff2aec88dbfe96c06f4270f4729d966
Phuong H <phuonghqh@gmail.com>
2014-12-12 17:08:58 +0700

    Allow to persist LinkedIn profile to DB

--- src/main/java/com/techlooper/service/impl/LinkedInServiceImpl.java
+++ src/main/java/com/techlooper/service/impl/LinkedInServiceImpl.java
@@ -1,10 +1,13 @@
 package com.techlooper.service.impl;
 
+import com.techlooper.entity.LinkedInProfile;
 import com.techlooper.entity.UserEntity;
 import com.techlooper.model.SocialProvider;
 import com.techlooper.repository.JsonConfigRepository;
 import com.techlooper.repository.couchbase.UserRepository;
 import com.techlooper.service.SocialService;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.linkedin.api.LinkedIn;
 import org.springframework.social.linkedin.api.LinkedInProfileFull;
@@ -14,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.inject.Inject;
+import java.util.Optional;
 
 import static com.techlooper.model.SocialProvider.LINKEDIN;
 
@@ -32,6 +36,9 @@ public class LinkedInServiceImpl implements SocialService {
   @Resource
   private UserRepository userRepository;
 
+  @Resource
+  private Mapper dozerBeanMapper;
+
   @Inject
   public LinkedInServiceImpl(JsonConfigRepository jsonConfigRepository) {
     redirectUri = jsonConfigRepository.getSocialConfig().stream()
@@ -39,28 +46,22 @@ public class LinkedInServiceImpl implements SocialService {
   }
 
   public AccessGrant getAccessGrant(String accessCode) {
-//    LinkedInConnectionFactory factory = (LinkedInConnectionFactory) linkedInConnectionFactory;
     return linkedInConnectionFactory.getOAuthOperations().exchangeForAccess(accessCode, redirectUri, null);
-//    Connection<LinkedIn> connection = factory.createConnection(accessGrant);
-//    UserEntity userEntity = new UserEntity("7");
-//    LinkedInProfileFull profile = connection.getApi().profileOperations().getUserProfileFull();
-//    userEntity.setLinkedInProfileFull(profile);
-
-//    userRepository.save(userEntity);
-//    userRepository.findOne("7")
-//    System.out.println(profile);
-//    Connection<LinkedIn> connection = fac.createConnection(accessGrant);
-//    connection.getKey();
-//    AccessGrant accessGrant = factory.getOAuthOperations().exchangeForAccess(accessCode, redirectUri, null);
-//    SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(connectionKey.getProviderUserId(), null, null));
-//    return accessGrant.getAccessToken();
   }
 
   public void persistProfile(AccessGrant accessGrant) {
     Connection<LinkedIn> connection = linkedInConnectionFactory.createConnection(accessGrant);
     LinkedInProfileFull profile = connection.getApi().profileOperations().getUserProfileFull();
-    UserEntity.Builder builder = new UserEntity.Builder().withLoginSource(SocialProvider.LINKEDIN);
-    builder.withFirstName(profile.getFirstName()).withLastName(profile.getLastName()).withEmailAddress(profile.getEmailAddress());
-    userRepository.save(builder.build());
+    LinkedInProfile profileEntity = dozerBeanMapper.map(profile, LinkedInProfile.class);
+    UserEntity userEntity = Optional.ofNullable(userRepository.findOne(profile.getEmailAddress())).orElse(new UserEntity());
+    UserEntity.Builder builder = UserEntity.Builder.get(userEntity).withProfile(SocialProvider.LINKEDIN, profileEntity);
+    if (!Optional.ofNullable(userEntity.getEmailAddress()).isPresent()) {
+      userRepository.save(builder.withId(profile.getEmailAddress())
+        .withLoginSource(SocialProvider.LINKEDIN)
+        .withFirstName(profile.getFirstName())
+        .withLastName(profile.getLastName())
+        .withEmailAddress(profile.getEmailAddress())
+        .build());
+    }
   }
 }
