689eb37720e88c5e2c96cd7548c6376cdd2e9b5e
Phuong H <phuonghqh@gmail.com>
2014-12-15 09:13:02 +0700

    Remove some log4j and add @Repository to UserRepo bean (help IDEA determine spring bean)

--- src/main/java/com/techlooper/repository/couchbase/UserRepository.java
+++ src/main/java/com/techlooper/repository/couchbase/UserRepository.java
@@ -4,6 +4,7 @@ import com.couchbase.client.protocol.views.Query;
 import com.techlooper.entity.UserEntity;
 import org.springframework.data.couchbase.core.view.View;
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by NguyenDangKhoa on 12/10/14.
