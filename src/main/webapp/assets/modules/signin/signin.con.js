499aa60a6e0913c7e758e5c9f36a30519b7b3212
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-12-18 10:21:21 +0700

    update code

--- src/main/webapp/assets/modules/signin/signin.con.js
+++ src/main/webapp/assets/modules/signin/signin.con.js
@@ -1,9 +1,6 @@
-angular.module('SignIn').controller('signInController', function ($location, jsonValue, utils, $scope, signInService, shortcutFactory) {
+angular.module('SignIn').controller('signInController', function ($location, jsonValue, utils, $scope, signInService) {
   $scope.authSource = jsonValue.authSource;
   //utils.sendNotification(jsonValue.notifications.switchScope, $scope);
   //utils.sendNotification(jsonValue.notifications.gotData);
   $scope.openOathDialog = signInService.openOathDialog;
-
-  $('.btn-close').click(function () {shortcutFactory.trigger('esc');});
-  $('.btn-logo').click(function () {shortcutFactory.trigger('esc');});
 });
