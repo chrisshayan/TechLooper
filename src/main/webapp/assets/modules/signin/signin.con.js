d6dfb5764ff97469343ae7675a6cc9862395a91f
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2014-12-18 10:19:03 +0700

    make esc for sign in page

--- src/main/webapp/assets/modules/signin/signin.con.js
+++ src/main/webapp/assets/modules/signin/signin.con.js
@@ -1,6 +1,9 @@
-angular.module('SignIn').controller('signInController', function ($location, jsonValue, utils, $scope, signInService) {
+angular.module('SignIn').controller('signInController', function ($location, jsonValue, utils, $scope, signInService, shortcutFactory) {
   $scope.authSource = jsonValue.authSource;
   //utils.sendNotification(jsonValue.notifications.switchScope, $scope);
   //utils.sendNotification(jsonValue.notifications.gotData);
   $scope.openOathDialog = signInService.openOathDialog;
+
+  $('.btn-close').click(function () {shortcutFactory.trigger('esc');});
+  $('.btn-logo').click(function () {shortcutFactory.trigger('esc');});
 });
