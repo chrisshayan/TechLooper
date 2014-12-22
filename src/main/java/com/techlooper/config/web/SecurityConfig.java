88332715ac95904b2031e1c1412f06cd5a055085
Phuong H <phuonghqh@gmail.com>
2014-12-22 16:32:00 +0700

    Protect "register" page

--- src/main/java/com/techlooper/config/web/SecurityConfig.java
+++ src/main/java/com/techlooper/config/web/SecurityConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * Created by NguyenDangKhoa on 11/27/14.
@@ -16,16 +18,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private static final String AUTHORIZATION_ROLE_ADMIN = "administrator";
+//  private static final String AUTHORIZATION_ROLE_ADMIN = "administrator";
 
-  @Value("${admin.path}")
-  private String adminPath;
-
-  @Value("${admin.username}")
-  private String username;
-
-  @Value("${admin.password}")
-  private String password;
+//  @Value("${admin.path}")
+//  private String adminPath;
+//
+//  @Value("${admin.username}")
+//  private String username;
+//
+//  @Value("${admin.password}")
+//  private String password;
 
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //    auth.inMemoryAuthentication().withUser(username).password(password).roles(AUTHORIZATION_ROLE_ADMIN);
@@ -33,13 +35,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   }
 
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().permitAll();
+//    http.authorizeRequests().anyRequest().permitAll();
 //    http
 //      .authorizeRequests()
 //        .antMatchers(adminPath).hasRole(AUTHORIZATION_ROLE_ADMIN)
 //      .and().httpBasic();
-    http.csrf().disable();
+    http.csrf().disable()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and().authorizeRequests().antMatchers("/modules/register/**").hasRole("USER")
+      .and().authorizeRequests().anyRequest().permitAll();
   }
+
+//  public void configure(WebSecurity web) throws Exception {
+//    web.ignoring().antMatchers("/images/**", "/css/**", "/generate-resources/**", "/modules/**", "/auth/**", "/getSocialConfig");
+//  }
 
   @Bean
   public AuthenticationProvider authenticationProvider() {
