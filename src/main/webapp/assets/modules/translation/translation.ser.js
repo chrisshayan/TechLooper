bc887ab2868bb8fced36e4348fa8364599628c47
Phuong H <phuonghqh@gmail.com>
2015-01-08 10:07:58 +0700

    Resovle confilct

--- src/main/webapp/assets/modules/translation/translation.ser.js
+++ src/main/webapp/assets/modules/translation/translation.ser.js
@@ -4,15 +4,9 @@ angular.module('Common').factory('translationService', function ($translate, uti
   var instance = {
     initialize: function ($scope) {
       scope = $scope;
-<<<<<<< HEAD
-      $(".langKey").on('click', function () {
-        var nextLang = instance.getNextLanguage();
-        $translate.use(nextLang);
-=======
       $(".langKey").click(function () {
         var lang = instance.getNextLanguage();
         $translate.use(lang).then(function () {utils.sendNotification(jsonValue.notifications.changeLang, lang);});
->>>>>>> 56adb09f4f6fd3d0cd757892496c5f28dc1e1e8a
         utils.apply();
 
         var nextLang = instance.getNextLanguage(lang);
