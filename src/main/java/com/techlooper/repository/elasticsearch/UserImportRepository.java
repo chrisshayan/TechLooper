791568da025abe91b9d6405f928df07b2c1dd39c
khoa-nd <ndkhoa.is@gmail.com>
2015-01-27 10:06:50 +0700

    Reformat code

--- src/main/java/com/techlooper/repository/elasticsearch/UserImportRepository.java
+++ src/main/java/com/techlooper/repository/elasticsearch/UserImportRepository.java
@@ -1,14 +1,10 @@
 package com.techlooper.repository.elasticsearch;
 
-import com.techlooper.entity.JobEntity;
-import com.techlooper.entity.UserEntity;
 import com.techlooper.entity.UserImportEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 /**
- * Created by chrisshayan on 7/11/14.
+ * Created by khoa-nd on 27/01/15.
  */
 public interface UserImportRepository extends ElasticsearchRepository<UserImportEntity, String> {
 }
