786161b5dc8bba3e03ac9db2543538733a268261
Phuong H <phuonghqh@gmail.com>
2015-03-12 15:36:05 +0700

    It professional link to bubble chart

--- src/main/webapp/assets/modules/talent-search/header/header.con.js
+++ src/main/webapp/assets/modules/talent-search/header/header.con.js
@@ -1,8 +1,12 @@
-techlooper.controller("tsHeaderController", function ($scope, tsHeaderService, $timeout) {
+techlooper.controller("tsHeaderController", function ($scope, tsHeaderService, $timeout, $location, jsonValue) {
   tsHeaderService.init();
   $timeout(function(){
     tsHeaderService.menuAnimate();
     tsHeaderService.langManager();
     tsHeaderService.settingLang();
   }, 100);
+
+  $scope.itProfessionalClick = function() {
+    $location.path(jsonValue.routerUris.bubble);
+  }
 });
