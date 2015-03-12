44082f3c8ba7dc5d8a986d6a045c33b8be2581ef
johnsonpham <S-MIS-TRINHPT@Trinhs-MacBook-Pro.local>
2015-03-12 16:43:04 +0700

    write angular js for talent search result page

--- src/main/webapp/assets/modules/talent-search/header/header.con.js
+++ src/main/webapp/assets/modules/talent-search/header/header.con.js
@@ -1,8 +1,10 @@
 techlooper.controller("tsHeaderController", function ($scope, tsHeaderService, $timeout) {
   tsHeaderService.init();
   $timeout(function(){
-    if($('.search-form-block').length > 0){
+    if($('.main-banner').length > 0){
       tsHeaderService.menuAnimate();
+    }else{
+      $('header').addClass('changed');
     }
     tsHeaderService.langManager();
     tsHeaderService.settingLang();
