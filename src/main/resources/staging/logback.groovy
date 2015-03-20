c86503461bf7abb90ea6951368df4c3541fb44f5
Phuong H <phuonghqh@gmail.com>
2015-03-20 18:20:48 +0700

    Add logback to staging

---
+++ src/main/resources/staging/logback.groovy
@@ -0,0 +1,35 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.rolling.FixedWindowRollingPolicy
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.SizeBasedTriggeringPolicy
+
+import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.ERROR
+
+scan()
+
+appender("CONSOLE", ConsoleAppender) {
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d{dd-MM-yyyy HH:mm:ss.SSS} %p [%t] %c{1}: %m%n"
+  }
+}
+
+appender("CRONF", RollingFileAppender) {
+  file = "techlooper_cron_.log"
+  rollingPolicy(FixedWindowRollingPolicy) {
+    fileNamePattern = "techlooper_cron_%i.log"
+    minIndex = 1
+    maxIndex = 12
+  }
+  triggeringPolicy(SizeBasedTriggeringPolicy) {
+    maxFileSize = "10MB"
+  }
+  encoder(PatternLayoutEncoder) {
+    pattern = "%d{dd-MM-yyyy HH:mm:ss.SSS} %p [%t] %c{1}: %m%n"
+  }
+}
+
+logger("com.techlooper.service.impl.LooperPointServiceImpl", ALL, ["CRONF"])
+
+root(ERROR, ["CONSOLE"])
