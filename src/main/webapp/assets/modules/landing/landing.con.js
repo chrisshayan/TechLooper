d744bdc7ccad553d8d7d7b13a0c36a08add9ddcb
Phuong H <phuonghqh@gmail.com>
2015-03-25 14:04:22 +0700

    Integrate submit form  with backend

--- src/main/webapp/assets/modules/landing/landing.con.js
+++ src/main/webapp/assets/modules/landing/landing.con.js
@@ -1,38 +1,54 @@
-techlooper.controller('landingController',
-  function ($scope) {
-    $scope.validationRegister = function(){
-      var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
-          fName = $('#landing-first-name').val(),
-          lName = $('#landing-last-name').val(),
-          email = $('#landing-email').val(),
-          errorContent ='';
-      var inputVal = new Array(fName, lName, email);
-      var inputMessage = new Array("first name", "last name", "email address");
-      $('.error-messages').html('');
-      $.each(inputVal, function( index, value ) {
-        if(value == ""){
-          if(errorContent ==''){
-            errorContent = inputMessage[index];
-          }else{
-            errorContent = errorContent + ', ' + inputMessage[index];
-          }
+techlooper.controller('landingController', function ($scope, $http, jsonValue) {
+  $scope.validationRegister = function () {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+      fName = $('#landing-first-name').val(),
+      lName = $('#landing-last-name').val(),
+      email = $('#landing-email').val(),
+      errorContent = '';
+    var inputVal = new Array(fName, lName, email);
+    var inputMessage = new Array("first name", "last name", "email address");
+    $('.error-messages').html('');
+    $.each(inputVal, function (index, value) {
+      if (value == "") {
+        if (errorContent == '') {
+          errorContent = inputMessage[index];
         }
-      });
-      if(email != '' && !emailReg.test(email)){
-        if(errorContent == ''){
-          errorContent = 'Email address is not valid';
-        }else{
-          errorContent = errorContent+ ', Email address is not valid';
+        else {
+          errorContent = errorContent + ', ' + inputMessage[index];
         }
       }
-      if(errorContent != ''){
-        $('.error-messages').append('Please enter your <strong>' + errorContent+ '</strong>').show();
-      }else{
+    });
+
+    if (email != '' && !emailReg.test(email)) {
+      if (errorContent == '') {
+        errorContent = 'Email address is not valid';
+      }
+      else {
+        errorContent = errorContent + ', Email address is not valid';
+      }
+    }
+    if (errorContent != '') {
+      $('.error-messages').append('Please enter your <strong>' + errorContent + '</strong>').show();
+    }
+    else {
+      $('.error-messages').hide();
+      $('#landing-first-name').val('');
+      $('#landing-last-name').val('');
+      $('#landing-email').val('');
+
+      $http.post(jsonValue.httpUri.userRegister, {
+        emailAddress: email,
+        firstName: fName,
+        lastName: lName
+      }).success(function (data) {
+        alert("Cam on");
         $('.error-messages').hide();
-        alert('thanks you')
         $('#landing-first-name').val('');
         $('#landing-last-name').val('');
         $('#landing-email').val('');
-      }
-    };
-  });
+      }).error(function (data) {
+          $('.error-messages').append('Register failed!!').show();
+        });
+    }
+  };
+});
