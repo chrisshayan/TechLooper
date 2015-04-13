c319193a42823393ac117f4e7c9cf183c558d51a
khoa-nd <ndkhoa.is@gmail.com>
2015-04-13 11:06:37 +0700

    Add GA tracking on clicking on Follow button

--- src/main/webapp/assets/modules/talent-search/company-profile/company-profile.ser.js
+++ src/main/webapp/assets/modules/talent-search/company-profile/company-profile.ser.js
@@ -2,6 +2,7 @@ techlooper.factory("companyProfileService", function () {
   var instance = {
     followManager: function(){
       $('.follow-button').find('button').prop('disabled', true).text('Following');
+      ga('send', 'event', 'FollowCompany', 'clicked');
     }
   };
   return instance;
