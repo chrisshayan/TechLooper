9c07d398d67f29fc390ecc69c19d179af1350b73
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-04-20 11:06:02 +0700

    add angularjs structure for skills list area

--- src/main/webapp/assets/modules/technical-detail/technical-detail.mod.js
+++ src/main/webapp/assets/modules/technical-detail/technical-detail.mod.js
@@ -16,20 +16,22 @@ techlooper.directive("technicalDetail", function () {
     replace: true,
     templateUrl: "modules/technical-detail/technical-skills.tem.html",
     link: function(){
-      var myCircle = Circles.create({
-        id:           'circles-1',
-        radius:       60,
-        value:        43,
-        maxValue:     100,
-        width:        10,
-        text:         function(value){return value + '%';},
-        colors:       ['#D3B6C6', '#4B253A'],
-        duration:       400,
-        wrpClass:     'circles-wrp',
-        textClass:      'circles-text',
-        styleWrapper: true,
-        styleText:    true
-      });
+      for(var i = 0; i< 5 ; i++){
+        var myCircle = Circles.create({
+          id:           'circles-'+i,
+          radius:       60,
+          value:        43,
+          maxValue:     100,
+          width:        10,
+          text:         function(value){return value;},
+          colors:       ['#D3B6C6', '#4B253A'],
+          duration:       400,
+          wrpClass:     'circles-wrp',
+          textClass:      'circles-text',
+          styleWrapper: true,
+          styleText:    true
+        });
+      }
     }
   }
 }).directive("trendSkills", function () {
