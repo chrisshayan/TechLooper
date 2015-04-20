463d7d303466512de66d38f8cb54ceb3de16f1a6
Phuong H <phuonghqh@gmail.com>
2015-04-20 16:57:36 +0700

    Added companies list

--- src/main/webapp/assets/modules/technical-detail/technical-detail.con.js
+++ src/main/webapp/assets/modules/technical-detail/technical-detail.con.js
@@ -8,6 +8,10 @@ techlooper.controller("technicalDetailController", function (utils, connectionFa
     return true;
   }
 
+  $scope.companyUrl = function(company) {//java-fpt+at-it-software-i35-en
+    return sprintf("http://vietnamworks.com/%s-%s+at-it-software-i35-en", term.toLowerCase(), company.name);
+  }
+
   connectionFactory.termStatisticInOneYear({term: term})
     .success(function (data, status, headers, config) {
       $scope.termStatistic = data;
