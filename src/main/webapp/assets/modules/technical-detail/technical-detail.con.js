d6b6f99cc5e862b2a5d421ee987e27486311b563
Phuong H <phuonghqh@gmail.com>
2015-04-22 15:13:03 +0700

    Added dynamic loading data

--- src/main/webapp/assets/modules/technical-detail/technical-detail.con.js
+++ src/main/webapp/assets/modules/technical-detail/technical-detail.con.js
@@ -25,12 +25,12 @@ techlooper.controller("technicalDetailController", function (utils, connectionFa
     return sprintf("http://vietnamworks.com/%s-%s+at-it-software-i35-en", termView.label, company.name);
   }
 
-  $scope.loadData = function() {
+  $scope.loadData = function () {
     utils.sendNotification(jsonValue.notifications.switchScope, $scope);
     connectionFactory.termStatisticInOneYear($scope.termRequest)
       .success(function (data, status, headers, config) {
         $scope.termRequest.skills = data.skills.map(function (skill) {return skill.skillName;});
-        $.each(data.skills, function (i, skill) {skill.id = i;})
+        $.each(data.skills, function (i, skill) {skill.id = i;});
         $scope.termStatistic = termService.toViewTerm(data);
         technicalDetailService.trendSkills($scope.termStatistic);
         utils.sendNotification(jsonValue.notifications.loaded);
