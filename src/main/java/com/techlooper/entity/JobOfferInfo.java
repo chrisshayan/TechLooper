b822592bacbc7cb485d247d25d6d8de33de1e247
khoa-nd <ndkhoa.is@gmail.com>
2015-05-05 14:57:19 +0700

    Add index name and type for job offer configuration

--- src/main/java/com/techlooper/entity/JobOfferInfo.java
+++ src/main/java/com/techlooper/entity/JobOfferInfo.java
@@ -9,104 +9,104 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 /**
  * Created by phuonghqh on 5/5/15.
  */
-@Document(indexName = "jobOffer")
+@Document(indexName = "techlooper", type = "jobOffer")
 public class JobOfferInfo {
 
-  @Id
-  private Long id;
+    @Id
+    private Long id;
 
-  @Field
-  private String jobTitle;
+    @Field
+    private String jobTitle;
 
-  @Field(type = FieldType.String)
-  private JobLevelEnum jobLevel;
+    @Field(type = FieldType.String)
+    private JobLevelEnum jobLevel;
 
-  @Field
-  private String location;
+    @Field
+    private String location;
 
-  @Field
-  private Integer netSalary;
+    @Field
+    private Integer netSalary;
 
-  @Field
-  private String[] skills;
+    @Field
+    private String[] skills;
 
-  @Field
-  private String reportTo;
+    @Field
+    private String reportTo;
 
-  @Field
-  private String[] jobCategories;
+    @Field
+    private Long[] jobCategories;
 
-  private String numberOfEmployees;
+    private Integer numberOfEmployees;
 
-  public String[] getJobCategories() {
-    return jobCategories;
-  }
+    public Long[] getJobCategories() {
+        return jobCategories;
+    }
 
-  public void setJobCategories(String[] jobCategories) {
-    this.jobCategories = jobCategories;
-  }
+    public void setJobCategories(Long[] jobCategories) {
+        this.jobCategories = jobCategories;
+    }
 
-  public String getNumberOfEmployees() {
-    return numberOfEmployees;
-  }
+    public Integer getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
 
-  public void setNumberOfEmployees(String numberOfEmployees) {
-    this.numberOfEmployees = numberOfEmployees;
-  }
+    public void setNumberOfEmployees(Integer numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getJobTitle() {
-    return jobTitle;
-  }
+    public String getJobTitle() {
+        return jobTitle;
+    }
 
-  public void setJobTitle(String jobTitle) {
-    this.jobTitle = jobTitle;
-  }
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
 
-  public JobLevelEnum getJobLevel() {
-    return jobLevel;
-  }
+    public JobLevelEnum getJobLevel() {
+        return jobLevel;
+    }
 
-  public void setJobLevel(JobLevelEnum jobLevel) {
-    this.jobLevel = jobLevel;
-  }
+    public void setJobLevel(JobLevelEnum jobLevel) {
+        this.jobLevel = jobLevel;
+    }
 
-  public String getLocation() {
-    return location;
-  }
+    public String getLocation() {
+        return location;
+    }
 
-  public void setLocation(String location) {
-    this.location = location;
-  }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
-  public Integer getNetSalary() {
-    return netSalary;
-  }
+    public Integer getNetSalary() {
+        return netSalary;
+    }
 
-  public void setNetSalary(Integer netSalary) {
-    this.netSalary = netSalary;
-  }
+    public void setNetSalary(Integer netSalary) {
+        this.netSalary = netSalary;
+    }
 
-  public String[] getSkills() {
-    return skills;
-  }
+    public String[] getSkills() {
+        return skills;
+    }
 
-  public void setSkills(String[] skills) {
-    this.skills = skills;
-  }
+    public void setSkills(String[] skills) {
+        this.skills = skills;
+    }
 
-  public String getReportTo() {
-    return reportTo;
-  }
+    public String getReportTo() {
+        return reportTo;
+    }
 
-  public void setReportTo(String reportTo) {
-    this.reportTo = reportTo;
-  }
+    public void setReportTo(String reportTo) {
+        this.reportTo = reportTo;
+    }
 }
