b8f10d52e7c0d7cae4c6fca0ca4d8e92af1ec4ec
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-05-15 13:14:19 +0700

    make how does it work for home page and make resposnive for it

--- src/main/webapp/assets/modules/app.js
+++ src/main/webapp/assets/modules/app.js
@@ -154,7 +154,7 @@ techlooper.config(["$routeProvider", "$translateProvider", "$authProvider", "loc
           if (window.location.host.indexOf("hiring") >= 0) {
             return "/home";
           }
-          return "/pie-chart";
+          return "/home";
         }
       });
   }]);
