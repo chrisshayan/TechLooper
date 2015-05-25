ad3acd7523332128b4b500005467a9f044e00316
Phuong H <phuonghqh@gmail.com>
2015-05-25 14:49:20 +0700

    Fix validation email msg

--- src/main/webapp/assets/modules/common/validator.ser.js
+++ src/main/webapp/assets/modules/common/validator.ser.js
@@ -5,7 +5,7 @@ angular.module("Common").factory("validatorService", function ($translate) {
       var error = {};
       $.each(elems, function(i, elem) {
         $(elem).hasClass("ng-invalid-required") && (error[$(elem).attr("ng-model")] = $translate.instant('requiredThisField'));
-        $(elem).hasClass("ng-invalid-email") && (error[$(elem).attr("ng-model")] = $translate.instant('requiredThisField'));
+        $(elem).hasClass("ng-invalid-email") && (error[$(elem).attr("ng-model")] = $translate.instant('emailInvalid'));
       });
       return  error;
     }
