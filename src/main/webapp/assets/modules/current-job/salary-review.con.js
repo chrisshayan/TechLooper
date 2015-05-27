e340abd4af130bde78124291777c72ae7dbc49f4
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-05-27 11:51:59 +0700

    make show hide form and Thanks message

--- src/main/webapp/assets/modules/current-job/salary-review.con.js
+++ src/main/webapp/assets/modules/current-job/salary-review.con.js
@@ -323,7 +323,23 @@ techlooper.controller("salaryReviewController", function ($scope, $rootScope, js
     delete jobAlert.topPaidJobs;
     $scope.jobAlert = jobAlert;
   }
+  $scope.applyNow = function(){
+    var formContent = $('.partner-company-form');
+    if(!formContent.hasClass('active')){
+      formContent.slideDown("normal");
+      formContent.addClass('active');
+      $('.note-Partner-Company-Form').show();
+    }else{
+      $('.partner-company-detail').find('h4').hide();
+      formContent.hide();
+      $('.apply-now-block').hide();
+      $('.note-Partner-Company-Form').hide();
+      $('.partner-company-thanks').slideDown("normal");
+    }
+  }
+  $scope.validationCompanyPartner = function(){
 
+  }
   var jobAlertTimeout;
   $scope.$watch("jobAlert", function () {
     if (jobAlertTimeout !== undefined) {
