f5d2cedb7f203006622c115d4ef375572bbcd07a
khoa-nd <ndkhoa.is@gmail.com>
2015-06-01 18:40:48 +0700

    Suggestion Controller

--- src/main/java/com/techlooper/controller/SuggestionController.java
+++ src/main/java/com/techlooper/controller/SuggestionController.java
@@ -1,7 +1,5 @@
 package com.techlooper.controller;
 
-import com.techlooper.model.SkillSuggestionItem;
-import com.techlooper.model.SkillSuggestionModel;
 import com.techlooper.service.SuggestionService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,19 +15,13 @@ public class SuggestionController {
     @Resource
     private SuggestionService suggestionService;
 
-    @RequestMapping(value = "/suggestion/skill/{query}", method = RequestMethod.POST)
-    public SkillSuggestionModel suggestSkill(@PathVariable String query) {
-        SkillSuggestionItem skillSuggestionItem = new SkillSuggestionItem();
-        skillSuggestionItem.setText("Java Developer");
-        SkillSuggestionModel skillSuggestionModel = new SkillSuggestionModel();
-        skillSuggestionModel.setSkills(Arrays.asList(skillSuggestionItem));
-        return skillSuggestionModel;
-//        return suggestionService.suggestSkills(query);
+    @RequestMapping(value = "/suggestion/skill/{query}", method = RequestMethod.GET)
+    public List<String> suggestSkill(@PathVariable String query) {
+        return suggestionService.suggestSkills(query);
     }
 
-    @RequestMapping(value = "/suggestion/jobTitle/{query}", method = RequestMethod.POST)
+    @RequestMapping(value = "/suggestion/jobTitle/{query}", method = RequestMethod.GET)
     public List<String> suggestJobTitle(@PathVariable String query) {
-        return Arrays.asList("Java Developer", "PHP Developer");
-//        return suggestionService.suggestJobTitles(query);
+        return suggestionService.suggestJobTitles(query);
     }
 }
