a96f18bfdae653773e1bb25d896f4e74f4c2511f
khoa-nd <ndkhoa.is@gmail.com>
2015-06-02 16:47:22 +0700

    Change Core Configuration

--- src/main/java/com/techlooper/config/CoreConfiguration.java
+++ src/main/java/com/techlooper/config/CoreConfiguration.java
@@ -10,6 +10,8 @@ import com.techlooper.model.UserInfo;
 import com.techlooper.model.VNWJobSearchRequest;
 import com.techlooper.model.VnwJobAlert;
 import com.techlooper.model.VnwJobAlertRequest;
+import com.techlooper.repository.JobSearchAPIConfigurationRepository;
+import com.techlooper.util.RestTemplateUtils;
 import freemarker.template.Template;
 import freemarker.template.TemplateExceptionHandler;
 import org.dozer.DozerBeanMapper;
@@ -27,6 +29,10 @@ import org.springframework.cache.support.CompositeCacheManager;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -62,12 +68,6 @@ public class CoreConfiguration {
   @Resource
   private Environment environment;
 
-  @Value("${mail.techlooper.form}")
-  private String mailTechlooperForm;
-
-  @Value("${mail.techlooper.reply_to}")
-  private String mailTechlooperReplyTo;
-
   @Value("${mail.form}")
   private String mailForm;
 
@@ -80,14 +80,18 @@ public class CoreConfiguration {
   @Value("${mail.citibank.cc_promotion.subject}")
   private String mailCitibankCreditCardPromotionSubject;
 
+//  @Value("${mail.salaryReview.subject}")
+//  private String salaryReviewSubject;
+
+//  @Resource
+//  private JobSearchAPIConfigurationRepository jobSearchAPIConfigurationRepository;
+
   @Value("classpath:vnwConfig.json")
   private org.springframework.core.io.Resource vnwConfigRes;
 
   @Bean
   public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-    PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-    configurer.setFileEncoding("UTF-8");
-    return configurer;
+      return new PropertySourcesPlaceholderConfigurer();
   }
 
   @Bean
@@ -217,8 +221,9 @@ public class CoreConfiguration {
   @Bean
   public MimeMessage salaryReviewMailMessage(JavaMailSender mailSender) throws MessagingException {
     MimeMessage mailMessage = mailSender.createMimeMessage();
-    mailMessage.setReplyTo(InternetAddress.parse(mailTechlooperReplyTo));
-    mailMessage.setFrom(InternetAddress.parse(mailTechlooperForm)[0]);
+    mailMessage.setReplyTo(InternetAddress.parse(mailReplyTo));
+//    mailMessage.setSubject(salaryReviewSubject);
+    mailMessage.setFrom(InternetAddress.parse(mailForm)[0]);
     return mailMessage;
   }
 
@@ -237,5 +242,8 @@ public class CoreConfiguration {
   @Bean
   public JsonNode vietnamworksConfiguration() throws IOException {
     return new ObjectMapper().readTree(vnwConfigRes.getInputStream());
+//    HttpEntity<String> requestEntity = RestTemplateUtils.configureHttpRequestEntity(
+//      MediaType.APPLICATION_JSON, jobSearchAPIConfigurationRepository.getApiKeyName(), jobSearchAPIConfigurationRepository.getApiKeyValue(), null);
+//    return restTemplate().exchange(jobSearchAPIConfigurationRepository.getConfigurationUrl(), HttpMethod.GET, requestEntity, JsonNode.class).getBody();
   }
 }
