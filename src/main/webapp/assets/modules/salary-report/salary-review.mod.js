f919d74619d36c86d8581818d39a29b34aa92805
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-06-03 16:35:10 +0700

    refactor html for last step of salary review

--- src/main/webapp/assets/modules/salary-report/salary-review.mod.js
+++ src/main/webapp/assets/modules/salary-report/salary-review.mod.js
@@ -26,4 +26,46 @@ techlooper
       replace: true,
       templateUrl: "modules/salary-report/sr-about-your-report.tem.html"
     }
+  })
+  .directive("srJobInformation", function () {
+    return {
+      restrict: "A",
+      replace: true,
+      templateUrl: "modules/salary-report/sr-job-information.tem.html"
+    }
+  })
+  .directive("srSalaryChart", function () {
+    return {
+      restrict: "A",
+      replace: true,
+      templateUrl: "modules/salary-report/sr-salary-chart.tem.html"
+    }
+  })
+  .directive("srSendMeReport", function () {
+    return {
+      restrict: "A",
+      replace: true,
+      templateUrl: "modules/salary-report/sr-send-me-report.tem.html"
+    }
+  })
+  .directive("srPromotionCompany", function () {
+    return {
+      restrict: "A",
+      replace: true,
+      templateUrl: "modules/salary-report/sr-promotion-company.tem.html"
+    }
+  })
+  .directive("srSurveyForm", function () {
+    return {
+      restrict: "A",
+      replace: true,
+      templateUrl: "modules/salary-report/sr-survey-form.tem.html"
+    }
+  })
+  .directive("srSimilarJob", function () {
+    return {
+      restrict: "A",
+      replace: true,
+      templateUrl: "modules/salary-report/sr-similar-job.tem.html"
+    }
   });
