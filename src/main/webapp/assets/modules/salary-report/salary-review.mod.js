f9014cdbf0a4cac7b29b500b9e483c8a57578197
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-06-05 16:14:57 +0700

    fixed sub header

--- src/main/webapp/assets/modules/salary-report/salary-review.mod.js
+++ src/main/webapp/assets/modules/salary-report/salary-review.mod.js
@@ -3,7 +3,24 @@ techlooper
     return {
       restrict: "A",
       replace: true,
-      templateUrl: "modules/salary-report/sr-navigation.tem.html"
+      templateUrl: "modules/salary-report/sr-navigation.tem.html",
+      link: function(){
+        //TO DO: fixed sub header
+        //var windScroll = $(window).scrollTop();
+        //if ($('.salary-review-block').position().top <= windScroll) {
+        //  $('.navi-step-salary-review').addClass('fixed');
+        //}
+        //$(window).scroll(function() {
+        //  windScroll = $(window).scrollTop();
+        //  if (windScroll > 0) {
+        //    if ($('.salary-review-block').position().top <= windScroll) {
+        //      $('.navi-step-salary-review').addClass('fixed');
+        //    } else {
+        //      $('.navi-step-salary-review').removeClass('fixed');
+        //    }
+        //  }
+        //});
+      }
     }
   })
   .directive("srAboutYourReport", function ($http, $location, utils, jsonValue) {
