c1953d6ab72fa94d02ad280e9d960b5a424081d6
Phuong H <phuonghqh@gmail.com>
2015-06-05 11:48:25 +0700

    Refactor Salary Review - Validation form

--- src/main/webapp/assets/modules/common/auto-tagbox.mod.js
+++ src/main/webapp/assets/modules/common/auto-tagbox.mod.js
@@ -15,10 +15,14 @@ techlooper.directive('autoTagbox', function ($timeout) {
       }
 
       scope.addTag = function(tag) {
+        if (!scope.config.newTag || !scope.config.newTag.length) {
+          return;
+        }
+
         scope.tags = scope.tags || [];
         scope.errors.length = 0;
 
-        if (scope.tags.length >= 3) {
+        if (scope.tags.length >= 50) {
           return scope.errors.push("maximum50");
         }
         else if (scope.tags.indexOf(scope.config.newTag) > -1) {
