3bcea020f06451fca9de051a7c1ceeeba796d9a6
Phuong H <phuonghqh@gmail.com>
2015-06-05 13:55:53 +0700

    Refactor Salary Review - Translation 1st step

--- src/main/webapp/assets/modules/common/vnwConfigService.js
+++ src/main/webapp/assets/modules/common/vnwConfigService.js
@@ -13,12 +13,12 @@ techlooper.factory("vnwConfigService", function (jsonValue, $translate, $rootSco
   var instance = {
     jobLevelsSelectize: {
       items: $.extend(true, [], jsonValue.jobLevels.filter(function (jobLevel) {return jobLevel.id > 0;})),
-      config: $.extend(true, {placeholder: $translate.instant("exManager")}, translateConfigBase)
+      config: $.extend(true, {}, translateConfigBase)
     },
 
     gendersSelectize: {
       items: jsonValue.genders,
-      config: $.extend(true, {placeholder: $translate.instant("exMale")}, translateConfigBase)
+      config: $.extend(true, {}, translateConfigBase)
     },
 
     yobsSelectize: {
@@ -28,8 +28,7 @@ techlooper.factory("vnwConfigService", function (jsonValue, $translate, $rootSco
         labelField: 'value',
         delimiter: '|',
         searchField: ['value'],
-        maxItems: 1,
-        placeholder: $translate.instant("exYob")
+        maxItems: 1
       }
     },
 
@@ -40,8 +39,7 @@ techlooper.factory("vnwConfigService", function (jsonValue, $translate, $rootSco
         labelField: 'name',
         delimiter: '|',
         maxItems: 1,
-        searchField: ['name'],
-        placeholder: $translate.instant("exHoChiMinh")
+        searchField: ['name']
       }
     },
 
@@ -50,8 +48,7 @@ techlooper.factory("vnwConfigService", function (jsonValue, $translate, $rootSco
       config: $.extend(true, translateConfigBase, {
         valueField: 'id',
         labelField: 'size',
-        searchField: ['size'],
-        placeholder: $translate.instant("ex149")
+        searchField: ['size']
       })
     },
 
@@ -63,8 +60,7 @@ techlooper.factory("vnwConfigService", function (jsonValue, $translate, $rootSco
         delimiter: '|',
         maxItems: 3,
         plugins: ['remove_button'],
-        searchField: ['name'],
-        placeholder: $translate.instant("exItSoftware")
+        searchField: ['name']
       }
     },
 
@@ -75,11 +71,27 @@ techlooper.factory("vnwConfigService", function (jsonValue, $translate, $rootSco
         labelField: 'translate',
         delimiter: '|',
         maxItems: 1,
-        searchField: ['translate'],
-        placeholder: $translate.instant("exDay")
+        searchField: ['translate']
       }
     }
   }
 
+  var doTranslate = function(i, item) {
+    $translate(item.translate).then(function(translate) {item.translate = translate;});
+  }
+
+  $.each(instance.jobLevelsSelectize.items, doTranslate);
+  $.each(instance.gendersSelectize.items, doTranslate);
+  $.each(instance.timeToSendsSelectize.items, doTranslate);
+
+  $translate(["exManager", "exMale", "exYob", "exHoChiMinh", "ex149", "exItSoftware", "exDay"]).then(function(trans) {
+    instance.jobLevelsSelectize.config.placeholder = trans.exManager;
+    instance.gendersSelectize.config.placeholder = trans.exMale;
+    instance.yobsSelectize.config.placeholder = trans.exYob;
+    instance.locationsSelectize.config.placeholder = trans.exHoChiMinh;
+    instance.companySizeSelectize.config.placeholder = trans.ex149;
+    instance.industriesSelectize.config.placeholder = trans.exItSoftware;
+    instance.timeToSendsSelectize.config.placeholder = trans.exDay;
+  });
   return instance;
 });
