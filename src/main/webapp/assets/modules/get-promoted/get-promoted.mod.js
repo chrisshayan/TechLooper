6e0ca893812b3cbe5134f3f9668bfd138f10517a
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-06-12 15:09:12 +0700

    implement No Data case for Get Promoted Results

--- src/main/webapp/assets/modules/get-promoted/get-promoted.mod.js
+++ src/main/webapp/assets/modules/get-promoted/get-promoted.mod.js
@@ -3,7 +3,7 @@ techlooper.directive("getPromotedForm", function ($http) {
     restrict: "E",
     replace: true,
     templateUrl: "modules/get-promoted/gp-form.tem.html",
-    link: function (scope, element, attr, ngModel) {
+    link: function (scope) {
       var jobTitleSuggestion = function (jobTitle) {
         if (!jobTitle) {
           return;
@@ -17,6 +17,30 @@ techlooper.directive("getPromotedForm", function ($http) {
       scope.$watch("getPromoted.jobTitle", function (newVal) {
         jobTitleSuggestion(newVal);
       });
+    }
+  }
+}).directive("getPromotedResults", function ($http, validatorService, $translate) {
+  return {
+    restrict: "E",
+    replace: true,
+    templateUrl: "modules/get-promoted/gp-result.tem.html",
+    link: function (scope, element, attr, ctrl) {
+      scope.sendMeNow = function () {
+        var error = validatorService.validate($(".send-me-skills-trend").find("[validate]:visible"));
+        scope.error = error;
+        if (!$.isEmptyObject(error)) {
+          return;
+        }
+
+        //scope.sendMeReport.salaryReviewId = scope.salaryReview.createdDateTime;
+        //scope.sendMeReport.lang = $translate.use();
+        //$http.post("salaryReview/placeSalaryReviewReport", scope.sendMeReport);
+        //$('.thanks-message-for-send-me-success').addClass('show');
+        //scope.state.showThanksSendMeReport = true;
+        //
+        //delete scope.state.showSendReport;
+        //delete scope.sendMeReport;
+      }
     }
   }
 });
