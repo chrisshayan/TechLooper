80dbdd4a9fc89ca8fd0d3a432480bcc3d0f1d580
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-06-12 11:17:07 +0700

    validation for Get Promoted form

--- src/main/webapp/assets/modules/get-promoted/get-promoted.con.js
+++ src/main/webapp/assets/modules/get-promoted/get-promoted.con.js
@@ -1,3 +1,13 @@
-techlooper.controller('getPromotedController', function () {
-
+techlooper.controller('getPromotedController', function ($scope, validatorService) {
+  $scope.getPromoted = {};
+  $scope.validationGetPromoted = function(st){
+    var elems = st.find("[validate]");
+    $scope.error = validatorService.validate(elems);
+  };
+  $scope.getPromoted = function(){
+    var jobTitle = $('.gp-form-field');
+    if(!$scope.validationGetPromoted(jobTitle)){
+      return false;
+    }
+  };
 });
--- src/main/webapp/assets/modules/get-promoted/get-promoted.mod.js
+++ src/main/webapp/assets/modules/get-promoted/get-promoted.mod.js
@@ -2,6 +2,21 @@ techlooper.directive("getPromotedForm", function ($http, $location, utils, jsonV
   return {
     restrict: "E",
     replace: true,
-    templateUrl: "modules/get-promoted/gp-form.tem.html"
+    templateUrl: "modules/get-promoted/gp-form.tem.html",
+    link: function (scope, element, attr, ngModel) {
+      var jobTitleSuggestion = function (jobTitle) {
+        if (!jobTitle) {
+          return;
+        }
+
+        $http.get("suggestion/jobTitle/" + jobTitle)
+          .success(function (data) {
+            scope.state.jobTitles = data.items.map(function (item) {return item.name;});
+          });
+      }
+      scope.$watch("getPromoted.jobTitle", function (newVal) {
+        jobTitleSuggestion(newVal);
+      });
+    }
   }
 });
