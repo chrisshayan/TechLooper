0d9e1463500ee02eb8be48961cea2539613bf68c
Phuong H <phuonghqh@gmail.com>
2015-06-15 10:07:27 +0700

    Validate get promoted form

--- src/main/webapp/assets/modules/get-promoted/get-promoted.con.js
+++ src/main/webapp/assets/modules/get-promoted/get-promoted.con.js
@@ -1,3 +1,33 @@
 techlooper.controller('getPromotedController', function ($scope, validatorService, vnwConfigService) {
   $scope.selectize = vnwConfigService;
+
+  var state = {
+    default: {
+    },
+
+    result: {
+      showResult: true
+    }
+  }
+
+  $scope.changeState = function(st) {
+    var pState = angular.copy(state[st] || st);
+    $scope.state = pState;
+  }
+
+  $scope.$watch("promotionInfo.jobTitle", function(newVal, oldVal) {
+    if (!newVal && !oldVal) {
+      return;
+    }
+
+    $scope.promotionForm.jobTitle.$touch = true;
+  });
+
+  $scope.doPromotion = function() {
+    if ($scope.promotionForm.$valid) {
+      $scope.changeState('result');
+    }
+  }
+
+  $scope.changeState("default");
 });
