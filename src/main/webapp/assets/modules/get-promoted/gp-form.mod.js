4535b9879544690278d3f8b873cd8241968f847b
Phuong H <phuonghqh@gmail.com>
2015-06-15 18:26:46 +0700

    Fix UI bug related to get promoted page

--- src/main/webapp/assets/modules/get-promoted/gp-form.mod.js
+++ src/main/webapp/assets/modules/get-promoted/gp-form.mod.js
@@ -1,28 +1,34 @@
-techlooper
-  .directive("getPromotedForm", function ($http, vnwConfigService) {
-    return {
-      restrict: "E",
-      replace: true,
-      templateUrl: "modules/get-promoted/gp-form.tem.html",
-      link: function (scope, element, attr, ctrl) {
-        scope.doPromotion = function () {
-          if (!scope.promotionForm.$valid) {
-            return false;
-          }
-          scope.masterPromotion = angular.copy(scope.promotionInfo);
-          $http.post("getPromoted", scope.promotionInfo)
-            .success(function (data, status, headers, config) {
-              scope.masterPromotion.result = data;
-              scope.changeState('result');
-            });
-        }
+techlooper.directive("getPromotedForm", function ($http, vnwConfigService, $location) {
+  return {
+    restrict: "E",
+    replace: true,
+    templateUrl: "modules/get-promoted/gp-form.tem.html",
+    link: function (scope, element, attr, ctrl) {
+      var campaign = $location.search();
 
-        scope.$watch("masterPromotion", function (newVal, oldVal) {
-          if (!newVal && !oldVal) return false;
-          scope.masterPromotion.jobLevelIds = vnwConfigService.getJobLevelIds(scope.masterPromotion.jobLevelId);
-          scope.masterPromotion.jobLevelTitle = vnwConfigService.getJobLevelText(scope.masterPromotion.jobLevelId);
-          scope.masterPromotion.jobCategoryTitle = vnwConfigService.getIndustryTexts(scope.masterPromotion.jobCategoryIds).join(" | ");
-        });
+      scope.doPromotion = function () {
+        if (!scope.promotionForm.$valid) {
+          return false;
+        }
+        scope.masterPromotion = angular.copy(scope.promotionInfo);
+        $http.post("getPromoted", scope.promotionInfo)
+          .success(function (data, status, headers, config) {
+            scope.masterPromotion.result = data;
+            scope.changeState('result');
+          });
       }
+
+      scope.$watch("masterPromotion", function (newVal, oldVal) {
+        if (!newVal && !oldVal) return false;
+        var jobLevelIds = vnwConfigService.getJobLevelIds(scope.masterPromotion.jobLevelId);
+        scope.masterPromotion.jobLevelIds = jobLevelIds ? jobLevelIds : [];
+
+        var jobLevelText = vnwConfigService.getJobLevelText(scope.masterPromotion.jobLevelId);
+        scope.masterPromotion.jobLevelTitle = jobLevelText;
+
+        var industryTexts = vnwConfigService.getIndustryTexts(scope.masterPromotion.jobCategoryIds);
+        scope.masterPromotion.jobCategoryTitle = industryTexts ? industryTexts.join(" | ") : undefined;
+      });
     }
-  });
+  }
+});
