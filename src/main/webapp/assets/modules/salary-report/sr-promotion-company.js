0a374dd338516c57fb2129f49cdf637c662c36a4
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-06-23 11:30:45 +0700

    fix bug for email address

--- src/main/webapp/assets/modules/salary-report/sr-promotion-company.js
+++ src/main/webapp/assets/modules/salary-report/sr-promotion-company.js
@@ -9,6 +9,7 @@ techlooper.directive("srPromotionCompany", function ($http, validatorService, vn
         scope.state.showPromotionForm = true;
         if($('#txtEmailPromotion').val() == ''){
           $('#txtEmailPromotion').val(scope.$parent.email);
+          $('#txtEmailPromotion').attr('value',scope.$parent.email);
         }
       }
 
@@ -23,21 +24,22 @@ techlooper.directive("srPromotionCompany", function ($http, validatorService, vn
           scope.state.showThanksCash = true;
           return;
         }
-
+        var emailVal = $('#txtEmailPromotion');
+        scope.$parent.email = emailVal.val();
+        if($('#txtEmailReport').val() == ''){
+          $('#txtEmailReport').val(scope.$parent.email);
+          $('#txtEmailReport').attr('value',scope.$parent.email);
+        }
+        if($('#txtEmailJobAlert').val() == ''){
+          $('#txtEmailJobAlert').val(scope.$parent.email);
+          $('#txtEmailJobAlert').attr('value',scope.$parent.email);
+        }
         scope.promotion.salaryReviewId = scope.salaryReview.createdDateTime;
         $http.post("promotion/citibank/creditCard", scope.promotion)
           .success(function () {
             localStorage.setItem('PROMOTION-KEY', 'yes');
-            var emailVal = $('#txtEmailPromotion');
-            scope.$parent.email = emailVal.val();
-            if($('#txtEmailReport').val() == ''){
-              $('#txtEmailReport').val(scope.$parent.email);
-            }
-            if($('#txtEmailJobAlert').val() == ''){
-              $('#txtEmailJobAlert').val(scope.$parent.email);
-            }
           }).error(function() {
-             localStorage.setItem('PROMOTION-KEY', 'yes');
+             localStorage.setItem('PROMOTION-KEY', 'no');
           });
         $('.partner-company-content').hide();
         //delete scope.state.showPromotion;
