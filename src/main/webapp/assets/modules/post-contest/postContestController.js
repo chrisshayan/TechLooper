e0194ec7b08e6ebcc1b8e3bde45b90acded9f5bf
Phuong H <phuonghqh@gmail.com>
2015-06-24 17:40:57 +0700

    Add state isValid function

--- src/main/webapp/assets/modules/post-contest/postContestController.js
+++ src/main/webapp/assets/modules/post-contest/postContestController.js
@@ -1,3 +1,37 @@
 techlooper.controller("postContestController", function ($scope) {
 
+  var state = {
+    challenge: {
+      showChallenge: true,
+      isValid: function () {
+        return true;
+      }
+    },
+
+    timeline: {
+      showTimeline: true,
+      isValid: function () {
+        return true;
+      }
+    },
+
+    reward: {
+      showReward: true,
+      isValid: function () {
+        return true;
+      }
+    }
+  }
+
+  $scope.changeState = function (st) {
+    if (!$scope.state.isValid()) {
+      return false;
+    }
+
+    var pState = angular.copy(state[st] || st);
+    $scope.state = pState;
+    $scope.$emit("$stateChangeSuccess");
+  }
+
+  $scope.changeState(state.challenge);
 });
