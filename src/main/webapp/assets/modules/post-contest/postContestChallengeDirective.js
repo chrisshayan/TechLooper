84e38bd3394f1d7a261b49499af00f4e25c5137b
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-06-25 11:03:00 +0700

    - Update translate for footer - style for navigation of post contest - add form for your challenge

--- src/main/webapp/assets/modules/post-contest/postContestChallengeDirective.js
+++ src/main/webapp/assets/modules/post-contest/postContestChallengeDirective.js
@@ -4,6 +4,7 @@ techlooper.directive("postContestChallenge", function ($http, utils) {
       replace: true,
       templateUrl: "modules/post-contest/postContestChallenge.html",
       link: function (scope, element, attr, ctrl) {
+        $('[data-toggle="tooltip"]').tooltip({html:true, placement: 'right'});
       }
     }
 });
