9c39ad364dfd4951ed6f11832303315a33f32d78
Phuong H <phuonghqh@gmail.com>
2015-06-25 14:27:30 +0700

    Fix compile issue

--- src/main/java/com/techlooper/repository/vnw/VnwUserRepo.java
+++ src/main/java/com/techlooper/repository/vnw/VnwUserRepo.java
@@ -8,5 +8,5 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface VnwUserRepo extends ReadOnlyRepository<VnwUser, Long> {
 
-  VnwUser findByUsernameAndUserPassAll
+  VnwUser findByUsernameAndUserPassAllIgnoreCase(String username, String userPass);
 }
