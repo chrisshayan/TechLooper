a03ea8be1fe3a34f5bac4a9539a109d6389f467f
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-06-30 15:56:00 +0700

    update translate

--- src/main/webapp/assets/modules/post-contest/postContestRewardDirective.js
+++ src/main/webapp/assets/modules/post-contest/postContestRewardDirective.js
@@ -4,6 +4,7 @@ techlooper.directive("postContestReward", function ($http, utils) {
     replace: true,
     templateUrl: "modules/post-contest/postContestReward.html",
     link: function (scope, element, attr, ctrl) {
+      $('[data-toggle="tooltip"]').tooltip({html: true, placement: 'right'});
     }
   }
 });
