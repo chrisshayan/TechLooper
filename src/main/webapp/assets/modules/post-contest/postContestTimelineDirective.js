d2dfe375c6b6775da14e5a3b074117b80a99257e
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-06-30 15:39:32 +0700

    make tooltip show on timeline form

--- src/main/webapp/assets/modules/post-contest/postContestTimelineDirective.js
+++ src/main/webapp/assets/modules/post-contest/postContestTimelineDirective.js
@@ -4,6 +4,7 @@ techlooper.directive("postContestTimeline", function ($http, utils) {
     replace: true,
     templateUrl: "modules/post-contest/postContestTimeline.html",
     link: function (scope, element, attr, ctrl) {
+      $('[data-toggle="tooltip"]').tooltip({html: true, placement: 'right'});
       $(element).find('.date').datepicker({
         autoclose:  true,
         format: 'dd/mm/yyyy'
