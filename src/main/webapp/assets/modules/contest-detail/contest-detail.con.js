0e29edcd1a8bc317809ef3e376da8f074275de77
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-07-06 14:49:03 +0700

    add facebook link share and update translate

--- src/main/webapp/assets/modules/contest-detail/contest-detail.con.js
+++ src/main/webapp/assets/modules/contest-detail/contest-detail.con.js
@@ -46,5 +46,11 @@ techlooper.controller('contestDetailController', function ($scope, apiService, l
   $scope.formatDate = function(d){
     return moment(d).format('MM DD YYYY');
   }
+
+  $scope.fbShare = function(url, title, descr, image, winWidth, winHeight) {
+    var winTop = (screen.height / 2) - (winHeight / 2);
+    var winLeft = (screen.width / 2) - (winWidth / 2);
+    window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+  }
 });
 
