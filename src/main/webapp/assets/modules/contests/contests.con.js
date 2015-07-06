dc1faf75e1485daf98ce8bf4ff2119ce9fb43da0
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-07-06 16:55:02 +0700

    Update source code for contests page: - Show Time Left - Show status of timeline

--- src/main/webapp/assets/modules/contests/contests.con.js
+++ src/main/webapp/assets/modules/contests/contests.con.js
@@ -1,4 +1,11 @@
-techlooper.controller('contestsController', function (apiService, $scope) {
+techlooper.controller('contestsController', function (apiService, $scope, jsonValue) {
+  $scope.status = function (type, contest) {
+    switch (type) {
+      case "contest-in-progress":
+        if (!contest) return false;
+        return (contest.progress.translate == jsonValue.status.progress.translate);
+    }
+  }
   apiService.searchContests().success(function(contests) {
     $scope.contestsList = contests;
   });
