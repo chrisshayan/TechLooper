baf142bd46b439fd0112a1ddc10cf60888746669
Phuong H <phuonghqh@gmail.com>
2015-07-09 09:10:35 +0700

    Remove empty space

--- src/main/webapp/assets/modules/common/resourcesService.js
+++ src/main/webapp/assets/modules/common/resourcesService.js
@@ -1,8 +1,13 @@
 techlooper.factory("resourcesService", function ($translate, $q) {
   var reviewStyleOptions = [{translate: "contestOwnerSignOff"}];
-  var qualityIdeaOptions = [{translate: "hasAcceptableTradeoffs"},{translate: "theSolutionAchievesTheStatedGoals"},{translate: "theSolutionIsPracticalAndReliable"},{translate: "theSolutionIsInnovative"}];
+  var qualityIdeaOptions = [
+    {translate: "hasAcceptableTradeoffs"},
+    {translate: "theSolutionAchievesTheStatedGoals"},
+    {translate: "theSolutionIsPracticalAndReliable"},
+    {translate: "theSolutionIsInnovative"}
+  ];
 
-  var singleSelectize = function(key) {
+  var singleSelectize = function (key) {
     return {
       create: false,
       valueField: 'title',
