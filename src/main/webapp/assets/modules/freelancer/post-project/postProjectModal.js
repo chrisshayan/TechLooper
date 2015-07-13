607ec2007c8b277a5385842197336ac8c8916b19
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-07-13 14:11:45 +0700

    re-tructure angular file for post project file

---
+++ src/main/webapp/assets/modules/freelancer/post-project/postProjectModal.js
@@ -0,0 +1,16 @@
+techlooper
+.directive("projectForm", function ($http) {
+  return {
+    restrict: "E",
+    replace: true,
+    templateUrl: "modules/freelancer/post-project/projectForm.html"
+  }
+})
+.directive("projectReview", function ($http) {
+  return {
+    restrict: "E",
+    replace: true,
+    templateUrl: "modules/freelancer/post-project/reviewProject.html"
+  }
+});
+
