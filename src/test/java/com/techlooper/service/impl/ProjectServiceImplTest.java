c90cf92d19c6841b4b064fbdba54e023582c402d
khoa-nd <ndkhoa.is@gmail.com>
2015-07-13 11:30:26 +0700

    Implement test case for service to save project into ES

---
+++ src/test/java/com/techlooper/service/impl/ProjectServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.techlooper.service.impl;
+
+import com.techlooper.config.ElasticsearchConfiguration;
+import com.techlooper.config.ProjectServiceConfigurationTest;
+import com.techlooper.entity.ProjectEntity;
+import com.techlooper.model.ProjectDto;
+import com.techlooper.service.ProjectService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ElasticsearchConfiguration.class, ProjectServiceConfigurationTest.class})
+public class ProjectServiceImplTest {
+
+    @Resource
+    private ProjectService projectService;
+
+    @Test
+    public void testSaveProject() throws Exception {
+        ProjectDto projectDto = new ProjectDto();
+        projectDto.setAuthorEmail("khoa.nguyen@navigosgroup.com");
+        projectDto.setProjectTitle("Test Service");
+        projectDto.setProjectDescription("Test Service Description");
+        projectDto.setSkills(Arrays.asList("Java", "Spring"));
+        projectDto.setPayMethod("Fixed Price");
+        projectDto.setEstimatedEndDate("13/07/2015");
+        projectDto.setBudget(100D);
+        projectDto.setNumberOfHires(30L);
+        projectDto.setEstimatedDuration("More than 6 months");
+        projectDto.setEstimatedWorkload("More than 30 hours/week");
+        projectDto.setHourlyRate(25D);
+        ProjectEntity projectEntity = projectService.saveProject(projectDto);
+        assertTrue(projectEntity.getProjectId() != null);
+        assertTrue(projectEntity.getCreatedDate() != null);
+    }
+}
