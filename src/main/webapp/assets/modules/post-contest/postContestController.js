aa94992550ae2e061f95291dd0a55a39a1f1c62a
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-07-14 15:35:55 +0700

    disabled button submit when on time loading for post challenge

--- src/main/webapp/assets/modules/post-contest/postContestController.js
+++ src/main/webapp/assets/modules/post-contest/postContestController.js
@@ -96,13 +96,14 @@ techlooper.controller("postContestController", function ($scope, $http, jsonValu
         var request = angular.copy($scope.contest);
         request.lang = $translate.use();
         utils.sendNotification(jsonValue.notifications.loading);
-
+        $('.submit-contest-content').find('button').addClass('disabled');
         $http.post("challenge/publish", request, {transformResponse: function (d, h) {return d;}})
           .then(function (response) {
             var title = utils.toAscii($scope.contest.challengeName);
             $location.url(sprintf("/challenge-detail/%s-%s-id", title, response.data));
           })
           .finally(function () {
+              $('.submit-contest-content').find('button').removeClass('disabled');
             utils.sendNotification(jsonValue.notifications.hideLoadingBox);
           });
         return true;
