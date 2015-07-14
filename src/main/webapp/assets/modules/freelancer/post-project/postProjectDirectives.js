dc28d92c294ec23d1f5ed2485469ac309dcc49a8
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-07-14 10:49:32 +0700

    disabled the date in the past on datepicker

--- src/main/webapp/assets/modules/freelancer/post-project/postProjectDirectives.js
+++ src/main/webapp/assets/modules/freelancer/post-project/postProjectDirectives.js
@@ -5,9 +5,12 @@ techlooper
       replace: true,
       templateUrl: "modules/freelancer/post-project/projectForm.html",
       link: function (scope, element, attr, ctrl){
+        var nowDate = new Date();
+        var today = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 0, 0, 0, 0);
         $(element).find('.date').datepicker({
           autoclose:  true,
-          format: 'dd/mm/yyyy'
+          format: 'dd/mm/yyyy',
+          startDate: today
         });
       }
     }
