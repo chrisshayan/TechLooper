6754aaf581ece4dea71610038a3496741f860a78
Phuong H <phuonghqh@gmail.com>
2015-07-16 14:47:28 +0700

    Add confirm dialog on exit project

--- src/main/webapp/assets/modules/common/confirmOnExitDirective.js
+++ src/main/webapp/assets/modules/common/confirmOnExitDirective.js
@@ -1,27 +1,46 @@
-techlooper.directive('confirmOnExit', function ($rootScope) {
+techlooper.directive('confirmOnExit', function ($rootScope, $confirm, $window, $location) {
   return {
     require: '^form',
     restrict: 'A',
     link: function (scope, element, attrs, ngModelCtrl) {
-      //window.onbeforeunload = function () {
-      //  if (ngModelCtrl.$dirty) {
-      //    return "The form is dirty, do you want to stay on the page?";
-      //  }
-      //}
-      //scope.$on('$routeChangeStart', function (event, next, current) {
-      //  console.log(123);
-      //  console.log(event.currentScope.$$phase);
-      //  //if (ngModelCtrl.$dirty) {
-      //  //  if(!event.currentScope.$$phase) {
-      //  //    return;
-      //  //  }
-      //  //    if (!confirm("The form is dirty, do you want to stay on the page?")) {
-      //  //      event.preventDefault();
-      //  //    }
-      //  //}
-      //
-      //  return false;
-      //});
+      window.onbeforeunload = function () {
+        if (ngModelCtrl.$dirty) {
+          return "The form is dirty, do you want to stay on the page?";
+        }
+      }
+
+      var processing = false;
+      var confirmed = false;
+      var e = undefined;
+      scope.$on('$locationChangeStart', function (event, next, current) {
+        if (processing) {
+          console.log(confirmed);
+          //if (!e) {
+            return event.preventDefault();
+          //}
+        }
+        if (ngModelCtrl.$dirty) {
+          processing = true;
+          confirmed = confirm("You have unsaved edits. Do you wish to leave?");
+          //confirmed = true;
+          ////e = event;
+          if (!confirmed) {
+          //  //processing = false;
+            event.preventDefault();
+          }
+
+          //$confirm({text: 'Are you sure you want to delete?'})
+          //  .then(function() {
+          //    scope.deletedConfirm = 'Deleted';
+          //  });
+          //
+          //event.preventDefault();
+          //else {
+            processing = false;
+          //return true;
+          //}
+        }
+      });
     }
   }
 });
