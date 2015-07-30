94c15fbd40ba81182c4dcf65020789d23cbe0cac
Phuong H <phuonghqh@gmail.com>
2015-07-30 11:38:48 +0700

    Add function to get current user in the system

--- src/main/webapp/assets/modules/home-page/homeController.js
+++ src/main/webapp/assets/modules/home-page/homeController.js
@@ -1,10 +1,10 @@
-techlooper.controller("homeController", function($scope, securityService, apiService, localStorageService, $location,
-                                                 jsonValue, $filter, $timeout) {
+techlooper.controller("homeController", function ($scope, securityService, apiService, localStorageService, $location,
+                                                  jsonValue, $filter, $timeout) {
 
   $scope.today = moment().format(jsonValue.dateFormat);
 
-  $scope.loginBySocial = function(provider) {
-    apiService.getSocialLoginUrl(provider).success(function(url) {
+  $scope.loginBySocial = function (provider) {
+    apiService.getSocialLoginUrl(provider).success(function (url) {
       localStorageService.set("lastFoot", $location.url());
       window.location = url;
     });
@@ -13,22 +13,26 @@ techlooper.controller("homeController", function($scope, securityService, apiSer
   var code = localStorageService.get("code");
   if (code) {
     securityService.login(code, localStorageService.get("social"), "social")
-      .success(function(data) {
+      .success(function (data) {
+        securityService.getCurrentUser("social")
+          .success(function (data) {
+
+          });
       })
-      .error(function(data) {
+      .error(function (data) {
       });
     localStorageService.remove("code", "social");
   }
 
-  apiService.getPersonalHomepage().success(function(data) {
+  apiService.getPersonalHomepage().success(function (data) {
     $scope.homePage = data;
-    $scope.homePage.termStatistic.logo = "images/" +  $.grep(jsonValue.technicalSkill, function(skill) {
-      return skill.term == $scope.homePage.termStatistic.term;
-    })[0].logo;
+    $scope.homePage.termStatistic.logo = "images/" + $.grep(jsonValue.technicalSkill, function (skill) {
+        return skill.term == $scope.homePage.termStatistic.term;
+      })[0].logo;
   });
 
   //TODO remove timeout function
-  $timeout(function(){
+  $timeout(function () {
     var tallest = 0;
     $('.personal-site-content-detail').find('.box-content').each(function () {
       var thisHeight = $(this).height();
