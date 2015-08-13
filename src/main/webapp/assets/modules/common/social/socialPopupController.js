c8cb2fb036070082761b7c4555c7a82771f7dfa0
Phuong H <phuonghqh@gmail.com>
2015-08-13 14:19:55 +0700

    Fix Login workflow

---
+++ src/main/webapp/assets/modules/common/social/socialPopupController.js
@@ -0,0 +1,8 @@
+techlooper.controller("socialPopupController", function($scope, apiService, localStorageService, $location) {
+  $scope.loginBySocial = function (provider) {
+    apiService.getSocialLoginUrl(provider).success(function (url) {
+      localStorageService.set("lastFoot", $location.url());
+      window.location = url;
+    });
+  }
+});
