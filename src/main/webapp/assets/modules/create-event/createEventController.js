8982598a9d808b57f032f512f142be0aa565a772
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-08-18 09:03:10 +0700

    Implement html, sass, jquery for "Create Event" page

--- src/main/webapp/assets/modules/create-event/createEventController.js
+++ src/main/webapp/assets/modules/create-event/createEventController.js
@@ -1,3 +1,17 @@
-techlooper.controller("createEventController", function ($scope) {
+techlooper.controller("createEventController", function ($scope, $translate) {
+  var placeholder = $translate.instant('whoJoinAndWhyEx');
+  $('#txtWhyEvent').val(placeholder);
+  $('#txtWhyEvent').focus(function(){
+    if($(this).val() === placeholder){
+      $(this).val('');
+      $(this).removeClass('change-color');
+    }
+  });
 
+  $('#txtWhyEvent').blur(function(){
+    if($(this).val() ===''){
+      $(this).val(placeholder);
+      $(this).addClass('change-color');
+    }
+  });
 });
