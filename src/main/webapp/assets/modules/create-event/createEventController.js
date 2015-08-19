a9b7b07ba16327ad81e59f363ba8490bead2e0c1
Phuong H <phuonghqh@gmail.com>
2015-08-19 17:55:29 +0700

    Remove old email list code

--- src/main/webapp/assets/modules/create-event/createEventController.js
+++ src/main/webapp/assets/modules/create-event/createEventController.js
@@ -16,37 +16,6 @@ techlooper.controller("createEventController", function ($scope, $translate, jso
     }
   });
 
-  $scope.errors = [];
-
-  $scope.removeTag = function (tag) {
-    $scope.emails.splice($scope.emails.indexOf(tag), 1);
-    $scope.errors.length = 0;
-  }
-
-  $scope.emails = $scope.emails || [];
-
-  $scope.addTag = function () {
-    var newTag = $scope.attendants;
-    if (!newTag || !newTag.length) {
-      return;
-    }
-    if ($('#txtAttendants').hasClass("ng-invalid-email")) {
-      return $scope.errors.push("emailInvalid");
-    }
-    else if ($scope.emails.length >= 100) {
-      return $scope.errors.push("maximum100");
-    }
-    else if (newTag.length > 40) {
-      return $scope.errors.push("tooLong");
-    }
-    else if ($scope.emails.indexOf(newTag) > -1) {
-      return $scope.errors.push("hasEmailExist");
-    }
-    $scope.emails.push(newTag);
-    $scope.errors.length = 0;
-    $scope.attendants = "";
-  }
-
   $scope.createWebinar = function () {
     $scope.webinarForm.$setSubmitted();
     if ($scope.webinarForm.$invalid) {
