d4a83959739ef59ca8efe4d486f5b1edf5cf96be
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-08-20 09:28:00 +0700

    update  placeholder for "Who should join and why?" textarea on the Post Event page

--- src/main/webapp/assets/modules/create-event/createEventController.js
+++ src/main/webapp/assets/modules/create-event/createEventController.js
@@ -1,21 +1,5 @@
 techlooper.controller("createEventController", function ($scope, $translate, jsonValue, apiService) {
-
-  var placeholder = $translate.instant('whoJoinAndWhyEx');
-  $('#txtWhyEvent').val(placeholder);
-  $('#txtWhyEvent').focus(function () {
-    if ($(this).val() === placeholder) {
-      $(this).val('');
-      $(this).removeClass('change-color');
-    }
-  });
-
-  $('#txtWhyEvent').blur(function () {
-    if ($(this).val() === '') {
-      $(this).val(placeholder);
-      $(this).addClass('change-color');
-    }
-  });
-
+  
   $scope.createWebinar = function () {
     $scope.webinarForm.$setSubmitted();
     if ($scope.webinarForm.$invalid) {
