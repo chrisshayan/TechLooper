e7cdca9adf256d6478e01eefa33746dc93283bd2
khoa-nd <ndkhoa.is@gmail.com>
2015-08-25 09:22:00 +0700

    Fix compile error due to repository class

--- src/main/java/com/techlooper/service/impl/GoogleCalendarServiceImpl.java
+++ src/main/java/com/techlooper/service/impl/GoogleCalendarServiceImpl.java
@@ -8,8 +8,7 @@ import com.google.api.services.calendar.model.EventDateTime;
 import com.techlooper.dto.WebinarInfoDto;
 import com.techlooper.entity.WebinarEntity;
 import com.techlooper.entity.vnw.VnwUser;
-import com.techlooper.repository.couchbase.UserRepository;
-import com.techlooper.repository.elasticsearch.WebinarRepository;
+import com.techlooper.repository.userimport.WebinarRepository;
 import com.techlooper.repository.vnw.VnwUserRepo;
 import com.techlooper.service.GoogleCalendarService;
 import org.dozer.Mapper;
@@ -28,56 +27,56 @@ import java.util.Collection;
 @Service
 public class GoogleCalendarServiceImpl implements GoogleCalendarService {
 
-  @Resource
-  private WebinarRepository webinarRepository;
+    @Resource
+    private WebinarRepository webinarRepository;
 
-  @Resource
-  private Mapper dozerMapper;
+    @Resource
+    private Mapper dozerMapper;
 
-  @Resource
-  private Calendar googleCalendar;
+    @Resource
+    private Calendar googleCalendar;
 
-  @Resource
-  private VnwUserRepo vnwUserRepo;
+    @Resource
+    private VnwUserRepo vnwUserRepo;
 
-  private static final String CALENDAR_ID = "techlooperawesome@gmail.com";
+    private static final String CALENDAR_ID = "techlooperawesome@gmail.com";
 
-  public WebinarInfoDto createWebinarInfo(WebinarInfoDto webinarInfoDto, String organiser) throws IOException {
-    VnwUser vnwUser = vnwUserRepo.findByUsernameIgnoreCase(organiser);
-    String organiserEmail = vnwUser != null ? vnwUser.getEmail() : organiser;
+    public WebinarInfoDto createWebinarInfo(WebinarInfoDto webinarInfoDto, String organiser) throws IOException {
+        VnwUser vnwUser = vnwUserRepo.findByUsernameIgnoreCase(organiser);
+        String organiserEmail = vnwUser != null ? vnwUser.getEmail() : organiser;
 
-    Event event = new Event()
-      .setSummary(webinarInfoDto.getName())
-      .setDescription(webinarInfoDto.getDescription());
+        Event event = new Event()
+                .setSummary(webinarInfoDto.getName())
+                .setDescription(webinarInfoDto.getDescription());
 
-    DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("dd/MM/yyyy hh:mm a");
-    org.joda.time.DateTime startDate = dateTimeFormatter.parseDateTime(webinarInfoDto.getStartDate());
-    org.joda.time.DateTime endDate = dateTimeFormatter.parseDateTime(webinarInfoDto.getEndDate());
-    event.setStart(new EventDateTime().setDateTime(new DateTime(startDate.toString())));
-    event.setEnd(new EventDateTime().setDateTime(new DateTime(endDate.toString())));
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("dd/MM/yyyy hh:mm a");
+        org.joda.time.DateTime startDate = dateTimeFormatter.parseDateTime(webinarInfoDto.getStartDate());
+        org.joda.time.DateTime endDate = dateTimeFormatter.parseDateTime(webinarInfoDto.getEndDate());
+        event.setStart(new EventDateTime().setDateTime(new DateTime(startDate.toString())));
+        event.setEnd(new EventDateTime().setDateTime(new DateTime(endDate.toString())));
 
-    webinarInfoDto.getAttendees().add(organiserEmail);
-    EventAttendee[] attendees = webinarInfoDto.getAttendees().stream()
-      .map(attEmail -> new EventAttendee().setEmail(attEmail))
-      .toArray(EventAttendee[]::new);
+        webinarInfoDto.getAttendees().add(organiserEmail);
+        EventAttendee[] attendees = webinarInfoDto.getAttendees().stream()
+                .map(attEmail -> new EventAttendee().setEmail(attEmail))
+                .toArray(EventAttendee[]::new);
 
-    event.setAttendees(Arrays.asList(attendees));
+        event.setAttendees(Arrays.asList(attendees));
 
-    event = googleCalendar.events().insert(CALENDAR_ID, event).setSendNotifications(true).execute();
+        event = googleCalendar.events().insert(CALENDAR_ID, event).setSendNotifications(true).execute();
 
-    WebinarEntity entity = dozerMapper.map(webinarInfoDto, WebinarEntity.class);
-    entity.setCalendarUrl(event.getHtmlLink());
-    entity.setHangoutLink(event.getHangoutLink());
+        WebinarEntity entity = dozerMapper.map(webinarInfoDto, WebinarEntity.class);
+        entity.setCalendarUrl(event.getHtmlLink());
+        entity.setHangoutLink(event.getHangoutLink());
 
-    entity.setOrganiser(organiserEmail);
+        entity.setOrganiser(organiserEmail);
 
-    entity = webinarRepository.save(entity);
-    return dozerMapper.map(entity, WebinarInfoDto.class);
-  }
+        entity = webinarRepository.save(entity);
+        return dozerMapper.map(entity, WebinarInfoDto.class);
+    }
 
-  public Collection<WebinarInfoDto> findNotExpiredWebinars() {
-    
+    public Collection<WebinarInfoDto> findNotExpiredWebinars() {
 
-    return null;
-  }
+
+        return null;
+    }
 }
