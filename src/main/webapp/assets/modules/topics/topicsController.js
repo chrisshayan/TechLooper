fc2fd9da0ec96ac9e387551858c849f4e53e12a4
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-09-15 10:56:56 +0700

    add url for latest topics

--- src/main/webapp/assets/modules/topics/topicsController.js
+++ src/main/webapp/assets/modules/topics/topicsController.js
@@ -2,6 +2,7 @@ techlooper.controller("topicsController", function ($scope, apiService, jsonValu
   utils.sendNotification(jsonValue.notifications.loading);
   apiService.getlatestTopics().success(function(response) {
     $scope.topics = response.topics;
+    console.log($scope.topics);
   }).finally(function () {
     utils.sendNotification(jsonValue.notifications.loaded);
   });
