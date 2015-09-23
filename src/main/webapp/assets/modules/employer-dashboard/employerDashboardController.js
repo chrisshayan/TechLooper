06856946d0b7968c57f7e0e92cc29e55ebb25847
Phuong H <phuonghqh@gmail.com>
2015-09-23 16:18:27 +0700

    Fix challenge list in dashboard

--- src/main/webapp/assets/modules/employer-dashboard/employerDashboardController.js
+++ src/main/webapp/assets/modules/employer-dashboard/employerDashboardController.js
@@ -5,54 +5,17 @@ techlooper.controller('employerDashboardController', function ($scope, jsonValue
   apiService.getEmployerDashboardInfo()
     .success(function (data) {
       utils.sortByDate(data.challenges, "startDateTime");
-
-      data.activeChallenges = $filter("progress")(data.challenges, "challenges", [jsonValue.status.registration, jsonValue.status.progress]);
-      data.notStartedChallenges = $filter("progress")(data.challenges, "challenges", jsonValue.status.notStarted);
-      data.closedChallenges = $filter("progress")(data.challenges, "challenges", jsonValue.status.closed);
-
       data.projects.sort(function (left, right) {return right.projectId - left.projectId;});
-
       $scope.dashboardInfo = data;
-      $scope.changeChallengesByStatus();
     })
     .finally(function () {utils.sendNotification(jsonValue.notifications.loaded, $(window).height());});
 
-  $scope.myFunction = function (val) {
-    return val.progress.translate != 'notStart' && val.progress.translate != 'closed';
-  };
+  $scope.changeChallengeStatus = function (status) {
+    $scope.challengeStatus = status;
+  }
 
   $scope.toEditPage = function (challenge) {
     $location.url("post-challenge?a=edit&id=" + challenge.challengeId);
-  }
-
-  $scope.changeChallengesByStatus = function (status) {
-    switch (status) {
-      case "active":
-        $scope.challengesByStatus = {
-          challenges: $scope.dashboardInfo.activeChallenges,
-          status: "active"
-        }
-        break;
-
-      case "notStarted":
-        $scope.challengesByStatus = {
-          challenges: $scope.dashboardInfo.notStartedChallenges,
-          status: "notStarted"
-        }
-        break;
-
-      case "closed":
-        $scope.challengesByStatus = {
-          challenges: $scope.dashboardInfo.closedChallenges,
-          status: "closed"
-        }
-        break;
-
-      default:
-        $scope.challengesByStatus = {
-          challenges: $scope.dashboardInfo.challenges
-        }
-    }
   }
 
   $scope.filterChallenges = function (status) {
@@ -66,13 +29,14 @@ techlooper.controller('employerDashboardController', function ($scope, jsonValue
       $("#challenge-" + challenge.challengeId).find("td")
         .animate({padding: 0}).wrapInner("<div />").children("div")
         .slideUp(100, function () {
+          //console.log(challenge);
           var index = $.inArray(challenge, $scope.dashboardInfo.challenges);
           if (index < 0) {
             return;
           }
-          $scope.dashboardInfo.notStartedChallenges.splice(index, 1);
-          //data.closedChallenges = $filter("progress")(data.challenges, "challenges", jsonValue.status.closed);
-          if (!$scope.dashboardInfo.notStartedChallenges.length) $scope.changeChallengesByStatus();
+          $scope.dashboardInfo.challenges.splice(index, 1);
+          var challenges = $filter("progress")($scope.dashboardInfo.challenges, "challenges", jsonValue.status.notStarted);
+          if (!challenges.length) $scope.changeChallengeStatus();
           $scope.$apply();
         });
     }
