e077450b54b5d7c64c1b2411345dbd48a0fb8d0d
khoa-nd <ndkhoa.is@gmail.com>
2015-09-24 15:18:16 +0700

    Check user exist on vietnamworks to decide send email on boarding or not

--- src/main/java/com/techlooper/service/impl/UserServiceImpl.java
+++ src/main/java/com/techlooper/service/impl/UserServiceImpl.java
@@ -1,12 +1,8 @@
 package com.techlooper.service.impl;
 
-import com.techlooper.entity.*;
 import com.techlooper.entity.userimport.UserImportEntity;
-import com.techlooper.entity.vnw.VnwUser;
 import com.techlooper.entity.vnw.dto.VnwUserDto;
 import com.techlooper.model.*;
-import com.techlooper.repository.couchbase.UserRegistrationRepository;
-import com.techlooper.repository.couchbase.UserRepository;
 import com.techlooper.repository.elasticsearch.ChallengeRegistrantRepository;
 import com.techlooper.repository.elasticsearch.ProjectRegistrantRepository;
 import com.techlooper.repository.elasticsearch.SalaryReviewRepository;
@@ -14,7 +10,10 @@ import com.techlooper.repository.elasticsearch.WebinarRepository;
 import com.techlooper.repository.talentsearch.query.TalentSearchQuery;
 import com.techlooper.repository.userimport.UserImportRepository;
 import com.techlooper.repository.vnw.VnwUserRepo;
-import com.techlooper.service.*;
+import com.techlooper.service.JobAggregatorService;
+import com.techlooper.service.TalentSearchDataProcessor;
+import com.techlooper.service.UserService;
+import com.techlooper.service.VietnamWorksUserService;
 import freemarker.template.Template;
 import org.dozer.Mapper;
 import org.elasticsearch.common.collect.Lists;
@@ -22,7 +21,6 @@ import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.QueryFilterBuilder;
 import org.elasticsearch.index.query.QueryStringQueryBuilder;
-import org.jasypt.util.text.TextEncryptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -41,11 +39,10 @@ import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.StringWriter;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
-import static org.elasticsearch.index.query.QueryBuilders.*;
-import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.wildcardQuery;
 
 /**
  * Created by NguyenDangKhoa on 12/11/14.
@@ -53,67 +50,67 @@ import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 @Service
 public class UserServiceImpl implements UserService {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
 
 //  @Resource
 //  private UserRepository userRepository;
 
-  @Resource
-  private UserImportRepository userImportRepository;
+    @Resource
+    private UserImportRepository userImportRepository;
 
-  @Resource
-  private Mapper dozerMapper;
+    @Resource
+    private Mapper dozerMapper;
 
 //  @Resource
 //  private TextEncryptor textEncryptor;
 
-//  @Resource
-//  private VietnamWorksUserService vietnamworksUserService;
+    @Resource
+    private VietnamWorksUserService vietnamworksUserService;
 
 //  @Resource
 //  private UserEvaluationService userEvaluationService;
 
-  @Resource
-  private ApplicationContext applicationContext;
+    @Resource
+    private ApplicationContext applicationContext;
 
-  @Resource
-  private SalaryReviewRepository salaryReviewRepository;
+    @Resource
+    private SalaryReviewRepository salaryReviewRepository;
 
-  @Resource
-  private VnwUserRepo vnwUserRepo;
+    @Resource
+    private VnwUserRepo vnwUserRepo;
 
-  @Resource
-  private Template onBoardingMailTemplateEn;
+    @Resource
+    private Template onBoardingMailTemplateEn;
 
-  @Resource
-  private Template onBoardingMailTemplateVi;
+    @Resource
+    private Template onBoardingMailTemplateVi;
 
-  @Resource
-  private JavaMailSender mailSender;
+    @Resource
+    private JavaMailSender mailSender;
 
-  @Resource
-  private MimeMessage fromTechlooperMailMessage;
+    @Resource
+    private MimeMessage fromTechlooperMailMessage;
 
-  @Resource
-  private ChallengeRegistrantRepository challengeRegistrantRepository;
+    @Resource
+    private ChallengeRegistrantRepository challengeRegistrantRepository;
 
-  @Resource
-  private ProjectRegistrantRepository projectRegistrantRepository;
+    @Resource
+    private ProjectRegistrantRepository projectRegistrantRepository;
 
-  @Resource
-  private WebinarRepository webinarRepository;
+    @Resource
+    private WebinarRepository webinarRepository;
 
-  @Resource
-  private JobAggregatorService jobAggregatorService;
+    @Resource
+    private JobAggregatorService jobAggregatorService;
 
-  @Value("${mail.onBoarding.subject.vi}")
-  private String onBoardingEmailSubjectVi;
+    @Value("${mail.onBoarding.subject.vi}")
+    private String onBoardingEmailSubjectVi;
 
-  @Value("${mail.onBoarding.subject.en}")
-  private String onBoardingEmailSubjectEn;
+    @Value("${mail.onBoarding.subject.en}")
+    private String onBoardingEmailSubjectEn;
 
-  @Value("${web.baseUrl}")
-  private String baseUrl;
+    @Value("${web.baseUrl}")
+    private String baseUrl;
 
 
 //  public void save(UserEntity userEntity) {
@@ -156,121 +153,114 @@ public class UserServiceImpl implements UserService {
 //    return registerSuccess;
 //  }
 
-  public boolean addCrawledUser(UserImportEntity userImportData, SocialProvider socialProvider) {
-    UserImportEntity userImportEntity = findUserImportByEmail(userImportData.getEmail());
+    public boolean addCrawledUser(UserImportEntity userImportData, SocialProvider socialProvider) {
+        UserImportEntity userImportEntity = findUserImportByEmail(userImportData.getEmail());
 
-    if (userImportEntity != null) {
-      userImportEntity.withProfile(socialProvider, userImportData.getProfiles().get(socialProvider));
+        if (userImportEntity != null) {
+            userImportEntity.withProfile(socialProvider, userImportData.getProfiles().get(socialProvider));
+        } else {
+            userImportEntity = dozerMapper.map(userImportData, UserImportEntity.class);
+            userImportEntity.withProfile(socialProvider, userImportData.getProfiles().get(socialProvider));
+            userImportEntity.setCrawled(true);
+        }
+
+        return userImportRepository.save(userImportEntity) != null;
     }
-    else {
-      userImportEntity = dozerMapper.map(userImportData, UserImportEntity.class);
-      userImportEntity.withProfile(socialProvider, userImportData.getProfiles().get(socialProvider));
-      userImportEntity.setCrawled(true);
-    }
 
-    return userImportRepository.save(userImportEntity) != null;
-  }
+    public int addCrawledUserAll(List<UserImportEntity> users, SocialProvider socialProvider, UpdateModeEnum updateMode) {
+        List<UserImportEntity> shouldBeSavedUsers = new ArrayList<>();
 
-  public int addCrawledUserAll(List<UserImportEntity> users, SocialProvider socialProvider, UpdateModeEnum updateMode) {
-    List<UserImportEntity> shouldBeSavedUsers = new ArrayList<>();
+        for (UserImportEntity user : users) {
+            try {
+                UserImportEntity userImportEntity = findUserImportByEmail(user.getEmail());
 
-    for (UserImportEntity user : users) {
-      try {
-        UserImportEntity userImportEntity = findUserImportByEmail(user.getEmail());
-
-        switch (updateMode) {
-          case ADD_NEW:
-            if (userImportEntity == null) {
-              shouldBeSavedUsers.add(user);
-            }
-            break;
-          case OVERWRITE:
-            if (userImportEntity != null) {
-              userImportEntity.withProfile(socialProvider, user.getProfiles().get(socialProvider));
-              shouldBeSavedUsers.add(userImportEntity);
-            }
-            else {
-              shouldBeSavedUsers.add(user);
-            }
-          default: //MERGE
-            if (userImportEntity != null) {
-              userImportEntity.mergeProfile(socialProvider, user.getProfiles().get(socialProvider));
-              shouldBeSavedUsers.add(userImportEntity);
-            }
-            else {
-              shouldBeSavedUsers.add(user);
+                switch (updateMode) {
+                    case ADD_NEW:
+                        if (userImportEntity == null) {
+                            shouldBeSavedUsers.add(user);
+                        }
+                        break;
+                    case OVERWRITE:
+                        if (userImportEntity != null) {
+                            userImportEntity.withProfile(socialProvider, user.getProfiles().get(socialProvider));
+                            shouldBeSavedUsers.add(userImportEntity);
+                        } else {
+                            shouldBeSavedUsers.add(user);
+                        }
+                    default: //MERGE
+                        if (userImportEntity != null) {
+                            userImportEntity.mergeProfile(socialProvider, user.getProfiles().get(socialProvider));
+                            shouldBeSavedUsers.add(userImportEntity);
+                        } else {
+                            shouldBeSavedUsers.add(user);
+                        }
+                }
+            } catch (Exception ex) {
+                LOGGER.error("User Import Fail : " + user.getEmail(), ex);
             }
         }
-      }
-      catch (Exception ex) {
-        LOGGER.error("User Import Fail : " + user.getEmail(), ex);
-      }
+
+        return Lists.newArrayList(userImportRepository.save(shouldBeSavedUsers)).size();
     }
 
-    return Lists.newArrayList(userImportRepository.save(shouldBeSavedUsers)).size();
-  }
+    public UserImportEntity findUserImportByEmail(String email) {
+        UserImportEntity userImportEntity = userImportRepository.findOne(email);
 
-  public UserImportEntity findUserImportByEmail(String email) {
-    UserImportEntity userImportEntity = userImportRepository.findOne(email);
+        if (userImportEntity != null) {
+            return userImportEntity;
+        } else {
+            QueryFilterBuilder queryFilterBuilder = FilterBuilders.queryFilter(
+                    QueryBuilders.queryStringQuery(email).defaultOperator(QueryStringQueryBuilder.Operator.AND)).cache(true);
+            SearchQuery userSearchQuery = new NativeSearchQueryBuilder()
+                    .withFilter(queryFilterBuilder)
+                    .withPageable(new PageRequest(0, 10))
+                    .build();
+            Page<UserImportEntity> result = userImportRepository.search(userSearchQuery);
+            if (result.getNumberOfElements() > 0) {
+                return result.getContent().get(0);
+            } else {
+                return null;
+            }
+        }
 
-    if (userImportEntity != null) {
-      return userImportEntity;
-    }
-    else {
-      QueryFilterBuilder queryFilterBuilder = FilterBuilders.queryFilter(
-        QueryBuilders.queryStringQuery(email).defaultOperator(QueryStringQueryBuilder.Operator.AND)).cache(true);
-      SearchQuery userSearchQuery = new NativeSearchQueryBuilder()
-        .withFilter(queryFilterBuilder)
-        .withPageable(new PageRequest(0, 10))
-        .build();
-      Page<UserImportEntity> result = userImportRepository.search(userSearchQuery);
-      if (result.getNumberOfElements() > 0) {
-        return result.getContent().get(0);
-      }
-      else {
-        return null;
-      }
     }
 
-  }
+    public List<UserImportEntity> getAll(int pageNumber, int pageSize) {
+        final SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(boolQuery()
+                        .mustNot(wildcardQuery("email", "*users.noreply.github.com"))
+                        .mustNot(wildcardQuery("email", "*missing.com")))
+                .withPageable(new PageRequest(pageNumber, pageSize))
+                .build();
 
-  public List<UserImportEntity> getAll(int pageNumber, int pageSize) {
-    final SearchQuery searchQuery = new NativeSearchQueryBuilder()
-      .withQuery(boolQuery()
-        .mustNot(wildcardQuery("email", "*users.noreply.github.com"))
-        .mustNot(wildcardQuery("email", "*missing.com")))
-      .withPageable(new PageRequest(pageNumber, pageSize))
-      .build();
-
-    return userImportRepository.search(searchQuery).getContent();
-  }
-
-  public TalentSearchResponse findTalent(TalentSearchRequest param) {
-    List<SocialProvider> socialProviders = Arrays.asList(SocialProvider.GITHUB);
-    TalentSearchResponse.Builder builder = new TalentSearchResponse.Builder();
-
-    for (SocialProvider socialProvider : socialProviders) {
-      TalentSearchQuery talentSearchQuery =
-        applicationContext.getBean(socialProvider + "TalentSearchQuery", TalentSearchQuery.class);
-      ElasticsearchRepository talentSearchRepository =
-        applicationContext.getBean(socialProvider + "TalentSearchRepository", ElasticsearchRepository.class);
-      TalentSearchDataProcessor talentSearchDataProcessor =
-        applicationContext.getBean(socialProvider + "TalentSearchDataProcessor", TalentSearchDataProcessor.class);
-
-      if (param != null) {
-        talentSearchDataProcessor.normalizeInputParameter(param);
-      }
-      else {
-        param = talentSearchDataProcessor.getSearchAllRequestParameter();
-      }
-
-      FacetedPage<UserImportEntity> pageResult = talentSearchRepository.search(talentSearchQuery.getSearchQuery(param));
-      builder.withTotal(pageResult.getTotalElements());
-      builder.withResult(talentSearchDataProcessor.process(pageResult.getContent()));
+        return userImportRepository.search(searchQuery).getContent();
     }
 
-    return builder.build();
-  }
+    public TalentSearchResponse findTalent(TalentSearchRequest param) {
+        List<SocialProvider> socialProviders = Arrays.asList(SocialProvider.GITHUB);
+        TalentSearchResponse.Builder builder = new TalentSearchResponse.Builder();
+
+        for (SocialProvider socialProvider : socialProviders) {
+            TalentSearchQuery talentSearchQuery =
+                    applicationContext.getBean(socialProvider + "TalentSearchQuery", TalentSearchQuery.class);
+            ElasticsearchRepository talentSearchRepository =
+                    applicationContext.getBean(socialProvider + "TalentSearchRepository", ElasticsearchRepository.class);
+            TalentSearchDataProcessor talentSearchDataProcessor =
+                    applicationContext.getBean(socialProvider + "TalentSearchDataProcessor", TalentSearchDataProcessor.class);
+
+            if (param != null) {
+                talentSearchDataProcessor.normalizeInputParameter(param);
+            } else {
+                param = talentSearchDataProcessor.getSearchAllRequestParameter();
+            }
+
+            FacetedPage<UserImportEntity> pageResult = talentSearchRepository.search(talentSearchQuery.getSearchQuery(param));
+            builder.withTotal(pageResult.getTotalElements());
+            builder.withResult(talentSearchDataProcessor.process(pageResult.getContent()));
+        }
+
+        return builder.build();
+    }
 
 //  public void registerUser(UserInfo userInfo) {
 //    UserRegistration user = dozerMapper.map(userInfo, UserRegistration.class);
@@ -285,55 +275,43 @@ public class UserServiceImpl implements UserService {
 //  }
 
 
-  public SalaryReviewDto findSalaryReviewById(String base64Id) {
-    Long id = Long.parseLong(new String(Base64.getDecoder().decode(base64Id)));
-    return dozerMapper.map(salaryReviewRepository.findOne(id), SalaryReviewDto.class);
-  }
-
-  public VnwUserDto findVnwUserByUsername(String username) {
-    return dozerMapper.map(vnwUserRepo.findByUsernameIgnoreCase(username), VnwUserDto.class);
-  }
-
-  public boolean sendOnBoardingEmail(String email, Language language) throws MessagingException {
-//    String email = userProfile.getEmail();
-//    String email = "phamthanhtrinh.vt@gmail.com";
-    Iterator<ChallengeRegistrantEntity> challengeRegistrant = challengeRegistrantRepository.search(matchQuery("registrantEmail", email).minimumShouldMatch("100%")).iterator();
-    boolean hasUser = challengeRegistrant.hasNext();
-    if (!hasUser) {
-      Iterator<ProjectRegistrantEntity> projectRegistrant = projectRegistrantRepository.search(matchQuery("registrantEmail", email).minimumShouldMatch("100%")).iterator();
-      hasUser = projectRegistrant.hasNext();
-    }
-    if (!hasUser) {
-      Iterator<WebinarEntity> webinar = webinarRepository.search(nestedQuery("attendees", matchQuery("attendees.email", email).minimumShouldMatch("100%"))).iterator();
-      hasUser = webinar.hasNext();
+    public SalaryReviewDto findSalaryReviewById(String base64Id) {
+        Long id = Long.parseLong(new String(Base64.getDecoder().decode(base64Id)));
+        return dozerMapper.map(salaryReviewRepository.findOne(id), SalaryReviewDto.class);
     }
 
-    if (!hasUser) {
-      try {
-        StringWriter stringWriter = new StringWriter();
-
-        Map<String, Object> templateModel = new HashMap<>();
-        templateModel.put("webBaseUrl", baseUrl);
-        templateModel.put("totalOfItJobs", jobAggregatorService.findJob(new JobSearchCriteria()).getJobs().size());
-
-        Template template = language == Language.vi ? onBoardingMailTemplateVi : onBoardingMailTemplateEn;
-        template.process(templateModel, stringWriter);
-
-        fromTechlooperMailMessage.setSubject(language == Language.vi ? onBoardingEmailSubjectVi : onBoardingEmailSubjectEn);
-        fromTechlooperMailMessage.setRecipients(Message.RecipientType.TO, email);
-        fromTechlooperMailMessage.setText(stringWriter.toString(), "UTF-8", "html");
-        fromTechlooperMailMessage.saveChanges();
-        mailSender.send(fromTechlooperMailMessage);
-
-        stringWriter.flush();
-      }
-      catch (Exception e) {
-        LOGGER.error("Can not send on boarding email", e);
-        return false;
-      }
+    public VnwUserDto findVnwUserByUsername(String username) {
+        return dozerMapper.map(vnwUserRepo.findByUsernameIgnoreCase(username), VnwUserDto.class);
     }
 
-    return true;
-  }
+    public boolean sendOnBoardingEmail(String email, Language language) throws MessagingException {
+        boolean existUser = vietnamworksUserService.existUser(email);
+
+        if (!existUser) {
+            try {
+                StringWriter stringWriter = new StringWriter();
+
+                Map<String, Object> templateModel = new HashMap<>();
+                templateModel.put("webBaseUrl", baseUrl);
+                templateModel.put("totalOfItJobs", jobAggregatorService.findJob(new JobSearchCriteria()).getJobs().size());
+
+                Template template = language == Language.vi ? onBoardingMailTemplateVi : onBoardingMailTemplateEn;
+                template.process(templateModel, stringWriter);
+
+                fromTechlooperMailMessage.setSubject(language == Language.vi ? onBoardingEmailSubjectVi : onBoardingEmailSubjectEn);
+                fromTechlooperMailMessage.setRecipients(Message.RecipientType.TO, email);
+                fromTechlooperMailMessage.setText(stringWriter.toString(), "UTF-8", "html");
+                fromTechlooperMailMessage.saveChanges();
+                mailSender.send(fromTechlooperMailMessage);
+
+                stringWriter.flush();
+            } catch (Exception e) {
+                LOGGER.error("Can not send on boarding email", e);
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 }
--- src/main/java/com/techlooper/service/impl/VietnamWorksUserServiceImpl.java
+++ src/main/java/com/techlooper/service/impl/VietnamWorksUserServiceImpl.java
@@ -1,33 +1,20 @@
 package com.techlooper.service.impl;
 
-import com.techlooper.entity.ChallengeRegistrantEntity;
-import com.techlooper.entity.ProjectRegistrantEntity;
 import com.techlooper.entity.VnwUserProfile;
-import com.techlooper.entity.WebinarEntity;
 import com.techlooper.repository.JobSearchAPIConfigurationRepository;
-import com.techlooper.repository.elasticsearch.ChallengeRegistrantRepository;
-import com.techlooper.repository.elasticsearch.ProjectRegistrantRepository;
-import com.techlooper.repository.elasticsearch.WebinarRepository;
 import com.techlooper.service.VietnamWorksUserService;
 import com.techlooper.util.JsonUtils;
 import com.techlooper.util.RestTemplateUtils;
-import org.elasticsearch.index.query.MatchQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
-import java.util.Iterator;
-
 import static org.apache.commons.lang3.StringUtils.EMPTY;
-import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
-import static org.elasticsearch.index.query.QueryBuilders.nestedQuery;
 
 /**
  * @author khoa-nd
@@ -36,35 +23,35 @@ import static org.elasticsearch.index.query.QueryBuilders.nestedQuery;
 @Service
 public class VietnamWorksUserServiceImpl implements VietnamWorksUserService {
 
-  private static final String RESPONSE_CODE_SUCCESS = "200";
+    private static final String RESPONSE_CODE_SUCCESS = "200";
 
-//  private static final String ACCOUNT_STATUS_NEW = "NEW";
+    private static final String ACCOUNT_STATUS_NEW = "NEW";
 
-  @Resource
-  private RestTemplate restTemplate;
+    @Resource
+    private RestTemplate restTemplate;
 
-  @Resource
-  private JobSearchAPIConfigurationRepository apiConfiguration;
+    @Resource
+    private JobSearchAPIConfigurationRepository apiConfiguration;
 
-//  public boolean existUser(String userEmail) {
-//    HttpEntity<String> requestEntity = RestTemplateUtils.configureHttpRequestEntity(
-//      MediaType.APPLICATION_JSON, apiConfiguration.getApiKeyName(),
-//      apiConfiguration.getApiKeyValue(), EMPTY);
-//    String requestUrl = apiConfiguration.getAccountStatus() + "/" + userEmail;
-//    ResponseEntity<String> responseEntity = restTemplate.exchange(
-//      requestUrl, HttpMethod.POST, requestEntity, String.class);
-//    String responseBody = responseEntity.getBody();
-//    return !responseBody.contains(ACCOUNT_STATUS_NEW);
-//  }
+    public boolean existUser(String userEmail) {
+        HttpEntity<String> requestEntity = RestTemplateUtils.configureHttpRequestEntity(
+                MediaType.APPLICATION_JSON, apiConfiguration.getApiKeyName(),
+                apiConfiguration.getApiKeyValue(), EMPTY);
+        String requestUrl = apiConfiguration.getAccountStatus() + "/" + userEmail;
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                requestUrl, HttpMethod.POST, requestEntity, String.class);
+        String responseBody = responseEntity.getBody();
+        return !responseBody.contains(ACCOUNT_STATUS_NEW);
+    }
 
-  public boolean register(VnwUserProfile userProfile) {
-    final String userInfoParameters = JsonUtils.toJSON(userProfile).orElse(EMPTY);
-    HttpEntity<String> requestEntity = RestTemplateUtils.configureHttpRequestEntity(
-      MediaType.APPLICATION_JSON, apiConfiguration.getApiKeyName(),
-      apiConfiguration.getApiKeyValue(), userInfoParameters);
-    ResponseEntity<String> responseEntity = restTemplate.exchange(
-      apiConfiguration.getRegisterUrl(), HttpMethod.POST, requestEntity, String.class);
-    String responseBody = responseEntity.getBody();
-    return responseBody.contains(RESPONSE_CODE_SUCCESS);
-  }
+    public boolean register(VnwUserProfile userProfile) {
+        final String userInfoParameters = JsonUtils.toJSON(userProfile).orElse(EMPTY);
+        HttpEntity<String> requestEntity = RestTemplateUtils.configureHttpRequestEntity(
+                MediaType.APPLICATION_JSON, apiConfiguration.getApiKeyName(),
+                apiConfiguration.getApiKeyValue(), userInfoParameters);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                apiConfiguration.getRegisterUrl(), HttpMethod.POST, requestEntity, String.class);
+        String responseBody = responseEntity.getBody();
+        return responseBody.contains(RESPONSE_CODE_SUCCESS);
+    }
 }
