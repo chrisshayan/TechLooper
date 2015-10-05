419aa7458c50aa31d374906f5fac82354a9baba4
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-10-05 14:27:37 +0700

    move submission challenge phase to component

---
+++ src/main/webapp/assets/modules/common/submissionChallenge.js
@@ -0,0 +1,7 @@
+techlooper.directive("submissionChallenge", function ($timeout, jsonValue, utils) {
+  return {
+    restrict: "E",
+    replace: true,
+    templateUrl: "modules/common/submission-challenge.html"
+  }
+});
