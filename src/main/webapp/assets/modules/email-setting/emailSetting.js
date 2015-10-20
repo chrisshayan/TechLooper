62c52458efd094400ed9457905dab47f9e9bb072
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-10-20 18:07:05 +0700

    validation email and show successfully and fail messages for setting email page

--- src/main/webapp/assets/modules/email-setting/emailSetting.js
+++ src/main/webapp/assets/modules/email-setting/emailSetting.js
@@ -1,23 +1,43 @@
-techlooper.controller("emailSetting", function ($scope, apiService) {
-
+techlooper.controller("emailSetting", function ($scope, utils, jsonValue, apiService, validatorService) {
+  utils.sendNotification(jsonValue.notifications.switchScope);
+  $scope.status = function (type) {
+    switch (type) {
+      case "already-save":
+        return $scope.saved;
+      case "already-fail":
+        return $scope.fail;
+    }
+  };
   apiService.loadEmailSetting()
     .success(function (data) {
       $scope.replyEmail = data.replyEmail;
       $scope.emailSignature = data.emailSignature;
-    })
+    }).finally(function(){
+      utils.sendNotification(jsonValue.notifications.loaded);
+    });
 
   $scope.saveEmailSetting = function () {
+    $scope.saved = false;
+    $scope.fail = false;
+    var error = validatorService.validate($(".email-setting-form").find('input'));
+    $scope.error = error;
+    if (!$.isEmptyObject(error)) {
+      return;
+    }
+    utils.sendNotification(jsonValue.notifications.switchScope);
     var emailSetting = {
       replyEmail: $scope.replyEmail,
       emailSignature: $scope.emailSignature
     };
     apiService.saveEmailSetting(emailSetting)
       .success(function () {
-        console.log("Success :");
+          $scope.saved = true;
       })
       .error(function () {
-        console.log("Fail :");
-      })
+          $scope.fail = true;
+      }).finally(function(){
+          utils.sendNotification(jsonValue.notifications.loaded);
+      });
   };
 
   $scope.cancleEmailSetting = function () {
