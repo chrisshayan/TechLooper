f547bf9aa91d5dc430cd877b33ab6390f0f9d1a1
khoa-nd <ndkhoa.is@gmail.com>
2015-10-22 15:56:35 +0700

    Clean the coding style

--- src/main/java/com/techlooper/model/CitibankCreditCardPromotion.java
+++ src/main/java/com/techlooper/model/CitibankCreditCardPromotion.java
@@ -10,89 +10,89 @@ import javax.validation.constraints.NotNull;
  */
 public class CitibankCreditCardPromotion {
 
-  @NotNull
-  private Long salaryReviewId;
+    @NotNull
+    private Long salaryReviewId;
 
-  @NotNull
-  private String fullName;
+    @NotNull
+    private String fullName;
 
-  @NotNull
-  @Length(min = 10, max = 20)
-  private String mobileNumber;
+    @NotNull
+    @Length(min = 10, max = 20)
+    private String mobileNumber;
 
-  @Email
-  private String email;
+    @Email
+    private String email;
 
-  @NotNull
-  private PaymentMethod paymentMethod;
+    @NotNull
+    private PaymentMethod paymentMethod;
 
-  private String location;
+    private String location;
 
-  private String netIncome;
+    private String netIncome;
 
-  private Boolean agree;
+    private Boolean agree;
 
-  public String getNetIncome() {
-    return netIncome;
-  }
+    public String getNetIncome() {
+        return netIncome;
+    }
 
-  public void setNetIncome(String netIncome) {
-    this.netIncome = netIncome;
-  }
+    public void setNetIncome(String netIncome) {
+        this.netIncome = netIncome;
+    }
 
-  public String getLocation() {
-    return location;
-  }
+    public String getLocation() {
+        return location;
+    }
 
-  public void setLocation(String location) {
-    this.location = location;
-  }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
-  public Long getSalaryReviewId() {
-    return salaryReviewId;
-  }
+    public Long getSalaryReviewId() {
+        return salaryReviewId;
+    }
 
-  public void setSalaryReviewId(Long salaryReviewId) {
-    this.salaryReviewId = salaryReviewId;
-  }
+    public void setSalaryReviewId(Long salaryReviewId) {
+        this.salaryReviewId = salaryReviewId;
+    }
 
-  public String getFullName() {
-    return fullName;
-  }
+    public String getFullName() {
+        return fullName;
+    }
 
-  public void setFullName(String fullName) {
-    this.fullName = fullName;
-  }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
-  public String getMobileNumber() {
-    return mobileNumber;
-  }
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
 
-  public void setMobileNumber(String mobileNumber) {
-    this.mobileNumber = mobileNumber;
-  }
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public PaymentMethod getPaymentMethod() {
-    return paymentMethod;
-  }
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
 
-  public void setPaymentMethod(PaymentMethod paymentMethod) {
-    this.paymentMethod = paymentMethod;
-  }
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 
-  public Boolean getAgree() {
-    return agree;
-  }
+    public Boolean getAgree() {
+        return agree;
+    }
 
-  public void setAgree(Boolean agree) {
-    this.agree = agree;
-  }
+    public void setAgree(Boolean agree) {
+        this.agree = agree;
+    }
 }
