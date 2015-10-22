a37a8b37ec56f26bfcb6f5433de69700e0256fe3
khoa-nd <ndkhoa.is@gmail.com>
2015-10-22 15:02:03 +0700

    Avoid unused import

--- src/main/java/com/techlooper/entity/ChallengeRegistrantCriteria.java
+++ src/main/java/com/techlooper/entity/ChallengeRegistrantCriteria.java
@@ -4,8 +4,6 @@ import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldIndex;
 
 import java.io.Serializable;
-import java.util.Collections;
-import java.util.HashSet;
 
 import static org.springframework.data.elasticsearch.annotations.FieldType.String;
 
@@ -14,65 +12,65 @@ import static org.springframework.data.elasticsearch.annotations.FieldType.Strin
  */
 public class ChallengeRegistrantCriteria implements Serializable {
 
-  @Field(type = String, index = FieldIndex.not_analyzed)
-  private String criteriaId;
+    @Field(type = String, index = FieldIndex.not_analyzed)
+    private String criteriaId;
 
-  private String name;
+    private String name;
 
-  private Long weight;
+    private Long weight;
 
-  private Long score;
+    private Long score;
 
-  private String comment;
+    private String comment;
 
-  public java.lang.String getComment() {
-    return comment;
-  }
+    public java.lang.String getComment() {
+        return comment;
+    }
 
-  public void setComment(java.lang.String comment) {
-    this.comment = comment;
-  }
+    public void setComment(java.lang.String comment) {
+        this.comment = comment;
+    }
 
-  public java.lang.String getCriteriaId() {
-    return criteriaId;
-  }
+    public java.lang.String getCriteriaId() {
+        return criteriaId;
+    }
 
-  public void setCriteriaId(java.lang.String criteriaId) {
-    this.criteriaId = criteriaId;
-  }
+    public void setCriteriaId(java.lang.String criteriaId) {
+        this.criteriaId = criteriaId;
+    }
 
-  public java.lang.String getName() {
-    return name;
-  }
+    public java.lang.String getName() {
+        return name;
+    }
 
-  public void setName(java.lang.String name) {
-    this.name = name;
-  }
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
 
-  public Long getWeight() {
-    return weight;
-  }
+    public Long getWeight() {
+        return weight;
+    }
 
-  public void setWeight(Long weight) {
-    this.weight = weight;
-  }
+    public void setWeight(Long weight) {
+        this.weight = weight;
+    }
 
-  public Long getScore() {
-    return score;
-  }
+    public Long getScore() {
+        return score;
+    }
 
-  public void setScore(Long score) {
-    this.score = score;
-  }
+    public void setScore(Long score) {
+        this.score = score;
+    }
 
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    ChallengeRegistrantCriteria that = (ChallengeRegistrantCriteria) o;
-    return !(criteriaId != null ? !criteriaId.equals(that.criteriaId) : that.criteriaId != null);
-  }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChallengeRegistrantCriteria that = (ChallengeRegistrantCriteria) o;
+        return !(criteriaId != null ? !criteriaId.equals(that.criteriaId) : that.criteriaId != null);
+    }
 
-  public int hashCode() {
-    return criteriaId != null ? criteriaId.hashCode() : 0;
-  }
+    public int hashCode() {
+        return criteriaId != null ? criteriaId.hashCode() : 0;
+    }
 }
