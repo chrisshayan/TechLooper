d0b023a97b02e816e123bb6d15123c735c86674e
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-10-22 15:26:16 +0700

    add popover for remove criteria

--- src/main/webapp/assets/modules/contest-detail/challengeCriteriaInputDirective.js
+++ src/main/webapp/assets/modules/contest-detail/challengeCriteriaInputDirective.js
@@ -8,8 +8,15 @@ techlooper.directive('challengeCriteriaInput', function () {
       contestDetail: "="
     },
     link: function (scope, element, attr, ctrl) {
+      scope.popover = {
+        "title": "Title",
+        "content": "Hello Popover<br />This is a multiline message!"
+      };
       //scope.criteriaForm.$setSubmitted();
       //scope.contestDetail.$invalid = scope.criteriaForm.$invalid;
+      //$('[data-toggle="popover"]').popover({
+      //  html: true
+      //});
     }
   };
 });
