fb538edb4d63e728e04c37fc8ba51874eb7769b4
khoa-nd <ndkhoa.is@gmail.com>
2015-10-22 16:31:31 +0700

    Avoid unused imports such as 'java.util.stream.Collectors'

--- src/main/java/com/techlooper/controller/UserController.java
+++ src/main/java/com/techlooper/controller/UserController.java
@@ -29,7 +29,6 @@ import java.security.Principal;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
