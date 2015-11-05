808735cf2141ffea0c90a43c99d767ec12904634
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-11-05 19:05:11 +0700

    fix bug loading

--- src/main/webapp/assets/modules/common/feedback/feedbackDirective.js
+++ src/main/webapp/assets/modules/common/feedback/feedbackDirective.js
@@ -36,17 +36,10 @@ techlooper.directive("feedbackForm", function (apiService, $timeout, resourcesSe
 
       scope.send = function () {
         if (scope.feedbackForm.$invalid) return;
-        $('.feedback-loading').css('visibility', 'inherit');
         var registrant = scope.registrants[0];
         apiService.sendFeedbackToRegistrant(registrant.registrantId, scope.composeEmail)
           .success(function () {
               scope.cancel();
-          })
-          .error(function () {
-            //scope.composeEmail.error = false;
-            $timeout(function () {
-              $('.feedback-loading').css('visibility', 'hidden');
-            }, 1200);
           });
       }
 
