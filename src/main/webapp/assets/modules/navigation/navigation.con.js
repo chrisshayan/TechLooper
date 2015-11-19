1d1c68f3cac1c33a8e260e4a8be0f67754ff475c
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-11-19 09:58:50 +0700

    fix bug javascript for complexity

--- src/main/webapp/assets/modules/navigation/navigation.con.js
+++ src/main/webapp/assets/modules/navigation/navigation.con.js
@@ -1,10 +1,11 @@
 techlooper.controller("navigationController", function ($scope, securityService, apiService, localStorageService, $location,
                                                         jsonValue, utils, $timeout, $rootScope, $window) {
   $scope.state = function (type) {
+    var isEmployer = undefined;
     switch (type) {
       case "show-employer-header":
         var isEmployerView = $rootScope.currentUiView && $rootScope.currentUiView.header === "EMPLOYER";
-        var isEmployer = $rootScope.userInfo && $rootScope.userInfo.roleName === "EMPLOYER";
+        isEmployer = $rootScope.userInfo && $rootScope.userInfo.roleName === "EMPLOYER";
         return isEmployerView || isEmployer;
 
       case "show-job-seeker-header":
@@ -12,50 +13,9 @@ techlooper.controller("navigationController", function ($scope, securityService,
         var isJobSeekerView = $rootScope.currentUiView && $rootScope.currentUiView.type != "LOGIN";
         return employerHeaderNotShown && isJobSeekerView;
 
-
       case "home-url":
-        var isEmployer = $rootScope.userInfo && $rootScope.userInfo.roleName === "EMPLOYER";
-        //if ($rootScope.userInfo) {
-        //  if ($rootScope.userInfo.roleName === "JOB_SEEKER") {
-        //    return "home";
-        //  }
-        //  return "hiring";
-        //}
+        isEmployer = $rootScope.userInfo && $rootScope.userInfo.roleName === "EMPLOYER";
         return isEmployer ? "hiring" : "home";
-
-      //case "employer-signed-in":
-      //  if (!$rootScope.userInfo) {
-      //    switch (utils.getView()) {
-      //      case jsonValue.views.hiring:
-      //      case jsonValue.views.whyChallenge:
-      //      case jsonValue.views.whyFreelancer:
-      //      case jsonValue.views.priceJob:
-      //        return true;
-      //    }
-      //    return false;
-      //  }
-      //  return $rootScope.userInfo.roleName === "EMPLOYER";
-
-      //case "job-seeker-signed-in":
-      //  if (!$rootScope.userInfo) {
-      //    switch (utils.getView()) {
-      //      case jsonValue.views.login:
-      //      case jsonValue.views.userType:
-      //        return false;
-      //    }
-      //
-      //    if ($scope.state("employer-signed-in")) {
-      //      return false;
-      //    }
-      //    return true;
-      //  }
-      //  return $rootScope.userInfo.roleName === "JOB_SEEKER";
-
-      //case "navBarHided":
-      //  return !($scope.state("employer-signed-in") && $scope.state("job-seeker-signed-in"));
-
-      //case "current-view":
-      //  return utils.getView();
     }
   }
 
@@ -63,18 +23,6 @@ techlooper.controller("navigationController", function ($scope, securityService,
     securityService.logout();
     localStorageService.remove("social");
   }
-
-  //var code = localStorageService.get("code");
-  //if (code) {
-  //  utils.sendNotification(jsonValue.notifications.loading, $(window).height());
-  //  securityService.login(code, localStorageService.get("social"), "social").success(function (data) {
-  //    securityService.getCurrentUser("social");
-  //  }).finally(function () {utils.sendNotification(jsonValue.notifications.loaded, $(window).height());});
-  //  localStorageService.remove("code");
-  //  return;
-  //}
-  //
-  //securityService.getCurrentUser();
 
   $scope.isActive = function (viewLocation) {
     var active = (viewLocation === $location.path());
