171a922703f7051cdaea95cc4c613af73b30b3dd
johnsonpham <phamthanhtrinh.vt@gmail.com>
2015-12-23 15:35:15 +0700

    remove css and home-page.term html file

--- src/main/webapp/assets/modules/hiring/hiring.con.js
+++ src/main/webapp/assets/modules/hiring/hiring.con.js
@@ -6,21 +6,12 @@ techlooper.controller('hiringController', function ($scope, $timeout) {
       eventAction: "click",
       eventLabel: "postnowbtn"
     });
-  }
-  $timeout(function(){
-    var tallest = 0;
-    $('.hiring-main-feature-item').each(function () {
-      var thisHeight = $(this).height();
-      if (thisHeight > tallest)
-        tallest = thisHeight;
-    });
-    $('.hiring-main-feature-item').height(tallest + $('.cta-button').height() + 10);
-  }, 100);
+  };
   if(localStorage.NG_TRANSLATE_LANG_KEY == 'vi'){
     $('.hiring-how-does-work-content').find('.hiring').addClass('vi');
   }else{
     $('.hiring-how-does-work-content').find('.hiring').removeClass('vi');
-  }
+  };
   $scope.gotoOtherPage  = function(eventCategory, eventLabel, url){
     ga("send", {
       hitType: "event",
@@ -29,5 +20,5 @@ techlooper.controller('hiringController', function ($scope, $timeout) {
       eventLabel: eventLabel
     });
     window.location.href = url;
-  }
+  };
 });
