0754a0ddbb4c2df344389847cc5793f6f3de4b8d
Phuong H <phuonghqh@gmail.com>
2015-12-30 10:46:35 +0700

    Fix internal challenge able to be published if no domain

--- src/main/webapp/assets/modules/common/arrayModelDirective.js
+++ src/main/webapp/assets/modules/common/arrayModelDirective.js
@@ -5,7 +5,7 @@ techlooper
       restrict: 'A',
       link: function (scope, element, attrs, ngModelCtrl) {
         scope.$watch(attrs.ngModel, function (newValue, oldValue) {
-          var array = $parse(attrs.ngModel)(scope);
+          var array = $parse(attrs.ngModel)(scope) || [];
           ngModelCtrl.$setValidity("required", array.length > 0);
         }, true);
       }
