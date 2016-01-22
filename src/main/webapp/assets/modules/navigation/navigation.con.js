00f38ac6b2540bdea957f37b3b15f0efbc81d434
phuong <phuonghqh@gmail.com>
2016-01-22 16:31:41 +0700

    Revert "Update changes"

--- src/main/webapp/assets/modules/navigation/navigation.con.js
+++ src/main/webapp/assets/modules/navigation/navigation.con.js
@@ -1,18 +1,31 @@
 techlooper.controller("navigationController", function ($scope, securityService, apiService, localStorageService, $location,
                                                         jsonValue, utils, $timeout, $rootScope, $window) {
   $scope.state = function (type) {
-    var isEmployer = undefined;
-    var currentViewIsLogin = $rootScope.currentUiView &&  $rootScope.currentUiView.isLoginPage;
+    var isEmployer = $rootScope.userInfo && $rootScope.userInfo.roleName === "EMPLOYER";
+    var isJobSeeker = $rootScope.userInfo && $rootScope.userInfo.roleName === "JOB_SEEKER";
+    //var userNotLogin = !$rootScope.userInfo;
+    var currentViewIsLogin = $rootScope.currentUiView && $rootScope.currentUiView.isLoginPage;
+    var isEmployerView = $rootScope.currentUiView && $rootScope.currentUiView.header == "EMPLOYER";
+    //var isJobSeekerView = $rootScope.currentUiView &&  $rootScope.currentUiView.header != "EMPLOYER" && $rootScope.currentUiView.type != "LOGIN";
+    //console.log(isJobSeekerView);
     switch (type) {
       case "show-employer-header":
-        var isEmployerView = $rootScope.currentUiView && $rootScope.currentUiView.header === "EMPLOYER";
-        isEmployer = $rootScope.userInfo && $rootScope.userInfo.roleName === "EMPLOYER";
-        return (!currentViewIsLogin && isEmployer) || isEmployerView;
+        if (currentViewIsLogin || isJobSeeker) return false;
+        if (isEmployer) return true;
+        //isEmployer = $rootScope.userInfo && $rootScope.userInfo.roleName === "EMPLOYER";
+        //console.log("emm", isEmployerView);
+        return isEmployerView;
+      //return (!currentViewIsLogin && isEmployer) || isEmployerView;
 
       case "show-job-seeker-header":
-        var employerHeaderNotShown = !$scope.state("show-employer-header");
-        var isJobSeekerView = $rootScope.currentUiView && $rootScope.currentUiView.type != "LOGIN";
-        return !currentViewIsLogin && employerHeaderNotShown && isJobSeekerView;
+        if (currentViewIsLogin || isEmployer) return false;
+        if (isJobSeeker) return true;
+        if (isEmployerView) return false;
+        //var employerHeaderNotShown = !$scope.state("show-employer-header");
+        //var isEmployerView = $rootScope.currentUiView && $rootScope.currentUiView.header === "EMPLOYER";
+        //console.log("seek", isJobSeekerView);
+        return !currentViewIsLogin;
+      //return !currentViewIsLogin && employerHeaderNotShown && isJobSeekerView;
 
       case "home-url":
         isEmployer = $rootScope.userInfo && $rootScope.userInfo.roleName === "EMPLOYER";
@@ -37,7 +50,7 @@ techlooper.controller("navigationController", function ($scope, securityService,
     }
   }
   $scope.langKey = localStorage.NG_TRANSLATE_LANG_KEY;
-  $scope.changeLanguages = function(key){
+  $scope.changeLanguages = function (key) {
     $scope.langKey = key;
     localStorage.NG_TRANSLATE_LANG_KEY = key;
     $window.location.reload();
