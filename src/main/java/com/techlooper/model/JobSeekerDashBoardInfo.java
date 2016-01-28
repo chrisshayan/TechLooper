bcfa76b343c244db4be20398b3733105305c58f6
khoa-nd <ndkhoa.is@gmail.com>
2016-01-28 14:08:32 +0700

    Make the challenge stats for job seeker dashboard

--- src/main/java/com/techlooper/model/JobSeekerDashBoardInfo.java
+++ src/main/java/com/techlooper/model/JobSeekerDashBoardInfo.java
@@ -1,6 +1,7 @@
 package com.techlooper.model;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by NguyenDangKhoa on 1/19/16.
@@ -10,6 +11,8 @@ public class JobSeekerDashBoardInfo {
     private String email;
 
     private List<ChallengeDashBoardInfo> challenges;
+
+    private Map<JobSeekerPhaseEnum, Integer> challengeStats;
 
     public String getEmail() {
         return email;
@@ -25,5 +28,13 @@ public class JobSeekerDashBoardInfo {
 
     public void setChallenges(List<ChallengeDashBoardInfo> challenges) {
         this.challenges = challenges;
+    }
+
+    public Map<JobSeekerPhaseEnum, Integer> getChallengeStats() {
+        return challengeStats;
+    }
+
+    public void setChallengeStats(Map<JobSeekerPhaseEnum, Integer> challengeStats) {
+        this.challengeStats = challengeStats;
     }
 }
